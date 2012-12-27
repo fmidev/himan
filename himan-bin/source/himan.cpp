39ad51e658aaf098c51cf51167c6e66bcaac2e7a
Mikko Partio <mikko.partio@fmi.fi>
2012-12-27 06:30:39 +0000

    support rhel6 and g++ 4.4

--- himan-bin/source/himan.cpp
+++ himan-bin/source/himan.cpp
@@ -1,57 +1,60 @@
 //============================================================================
-// Name        : hp.cpp
+// Name        : himan.cpp
 // Author      : Mikko Partio
 // Version     : 0.1
 // Copyright   : My copyright notice
-// Description : Main program for hilpee program
+// Description : Main program for himan program
 //============================================================================
 
 #include <iostream>
+#include "himan_common.h"
 #include "plugin_factory.h"
 #include "ini_parser.h"
-#include "hilpee_plugin.h"
+#include "himan_plugin.h"
 #include "compiled_plugin.h"
 #include "auxiliary_plugin.h"
 #include "logger_factory.h"
 #include <vector>
 #include <boost/lexical_cast.hpp>
-#include <memory> // for std::unique_ptr
 
-using namespace hilpee;
+using namespace himan;
 
-int main(int argc, char ** argv) {
+void banner();
+
+int main(int argc, char** argv)
+{
 
 	std::shared_ptr<configuration> theConfiguration = ini_parser::Instance()->Parse(argc, argv);
 
+	banner();
+
 	std::unique_ptr<logger> theLogger = logger_factory::Instance()->GetLog("himan");
 
-	std::cout << "************************************************" << std::endl;
-	std::cout << "* By the power of Greyskull, I Have The Power! *" << std::endl;
-	std::cout << "************************************************" << std::endl;
-
-	std::vector<std::shared_ptr<plugin::hilpee_plugin>> thePlugins = plugin_factory::Instance()->Plugins();
+	std::vector<std::shared_ptr<plugin::himan_plugin>> thePlugins = plugin_factory::Instance()->Plugins();
 
 	theLogger->Info("Found " + boost::lexical_cast<std::string> (thePlugins.size()) + " plugins");
 
-	for (size_t i = 0; i < thePlugins.size(); i++) {
+	for (size_t i = 0; i < thePlugins.size(); i++)
+	{
 		std::string stub = "Plugin '"  + thePlugins[i]->ClassName() + "'";
 
-		switch (thePlugins[i]->PluginClass()) {
-		case kCompiled:
-			theLogger->Info(stub + " \ttype compiled (hard-coded) --> " + std::dynamic_pointer_cast<plugin::compiled_plugin> (thePlugins[i])->Formula());
-			break;
+		switch (thePlugins[i]->PluginClass())
+		{
+			case kCompiled:
+				theLogger->Info(stub + " \ttype compiled (hard-coded) --> " + std::dynamic_pointer_cast<plugin::compiled_plugin> (thePlugins[i])->Formula());
+				break;
 
-		case kAuxiliary:
-			theLogger->Info(stub + "\ttype aux");
-			break;
+			case kAuxiliary:
+				theLogger->Info(stub + "\ttype aux");
+				break;
 
-		case kInterpreted:
-			theLogger->Info(stub + "\ttype interpreted");
-			break;
+			case kInterpreted:
+				theLogger->Info(stub + "\ttype interpreted");
+				break;
 
-		default:
-			theLogger->Warning("Unknown plugin type");
-			break;
+			default:
+				theLogger->Warning("Unknown plugin type");
+				break;
 		}
 	}
 
@@ -60,15 +63,21 @@ int main(int argc, char ** argv) {
 
 	std::vector<std::string> theRequestedPlugins = theConfiguration->Plugins();
 
-	for (std::string theName : theRequestedPlugins)
-		theLogger->Debug(theName);
+	for (size_t i = 0; i < theRequestedPlugins.size(); i++)
+	{
+		theLogger->Debug(theRequestedPlugins[i]);
+	}
 
-	for (std::string theName : theRequestedPlugins) {
+	for (size_t i = 0; i < theRequestedPlugins.size(); i++)
+	{
+		std::string theName = theRequestedPlugins[i];
+
 		theLogger->Info("Calculating " + theName);
 
 		std::shared_ptr<plugin::compiled_plugin> thePlugin = std::dynamic_pointer_cast<plugin::compiled_plugin > (plugin_factory::Instance()->Plugin(theName));
 
-		if (!thePlugin) {
+		if (!thePlugin)
+		{
 			theLogger->Error("Unable to declare plugin " + theName);
 			continue;
 		}
@@ -78,4 +87,15 @@ int main(int argc, char ** argv) {
 	}
 
 	return 0;
+}
+
+void banner()
+{
+	std::cout << std::endl
+	          << "************************************************" << std::endl
+	          << "* By the Power of Grayskull, I Have the Power! *" << std::endl
+	          << "************************************************" << std::endl << std::endl;
+
+	sleep(1);
+
 }
