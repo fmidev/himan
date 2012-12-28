9460859911a1266842a098df098ce2a6202e66eb
Mikko Partio <mikko.partio@fmi.fi>
2012-12-28 08:40:30 +0000

    Initial import

---
+++ himan-lib/include/producer.h
@@ -0,0 +1,68 @@
+/*
+ * producer.h
+ *
+ *  Created on: Dec 28, 2012
+ *      Author: partio
+ */
+
+#ifndef PRODUCER_H
+#define PRODUCER_H
+
+#include "himan_common.h"
+
+namespace himan
+{
+
+class producer
+{
+
+	public:
+
+		producer();
+		explicit producer(long theFmiProducerId);
+		producer(long theCentre, long theProcess);
+
+		~producer() {}
+
+		std::string ClassName() const
+		{
+			return "himan::producer";
+		}
+
+		HPVersionNumber Version() const
+		{
+			return HPVersionNumber(0, 1);
+		}
+
+		std::ostream& Write(std::ostream& file) const;
+
+		void Centre(long theCentre);
+		long Centre() const;
+
+		void Process(long theProcess);
+		long Process() const;
+
+		void Id(long theId);
+		long Id() const;
+
+		void Name(const std::string& theName);
+		std::string Name() const;
+
+	private:
+
+		long itsFmiProducerId;
+		long itsProcess;
+		long itsCentre;
+		std::string itsNeonsName;
+
+};
+
+inline
+std::ostream& operator<<(std::ostream& file, producer& ob)
+{
+	return ob.Write(file);
+}
+
+} // namespace himan
+
+#endif /* PRODUCER_H */
---
+++ himan-lib/source/producer.cpp
@@ -0,0 +1,91 @@
+/*
+ * producer.cpp
+ *
+ *  Created on: Dec 28, 2012
+ *      Author: partio
+ */
+
+#include "producer.h"
+
+using namespace himan;
+
+producer::producer()
+	: itsFmiProducerId(kHPMissingInt)
+	, itsProcess(kHPMissingInt)
+	, itsCentre(kHPMissingInt)
+	, itsNeonsName("")
+{
+
+}
+
+producer::producer(long theFmiProducerId)
+	: itsFmiProducerId(theFmiProducerId)
+	, itsProcess(kHPMissingInt)
+	, itsCentre(kHPMissingInt)
+	, itsNeonsName("")
+{
+
+}
+
+producer::producer(long theCentre, long theProcess)
+	: itsFmiProducerId(kHPMissingInt)
+	, itsProcess(theProcess)
+	, itsCentre(theCentre)
+	, itsNeonsName("")
+{
+
+}
+
+void producer::Centre(long theCentre)
+{
+	itsCentre = theCentre;
+}
+
+long producer::Centre() const
+{
+	return itsCentre;
+}
+
+void producer::Process(long theProcess)
+{
+	itsProcess = theProcess;
+}
+
+long producer::Process() const
+{
+	return itsProcess;
+}
+
+void producer::Id(long theId)
+{
+	itsFmiProducerId = theId;
+}
+
+long producer::Id() const
+{
+	return itsFmiProducerId;
+}
+
+void producer::Name(const std::string& theName)
+{
+	itsNeonsName = theName;
+}
+
+std::string producer::Name() const
+{
+	return itsNeonsName;
+}
+
+std::ostream& producer::Write(std::ostream& file) const
+{
+
+	file << "<" << ClassName() << " " << Version() << ">" << std::endl;
+
+	file << "__itsFmiProducerId__ " << itsFmiProducerId << std::endl;
+	file << "__itsProcess__ " << itsProcess << std::endl;
+	file << "__itsCentre__ " << itsCentre << std::endl;
+	file << "__itsNeonsName__ " << itsNeonsName << std::endl;
+
+	return file;
+
+}
