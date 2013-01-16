c7295c198fe335193c5abdb0bbb47cab1065ca19
Mikko Aalto <mikko.aalto@fmi.fi>
2013-01-16 08:23:46 +0000

    Rename

---
+++ himan-plugins/include/seaicing.cpp
@@ -0,0 +1,73 @@
+/**
+ * @file icing.h
+ * 
+ * @brief A plugin to calculate sea spray icing.
+ *
+ * @date Jan 03, 2013
+ * 
+ * @author aaltom
+ * 
+ */
+
+#ifndef ICING_H
+#define ICING_H
+
+#include "compiled_plugin.h"
+#include "compiled_plugin_base.h"
+
+namespace himan
+{
+namespace plugin
+{
+
+class icing : public compiled_plugin, private compiled_plugin_base
+{
+	public:
+		icing();
+
+		inline virtual ~icing() {}
+
+		icing(const icing& other) = delete;
+		icing& operator=(const icing& other) = delete;
+
+		virtual void Process(std::shared_ptr<configuration> theConfiguration);
+
+		virtual std::string ClassName() const
+		{
+			return "himan::plugin::icing";
+		}
+
+		virtual HPPluginClass PluginClass() const
+		{
+			return kCompiled;
+		}
+
+		virtual HPVersionNumber Version() const
+		{
+			return HPVersionNumber(0, 1);
+		}
+
+	private:
+
+		void Run(std::shared_ptr<info> myTargetInfo, std::shared_ptr<const configuration> theConfiguration, unsigned short theThreadIndex);
+		void Calculate(std::shared_ptr<info> myTargetInfo, std::shared_ptr<const configuration> theConfiguration, unsigned short theThreadIndex);
+
+		bool itsUseCuda;
+};
+
+// the class factory
+
+#ifndef HIMAN_AUXILIARY_INCLUDE
+
+extern "C" std::shared_ptr<himan_plugin> create()
+{
+	return std::shared_ptr<icing> (new icing());
+}
+
+#endif /* HIMAN_AUXILIARY_INCLUDE */
+
+} // namespace plugin
+} // namespace himan
+
+
+#endif /* ICING_H */
---
+++ himan-plugins/source/seaicing.cpp
@@ -0,0 +1,351 @@
+/**
+ * @file icing.cpp
+ *
+ *  Created on: Jan 03, 2013
+ *  @author aaltom
+ */
+
+#include "icing.h"
+#include <iostream>
+#include "plugin_factory.h"
+#include "logger_factory.h"
+#include <boost/lexical_cast.hpp>
+#include "util.h"
+
+#define HIMAN_AUXILIARY_INCLUDE
+
+#include "fetcher.h"
+#include "writer.h"
+#include "pcuda.h"
+#include "neons.h"
+
+#undef HIMAN_AUXILIARY_INCLUDE
+
+using namespace std;
+using namespace himan::plugin;
+
+#undef HAVE_CUDA
+
+#ifdef HAVE_CUDA
+namespace himan
+{
+namespace plugin
+{
+namespace icing_cuda
+{
+void doCuda(const float* Tin, float TBase, const float* Pin, float TScale, float* TPout, size_t N, float PConst, unsigned short index);
+}
+}
+}
+#endif
+
+icing::icing() : itsUseCuda(false)
+{
+	itsClearTextFormula = "Icing = FF * ( -0.35 -T2m ) / ( 1 + 0.3 * ( T0 + 0.35 ))";
+
+	itsLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("icing"));
+
+}
+
+void icing::Process(shared_ptr<configuration> theConfiguration)
+{
+
+	shared_ptr<plugin::pcuda> c = dynamic_pointer_cast<plugin::pcuda> (plugin_factory::Instance()->Plugin("pcuda"));
+
+	if (c && c->HaveCuda())
+	{
+		string msg = "I possess the powers of CUDA ";
+
+		if (!theConfiguration->UseCuda())
+		{
+			msg += ", but I won't use them";
+		}
+		else
+		{
+			msg += ", and I'm not afraid to use them";
+			itsUseCuda = true;
+		}
+
+		itsLogger->Info(msg);
+
+	}
+
+	// Get number of threads to use
+
+	unsigned short threadCount = ThreadCount(theConfiguration->ThreadCount());
+
+	boost::thread_group g;
+
+	/*
+	 * The target information is parsed from the configuration file.
+	 */
+
+	shared_ptr<info> theTargetInfo = theConfiguration->Info();
+
+	/*
+	 * Get producer information from neons if whole_file_write is false.
+	 */
+
+	if (!theConfiguration->WholeFileWrite())
+	{
+		shared_ptr<plugin::neons> n = dynamic_pointer_cast<plugin::neons> (plugin_factory::Instance()->Plugin("neons"));
+
+		map<string,string> prodInfo = n->ProducerInfo(theTargetInfo->Producer().Id());
+
+		if (!prodInfo.empty())
+		{
+			producer prod(theTargetInfo->Producer().Id());
+
+			prod.Process(boost::lexical_cast<long> (prodInfo["process"]));
+			prod.Centre(boost::lexical_cast<long> (prodInfo["centre"]));
+			prod.Name(prodInfo["name"]);
+
+			theTargetInfo->Producer(prod);
+		}
+
+	}
+
+	/*
+	 * Set target parameter to icing
+	 * - name ICING-N
+	 * - univ_id 190
+	 * - grib2 descriptor 0'00'002
+	 *
+	 * We need to specify grib and querydata parameter information
+	 * since we don't know which one will be the output format.
+	 * (todo: we could check from theConfiguration but why bother?)
+	 *
+	 */
+
+	vector<param> theParams;
+
+	param theRequestedParam("ICEIND-N", 190);
+
+	theRequestedParam.GribDiscipline(0);
+	theRequestedParam.GribCategory(0);
+	theRequestedParam.GribParameter(2);
+
+	theParams.push_back(theRequestedParam);
+
+	theTargetInfo->Params(theParams);
+
+	/*
+	 * Create data structures.
+	 */
+
+	theTargetInfo->Create();
+
+	/*
+	 * Initialize parent class functions for dimension handling
+	 */
+
+	Dimension(theConfiguration->LeadingDimension());
+	FeederInfo(theTargetInfo->Clone());
+	FeederInfo()->Param(theRequestedParam);
+
+	/*
+	 * Each thread will have a copy of the target info.
+	 */
+
+	vector<shared_ptr<info> > theTargetInfos;
+
+	theTargetInfos.resize(threadCount);
+
+	for (size_t i = 0; i < threadCount; i++)
+	{
+
+		itsLogger->Info("Thread " + boost::lexical_cast<string> (i + 1) + " starting");
+
+		theTargetInfos[i] = theTargetInfo->Clone();
+
+		boost::thread* t = new boost::thread(&icing::Run,
+											 this,
+											 theTargetInfos[i],
+											 theConfiguration,
+											 i + 1);
+
+		g.add_thread(t);
+
+	}
+
+	g.join_all();
+
+	if (theConfiguration->WholeFileWrite())
+	{
+
+		shared_ptr<writer> theWriter = dynamic_pointer_cast <writer> (plugin_factory::Instance()->Plugin("writer"));
+
+		theTargetInfo->FirstTime();
+
+		string theOutputFile = "himan_" + theTargetInfo->Param().Name() + "_" + theTargetInfo->Time().OriginDateTime()->String("%Y%m%d%H");
+		theWriter->ToFile(theTargetInfo, theConfiguration->OutputFileType(), false, theOutputFile);
+
+	}
+}
+
+void icing::Run(shared_ptr<info> myTargetInfo, shared_ptr<const configuration> theConfiguration, unsigned short theThreadIndex)
+{
+	while (AdjustLeadingDimension(myTargetInfo))
+	{
+		Calculate(myTargetInfo, theConfiguration, theThreadIndex);
+	}
+}
+
+/*
+ * Calculate()
+ *
+ * This function does the actual calculation.
+ */
+
+void icing::Calculate(shared_ptr<info> myTargetInfo, shared_ptr<const configuration> theConfiguration, unsigned short theThreadIndex)
+{
+
+	shared_ptr<fetcher> theFetcher = dynamic_pointer_cast <fetcher> (plugin_factory::Instance()->Plugin("fetcher"));
+
+	// Required source parameters
+
+	param TParam("T-K");
+	level TgLevel(himan::kGround, 0, "GROUND");
+        param TgParam("TG-K");
+	param FfParam("FFG-MS");  // 10 meter wind
+        level FfLevel(himan::kGndLayer, 0, "GNDLAYER");
+
+	unique_ptr<logger> myThreadedLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("icingThread #" + boost::lexical_cast<string> (theThreadIndex)));
+
+	ResetNonLeadingDimension(myTargetInfo);
+
+	myTargetInfo->FirstParam();
+
+	while (AdjustNonLeadingDimension(myTargetInfo))
+	{
+
+		myThreadedLogger->Debug("Calculating time " + myTargetInfo->Time().ValidDateTime()->String("%Y%m%d%H") +
+								" level " + boost::lexical_cast<string> (myTargetInfo->Level().Value()));
+
+		myTargetInfo->Data()->Resize(theConfiguration->Ni(), theConfiguration->Nj());
+
+		shared_ptr<info> TInfo;
+		shared_ptr<info> TgInfo;
+		shared_ptr<info> FfInfo;
+
+		try
+		{
+			// Source info for T
+			TInfo = theFetcher->Fetch(theConfiguration,
+								 myTargetInfo->Time(),
+								 TgLevel,
+								 TParam);
+				
+			// Source info for Tg
+			TgInfo = theFetcher->Fetch(theConfiguration,
+								 myTargetInfo->Time(),
+								 FfLevel,
+								 TgParam);
+
+			// Source info for FF
+			FfInfo = theFetcher->Fetch(theConfiguration,
+								 myTargetInfo->Time(),
+								 TgLevel,
+								 FfParam);
+				
+		}
+		catch (HPExceptionType e)
+		{
+			//HPExceptionType t = static_cast<HPExceptionType> (e);
+
+			switch (e)
+			{
+			case kFileDataNotFound:
+				itsLogger->Info("Skipping step " + boost::lexical_cast<string> (myTargetInfo->Time().Step()) + ", level " + boost::lexical_cast<string> (myTargetInfo->Level().Value()));
+				myTargetInfo->Data()->Fill(kFloatMissing); // Fill data with missing value
+				continue;
+				break;
+
+			default:
+				throw runtime_error(ClassName() + ": Unable to proceed");
+				break;
+			}
+		}
+
+		shared_ptr<NFmiGrid> targetGrid = myTargetInfo->ToNewbaseGrid();
+		shared_ptr<NFmiGrid> TGrid = TInfo->ToNewbaseGrid();
+		shared_ptr<NFmiGrid> TgGrid = TgInfo->ToNewbaseGrid();
+		shared_ptr<NFmiGrid> FfGrid = FfInfo->ToNewbaseGrid();
+
+		int missingCount = 0;
+		int count = 0;
+
+		assert(targetGrid->Size() == myTargetInfo->Data()->Size());
+
+		bool equalGrids = (myTargetInfo->GridAndAreaEquals(TInfo) &&
+							myTargetInfo->GridAndAreaEquals(TgInfo) &&
+							myTargetInfo->GridAndAreaEquals(FfInfo));
+
+		myTargetInfo->ResetLocation();
+
+		targetGrid->Reset();
+
+		while (myTargetInfo->NextLocation() && targetGrid->Next())
+		{
+			count++;
+
+			double T = kFloatMissing;
+			double Tg = kFloatMissing;
+			double Ff = kFloatMissing;
+
+			InterpolateToPoint(targetGrid, TGrid, equalGrids, T);
+			InterpolateToPoint(targetGrid, TgGrid, equalGrids, Tg);
+			InterpolateToPoint(targetGrid, FfGrid, equalGrids, Ff);
+  
+
+			if (T == kFloatMissing || Tg == kFloatMissing || Ff == kFloatMissing)
+			{
+				missingCount++;
+
+				myTargetInfo->Value(-10);  // No missing values
+				continue;
+			}
+
+			double Icing;
+                        double TBase = 273.15;
+                        
+                        Tg = Tg - TBase;
+                        T = T - TBase;
+
+			if (Tg < -2 )
+			{                         
+				Icing = -10;
+			}
+			else
+			{
+				Icing = Ff * ( -0.35 -T ) / ( 1 + 0.3 * ( Tg + 0.35 ));
+                                
+				if (Icing > 100)
+				{
+					Icing = 100;
+				}
+			}
+
+			if (!myTargetInfo->Value(Icing))
+			{
+				throw runtime_error(ClassName() + ": Failed to set value to matrix");
+			}
+
+		}
+
+		/*
+		 * Now we are done for this level
+		 *
+		 * Clone info-instance to writer since it might change our descriptor places		 
+		 */
+
+		myThreadedLogger->Info("Missing values: " + boost::lexical_cast<string> (missingCount) + "/" + boost::lexical_cast<string> (count));
+
+		if (!theConfiguration->WholeFileWrite())
+		{
+			shared_ptr<writer> theWriter = dynamic_pointer_cast <writer> (plugin_factory::Instance()->Plugin("writer"));
+
+			theWriter->ToFile(myTargetInfo->Clone(), theConfiguration->OutputFileType(), true);
+		}
+	}
+}
