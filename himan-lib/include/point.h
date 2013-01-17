679f70b6d2a79529045808890ce23f31e80eedbf
Mikko Partio <mikko.partio@fmi.fi>
2013-01-17 07:58:56 +0000

    Initial import

---
+++ himan-lib/include/point.h
@@ -0,0 +1,136 @@
+/**
+ * @file point.h
+ *
+ * @date Jan 16, 2013
+ * @author partio
+ */
+
+#ifndef POINT_H
+#define POINT_H
+#include <ostream>
+namespace himan
+{
+
+/**
+ * @class point
+ *
+ * @brief Define simple XY point. Mimics NFmiPoint in some aspects,
+ * but functionality is re-written to avoid including newbase headers.
+ */
+
+class point
+{
+public:
+	point();
+	point(double itsX, double itsY);
+	~point() {}
+
+	point(const point& other);
+	point& operator=(const point& other);
+
+	bool operator==(const point& thePoint) const;
+	bool operator!=(const point& thePoint) const;
+
+	std::string ClassName() const
+	{
+		return "himan::point";
+	}
+
+	HPVersionNumber Version() const
+	{
+		return HPVersionNumber(0, 1);
+	}
+
+	/**
+	 * @return X coordinate value
+	 */
+
+	double X() const;
+
+	/**
+	 * @return Y coordinate value
+	 */
+
+	double Y() const;
+
+	void X(double theX);
+	void Y(double theY);
+
+    std::ostream& Write(std::ostream& file) const
+    {
+		file << "<" << ClassName() << " " << Version() << ">" << std::endl;
+		file << "__itsX__ " << itsX << std::endl;
+		file << "__itsY__ " << itsY << std::endl;
+
+		return file;
+    }
+
+
+private:
+	double itsX;
+	double itsY;
+};
+
+inline point::point() : itsX(kHPMissingFloat), itsY(kHPMissingFloat)
+{
+}
+
+inline point::point(double theX, double theY) : itsX(theX), itsY(theY)
+{
+}
+
+inline point::point(const point& other) : itsX(other.X()), itsY(other.Y())
+{
+}
+
+inline point& point::operator=(const point& other)
+{
+	itsX = other.X();
+	itsY = other.Y();
+
+	return *this;
+}
+
+inline bool point::operator==(const point& other) const
+{
+	const double kCoordinateEpsilon = 0.00001;
+
+	bool yEquals = (fabs(itsY - other.Y()) < kCoordinateEpsilon);
+	bool xEquals = (fabs(itsX - other.X()) < kCoordinateEpsilon);
+
+	return (xEquals && yEquals);
+}
+
+inline bool point::operator!=(const point& thePoint) const
+{
+	return !(*this == thePoint);
+}
+
+inline double point::X() const
+{
+	return itsX;
+}
+
+inline double point::Y() const
+{
+	return itsY;
+}
+
+inline void point::X(double theX)
+{
+	itsX = theX;
+}
+
+inline void point::Y(double theY)
+{
+	itsY = theY;
+}
+
+inline std::ostream& operator<<(std::ostream& file, point& ob)
+{
+    return ob.Write(file);
+}
+
+} // namespace himan
+
+#endif /* POINT_H */
