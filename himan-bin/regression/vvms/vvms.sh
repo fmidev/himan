fdaa6de4c1085677218662cc9b7ca45e4fcfd0af
Mikko Partio <mikko.partio@fmi.fi>
2013-01-21 12:27:17 +0000

    Add test for vvms

---
+++ himan-bin/regression/vvms/vvms.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan"
+fi
+
+rm -f himan*.grib
+
+$HIMAN -d 5 -f vvms_ec1h.ini -t grib t.grib vv.grib
+
+grib_compare result.grib himan_VV-MS_2013012100.grib
+
+python test.py result.grib himan_VV-MS_2013012100.grib
+
+if [ $? -eq 0 ];then
+  echo vvms success!
+else
+  echo vvms failed
+  exit 1
+fi
