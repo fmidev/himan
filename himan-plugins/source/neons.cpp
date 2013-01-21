2c405974dc990f972dbc772ff7d0f1cf775bd09b
Mikko Partio <mikko.partio@fmi.fi>
2013-01-21 13:32:54 +0000

    Formatting

--- himan-plugins/source/neons.cpp
+++ himan-plugins/source/neons.cpp
@@ -1,8 +1,8 @@
-/*
- * neons.cpp
+/**
+ * @file neons.cpp
  *
- *  Created on: Nov 20, 2012
- *      Author: partio
+ * @date Nov 20, 2012
+ * @author partio
  */
 
 #include "neons.h"
@@ -20,339 +20,338 @@ once_flag oflag;
 
 neons::neons() : itsInit(false), itsNeonsDB()
 {
-    itsLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("neons"));
+	itsLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("neons"));
 
-    // no lambda functions for gcc 4.4 :(
-    // call_once(oflag, [](){ NFmiNeonsDBPool::MaxWorkers(MAX_WORKERS); });
+	// no lambda functions for gcc 4.4 :(
+	// call_once(oflag, [](){ NFmiNeonsDBPool::MaxWorkers(MAX_WORKERS); });
 
-    call_once(oflag, &himan::plugin::neons::InitPool, this);
+	call_once(oflag, &himan::plugin::neons::InitPool, this);
 }
 
 void neons::InitPool()
 {
-    NFmiNeonsDBPool::Instance()->MaxWorkers(MAX_WORKERS);
+	NFmiNeonsDBPool::Instance()->MaxWorkers(MAX_WORKERS);
 
-    char* host;
+	char* host;
 
-    host = std::getenv("HOSTNAME");
+	host = std::getenv("HOSTNAME");
 
-    // TODO: a smarter way to check if we are in production server
+	// TODO: a smarter way to check if we are in production server
 
-    if (host != NULL && (string(host) == "gogol.fmi.fi" || string(host) == "tolstoi.fmi.fi"))
-    {
-        NFmiNeonsDBPool::Instance()->ExternalAuthentication(true);
-        NFmiNeonsDBPool::Instance()->ReadWriteTransaction(true);
-    }
+	if (host != NULL && (string(host) == "gogol.fmi.fi" || string(host) == "tolstoi.fmi.fi"))
+	{
+		itsLogger->Trace("Production machine '" + string(host) + "' detected, initiating connection as user bdm");
+		NFmiNeonsDBPool::Instance()->ExternalAuthentication(true);
+		NFmiNeonsDBPool::Instance()->ReadWriteTransaction(true);
+	}
+	else
+	{
+		itsLogger->Trace("Not on production machine, initiating connection as user neons_client");
+	}
 
 }
 
 void neons::Init()
 {
-    if (!itsInit)
-    {
-        try
-        {
-            itsNeonsDB = unique_ptr<NFmiNeonsDB> (NFmiNeonsDBPool::Instance()->GetConnection());
-        }
-        catch (int e)
-        {
-            itsLogger->Fatal("Failed to get connection");
-            exit(1);
-        }
+	if (!itsInit)
+	{
+		try
+		{
+			itsNeonsDB = unique_ptr<NFmiNeonsDB> (NFmiNeonsDBPool::Instance()->GetConnection());
+		}
+		catch (int e)
+		{
+			itsLogger->Fatal("Failed to get connection");
+			exit(1);
+		}
 
-        itsInit = true;
-    }
+		itsInit = true;
+	}
 }
 
 vector<string> neons::Files(const search_options& options)
 {
 
-    Init();
+	Init();
 
-    vector<string> files;
+	vector<string> files;
 
-    // const int kFMICodeTableVer = 204;
+	// const int kFMICodeTableVer = 204;
 
-    string analtime = options.configuration->Info()->OriginDateTime().String("%Y%m%d%H%M%S");
-    string levelvalue = boost::lexical_cast<string> (options.level.Value());
+	string analtime = options.configuration->Info()->OriginDateTime().String("%Y%m%d%H%M%S");
+	string levelvalue = boost::lexical_cast<string> (options.level.Value());
 
-    map<string, string> producerInfo = itsNeonsDB->GetProducerDefinition(options.configuration->SourceProducer());
+	map<string, string> producerInfo = itsNeonsDB->GetProducerDefinition(options.configuration->SourceProducer());
 
-    if (producerInfo.empty())
-    {
-        itsLogger->Warning("Producer definition not found for producer " + boost::lexical_cast<string> (options.configuration->SourceProducer()));
-        return files;
-    }
+	if (producerInfo.empty())
+	{
+		itsLogger->Warning("Producer definition not found for producer " + boost::lexical_cast<string> (options.configuration->SourceProducer()));
+		return files;
+	}
 
-    string ref_prod = producerInfo["ref_prod"];
-    string proddef = producerInfo["producer_id"];
-    string no_vers = producerInfo["no_vers"];
+	string ref_prod = producerInfo["ref_prod"];
+	string proddef = producerInfo["producer_id"];
+	string no_vers = producerInfo["no_vers"];
 
-    //string param_name = itsNeonsDB->GetGridParameterName(options.param.UnivId(), kFMICodeTableVer, boost::lexical_cast<long>(no_vers));
+	//string param_name = itsNeonsDB->GetGridParameterName(options.param.UnivId(), kFMICodeTableVer, boost::lexical_cast<long>(no_vers));
 
-    //string level_name = itsNeonsDB->GetGridLevelName(options.param.UnivId(), options.level.Type(), kFMICodeTableVer, boost::lexical_cast<long>(no_vers));
+	//string level_name = itsNeonsDB->GetGridLevelName(options.param.UnivId(), options.level.Type(), kFMICodeTableVer, boost::lexical_cast<long>(no_vers));
 
-    string level_name = options.level.Name();
+	string level_name = options.level.Name();
 
-    vector<vector<string> > gridgeoms = itsNeonsDB->GetGridGeoms(ref_prod, analtime);
+	vector<vector<string> > gridgeoms = itsNeonsDB->GetGridGeoms(ref_prod, analtime);
 
-    if (gridgeoms.size() == 0)
-    {
-        itsLogger->Warning("No data found for given search options");
-        return files;
-    }
+	if (gridgeoms.size() == 0)
+	{
+		itsLogger->Warning("No data found for given search options");
+		return files;
+	}
 
-    for (size_t i = 0; i < gridgeoms.size(); i++)
-    {
-        string tablename = gridgeoms[i][1];
-        string dset = gridgeoms[i][2];
+	for (size_t i = 0; i < gridgeoms.size(); i++)
+	{
+		string tablename = gridgeoms[i][1];
+		string dset = gridgeoms[i][2];
 
-        /// @todo GFS (or in fact codetable 2) has wrong temperature parameter defined
+		/// @todo GFS (or in fact codetable 2) has wrong temperature parameter defined
 
-        string parm_name = options.param.Name();
+		string parm_name = options.param.Name();
 
-        if (parm_name == "T-K" && no_vers == "2")
-        {
-             	parm_name = "T-C";
-        }
+		if (parm_name == "T-K" && no_vers == "2")
+		{
+			 	parm_name = "T-C";
+		}
 
-        string query = "SELECT parm_name, lvl_type, lvl1_lvl2, fcst_per, file_location, file_server "
-                       "FROM "+tablename+" "
-                       "WHERE dset_id = "+dset+" "
-                       "AND parm_name = '"+parm_name+"' "
-                       "AND lvl_type = '"+level_name+"' "
-                       "AND lvl1_lvl2 = " +levelvalue+" "
-                       "AND fcst_per = "+boost::lexical_cast<string> (options.time.Step())+" "
-                       "ORDER BY dset_id, fcst_per, lvl_type, lvl1_lvl2";
+		string query = "SELECT parm_name, lvl_type, lvl1_lvl2, fcst_per, file_location, file_server "
+					   "FROM "+tablename+" "
+					   "WHERE dset_id = "+dset+" "
+					   "AND parm_name = '"+parm_name+"' "
+					   "AND lvl_type = '"+level_name+"' "
+					   "AND lvl1_lvl2 = " +levelvalue+" "
+					   "AND fcst_per = "+boost::lexical_cast<string> (options.time.Step())+" "
+					   "ORDER BY dset_id, fcst_per, lvl_type, lvl1_lvl2";
 
-        itsNeonsDB->Query(query);
+		itsNeonsDB->Query(query);
 
-        vector<string> values = itsNeonsDB->FetchRow();
+		vector<string> values = itsNeonsDB->FetchRow();
 
-        if (values.empty())
-        {
-            continue;
-        }
+		if (values.empty())
+		{
+			continue;
+		}
 
-        files.push_back(values[4]);
+		files.push_back(values[4]);
 
-    }
+	}
 
-    return files;
+	return files;
 
 }
 
 bool neons::Save(shared_ptr<const info> resultInfo, const string& theFileName)
 {
-    Init();
+	Init();
 
-    stringstream query;
+	stringstream query;
 
-    /*
-     * 1. Get grid information
-     * 2. Get model information
-     * 3. Get data set information (ie model run)
-     * 4. Insert or update
-     */
+	/*
+	 * 1. Get grid information
+	 * 2. Get model information
+	 * 3. Get data set information (ie model run)
+	 * 4. Insert or update
+	 */
 
-    himan::point firstGridPoint = resultInfo->FirstGridPoint();
+	himan::point firstGridPoint = resultInfo->FirstGridPoint();
 
-    /*
-     * pas_latitude and pas_longitude cannot be checked programmatically
-     * since f.ex. in the case for GFS in neons we have value 500 and
-     * by calculating we have value 498. But not check these columns should
-     * not matter as long as row_cnt, col_cnt, lat_orig and lon_orig match
-     * (since pas_latitude and pas_longitude are derived from these anyway)
-     */
+	/*
+	 * pas_latitude and pas_longitude cannot be checked programmatically
+	 * since f.ex. in the case for GFS in neons we have value 500 and
+	 * by calculating we have value 498. But not check these columns should
+	 * not matter as long as row_cnt, col_cnt, lat_orig and lon_orig match
+	 * (since pas_latitude and pas_longitude are derived from these anyway)
+	 */
 
-    query 	<< "SELECT geom_name "
-            << "FROM grid_reg_geom "
-            << "WHERE row_cnt = " << resultInfo->Nj()
-            << " AND col_cnt = " << resultInfo->Ni()
-            << " AND lat_orig = " << (firstGridPoint.Y() * 1e3)
-            << " AND long_orig = " << (firstGridPoint.X() * 1e3);
+	query 	<< "SELECT geom_name "
+			<< "FROM grid_reg_geom "
+			<< "WHERE row_cnt = " << resultInfo->Nj()
+			<< " AND col_cnt = " << resultInfo->Ni()
+			<< " AND lat_orig = " << (firstGridPoint.Y() * 1e3)
+			<< " AND long_orig = " << (firstGridPoint.X() * 1e3);
 //			<< " AND pas_latitude = " << static_cast<long> (resultInfo->Dj() * 1e3)
 //			<< " AND pas_longitude = " << static_cast<long> (resultInfo->Di() * 1e3);
 
-    itsNeonsDB->Query(query.str());
+	itsNeonsDB->Query(query.str());
 
-    vector<string> row;
+	vector<string> row;
 
-    row = itsNeonsDB->FetchRow();
+	row = itsNeonsDB->FetchRow();
 
-    if (row.empty())
-    {
-        itsLogger->Warning("Grid geometry not found from neons");
-        return false;
-    }
+	if (row.empty())
+	{
+		itsLogger->Warning("Grid geometry not found from neons");
+		return false;
+	}
 
-    string geom_name = row[0];
+	string geom_name = row[0];
 
-    query.str("");
+	query.str("");
 
-    query 	<< "SELECT "
-            << "nu.model_id AS process, "
-            << "nu.ident_id AS centre, "
-            << "m.model_name, "
-            << "m.model_type, "
-            << "type_smt "
-            << "FROM "
-            << "grid_num_model_grib nu, "
-            << "grid_model m, "
-            << "grid_model_name na, "
-            << "fmi_producers f "
-            << "WHERE f.producer_id = " << resultInfo->Producer().Id()
-            << " AND m.model_type = f.ref_prod "
-            << " AND nu.model_name = m.model_name "
-            << " AND m.flag_mod = 0 "
-            << " AND nu.model_name = na.model_name "
-            << " AND m.model_name = na.model_name ";
+	query 	<< "SELECT "
+			<< "nu.model_id AS process, "
+			<< "nu.ident_id AS centre, "
+			<< "m.model_name, "
+			<< "m.model_type, "
+			<< "type_smt "
+			<< "FROM "
+			<< "grid_num_model_grib nu, "
+			<< "grid_model m, "
+			<< "grid_model_name na, "
+			<< "fmi_producers f "
+			<< "WHERE f.producer_id = " << resultInfo->Producer().Id()
+			<< " AND m.model_type = f.ref_prod "
+			<< " AND nu.model_name = m.model_name "
+			<< " AND m.flag_mod = 0 "
+			<< " AND nu.model_name = na.model_name "
+			<< " AND m.model_name = na.model_name ";
 
-    itsNeonsDB->Query(query.str());
+	itsNeonsDB->Query(query.str());
 
-    row = itsNeonsDB->FetchRow();
+	row = itsNeonsDB->FetchRow();
 
-    if (row.empty())
-    {
-        itsLogger->Warning("Producer definition not found from neons (id: " + boost::lexical_cast<string> (resultInfo->Producer().Id()) + ")");
-        return false;
-    }
+	if (row.empty())
+	{
+		itsLogger->Warning("Producer definition not found from neons (id: " + boost::lexical_cast<string> (resultInfo->Producer().Id()) + ")");
+		return false;
+	}
 
-    string process = row[0];
-    string centre = row[1];
-    string model_name = row[2];
-    string model_type = row[3];
+	string process = row[0];
+	string centre = row[1];
+	string model_name = row[2];
+	string model_type = row[3];
 
-    /*
-    	query 	<< "SELECT "
-    	        << "m.model_name, "
-    	        << "model_type, "
-    	        << "type_smt "
-    	        << "FROM grid_num_model_grib nu, "
-    	        << "grid_model m, "
-    	        << "grid_model_name na "
-    	        << "WHERE nu.model_id = " << info.process
-    	        << " AND nu.ident_id = " << info.centre
-    	        << " AND m.flag_mod = 0 "
-    	        << " AND nu.model_name = na.model_name "
-    	        << " AND m.model_name = na.model_name";
+	/*
+		query 	<< "SELECT "
+				<< "m.model_name, "
+				<< "model_type, "
+				<< "type_smt "
+				<< "FROM grid_num_model_grib nu, "
+				<< "grid_model m, "
+				<< "grid_model_name na "
+				<< "WHERE nu.model_id = " << info.process
+				<< " AND nu.ident_id = " << info.centre
+				<< " AND m.flag_mod = 0 "
+				<< " AND nu.model_name = na.model_name "
+				<< " AND m.model_name = na.model_name";
 
-    */
+	*/
 
-    query.str("");
+	query.str("");
 
-    query	<< "SELECT "
-            << "dset_id, "
-            << "table_name, "
-            << "rec_cnt_dset "
-            << "FROM as_grid "
-            << "WHERE "
-            << "model_type = '" << model_type << "'"
-            << " AND geom_name = '" << geom_name << "'"
-            << " AND dset_name = 'AF'"
-            << " AND base_date = '" << resultInfo->OriginDateTime().String("%Y%m%d%H%M") << "'";
+	query	<< "SELECT "
+			<< "dset_id, "
+			<< "table_name, "
+			<< "rec_cnt_dset "
+			<< "FROM as_grid "
+			<< "WHERE "
+			<< "model_type = '" << model_type << "'"
+			<< " AND geom_name = '" << geom_name << "'"
+			<< " AND dset_name = 'AF'"
+			<< " AND base_date = '" << resultInfo->OriginDateTime().String("%Y%m%d%H%M") << "'";
 
-    itsNeonsDB->Query(query.str());
+	itsNeonsDB->Query(query.str());
 
-    row = itsNeonsDB->FetchRow();
+	row = itsNeonsDB->FetchRow();
 
-    if (row.empty())
-    {
-        itsLogger->Warning("Data set definition not found from neons");
-        return false;
-    }
+	if (row.empty())
+	{
+		itsLogger->Warning("Data set definition not found from neons");
+		return false;
+	}
 
-    string table_name = row[1];
-    string dset_id = row[0];
+	string table_name = row[1];
+	string dset_id = row[0];
 
-    string host = "himan_test_host";
-    string eps_specifier = "0";
+	string host = "himan_test_host";
+	string eps_specifier = "0";
 
-    query.str("");
+	query.str("");
 
-    query << "LOCK TABLE as_grid IN SHARE MODE";
+	query << "UPDATE as_grid "
+		   << "SET rec_cnt_dset = "
+		   << "rec_cnt_dset + 1, "
+		   << "date_maj_dset = sysdate "
+		   << "WHERE dset_id = " << dset_id;
 
-    itsNeonsDB->Execute(query.str());
-
-    query.str("");
-
-    query << "UPDATE as_grid "
-           << "SET rec_cnt_dset = "
-           << "rec_cnt_dset + 1, "
-           << "date_maj_dset = sysdate "
-           << "WHERE dset_id = " << dset_id;
-
-    try
-    {
-    	itsNeonsDB->Execute(query.str());
-    }
-    catch (int e)
-    {
-       itsLogger->Error("Error code: " + boost::lexical_cast<string> (e));
-       itsLogger->Error("Query: " + query.str());
+	try
+	{
+		itsNeonsDB->Execute(query.str());
+	}
+	catch (int e)
+	{
+	   itsLogger->Error("Error code: " + boost::lexical_cast<string> (e));
+	   itsLogger->Error("Query: " + query.str());
    	   itsNeonsDB->Rollback();
    	   return false;
-    }
+	}
 
-    query.str("");
+	query.str("");
 
-    query  << "INSERT INTO " << table_name
-           << " (dset_id, parm_name, lvl_type, lvl1_lvl2, fcst_per, eps_specifier, file_location, file_server) "
-           << "VALUES ("
-           << dset_id << ", "
-           << "'" << resultInfo->Param().Name() << "', "
-           << "'" << resultInfo->Level().Name() << "', "
-           << resultInfo->Level().Value() << ", "
-           << resultInfo->Time().Step() << ", "
-           << "'" << eps_specifier << "', "
-           << "'" << theFileName << "', "
-           << "'" << host << "')";
+	query  << "INSERT INTO " << table_name
+		   << " (dset_id, parm_name, lvl_type, lvl1_lvl2, fcst_per, eps_specifier, file_location, file_server) "
+		   << "VALUES ("
+		   << dset_id << ", "
+		   << "'" << resultInfo->Param().Name() << "', "
+		   << "'" << resultInfo->Level().Name() << "', "
+		   << resultInfo->Level().Value() << ", "
+		   << resultInfo->Time().Step() << ", "
+		   << "'" << eps_specifier << "', "
+		   << "'" << theFileName << "', "
+		   << "'" << host << "')";
 
 
-    try
-    {
-    	itsNeonsDB->Execute(query.str());
-    	itsNeonsDB->Commit();
-    }
-    catch (int e)
-    {
-        itsLogger->Error("Error code: " + boost::lexical_cast<string> (e));
-        itsLogger->Error("Query: " + query.str());
-        itsNeonsDB->Rollback();
-        return false;
-    }
+	try
+	{
+		itsNeonsDB->Execute(query.str());
+		itsNeonsDB->Commit();
+	}
+	catch (int e)
+	{
+		itsLogger->Error("Error code: " + boost::lexical_cast<string> (e));
+		itsLogger->Error("Query: " + query.str());
+		itsNeonsDB->Rollback();
+		return false;
+	}
 
-    itsLogger->Info("Saved information on file '" + theFileName + "' to neons");
+	itsLogger->Info("Saved information on file '" + theFileName + "' to neons");
 
-    return true;
+	return true;
 }
 
 map<string,string> neons::ProducerInfo(long fmiProducerId)
 {
 
-    Init();
+	Init();
 
-    string query = "SELECT n.model_id, n.ident_id, n.model_name FROM grid_num_model_grib n "
-                   "WHERE n.model_name = (SELECT model_name from grid_model WHERE model_type = "
-                   "(SELECT ref_prod FROM fmi_producers WHERE producer_id = "
-                   + boost::lexical_cast<string> (fmiProducerId) + "))";
+	string query = "SELECT n.model_id, n.ident_id, n.model_name FROM grid_num_model_grib n "
+				   "WHERE n.model_name = (SELECT model_name from grid_model WHERE model_type = "
+				   "(SELECT ref_prod FROM fmi_producers WHERE producer_id = "
+				   + boost::lexical_cast<string> (fmiProducerId) + "))";
 
-    itsNeonsDB->Query(query);
+	itsNeonsDB->Query(query);
 
-    vector<string> row = itsNeonsDB->FetchRow();
+	vector<string> row = itsNeonsDB->FetchRow();
 
-    map<string,string> ret;
+	map<string,string> ret;
 
-    if (row.empty())
-    {
-        return ret;
-    }
+	if (row.empty())
+	{
+		return ret;
+	}
 
-    ret["process"] = row[0];
-    ret["centre"] = row[1];
-    ret["name"] = row[2];
+	ret["process"] = row[0];
+	ret["centre"] = row[1];
+	ret["name"] = row[2];
 
-    return ret;
+	return ret;
 }
 
 std::string neons::LatestTime(const producer& prod)
@@ -392,18 +391,18 @@ NFmiNeonsDB& neons::NeonsDB()
  *  \par codeTableVersion  - code table number
  */
 std::string neons::GribParameterName(const long fmiParameterId, const long codeTableVersion) 
-{    
-    Init();
-    
-    std::string paramName = itsNeonsDB->GetGridParameterName(fmiParameterId, codeTableVersion, codeTableVersion);
-    return paramName; 
-    
+{	
+	Init();
+	
+	std::string paramName = itsNeonsDB->GetGridParameterName(fmiParameterId, codeTableVersion, codeTableVersion);
+	return paramName; 
+	
 }
 
 std::string neons::GribParameterName(const long fmiParameterId, const long category, const long discipline, const long producer) 
 {
-    Init();
-    
-    std::string paramName = itsNeonsDB->GetGridParameterName(fmiParameterId, category, discipline, producer);
-    return paramName;   
+	Init();
+	
+	std::string paramName = itsNeonsDB->GetGridParameterName(fmiParameterId, category, discipline, producer);
+	return paramName;   
 }
