de06bb6da79d8726bfa36717fae66811b1629ab8
Mikko Partio <mikko.partio@fmi.fi>
2013-01-24 08:38:23 +0000

    Check that grib metadata is the same between files

--- himan-bin/regression/vvms/vvms.sh
+++ himan-bin/regression/vvms/vvms.sh
@@ -12,6 +12,13 @@ $HIMAN -d 5 -f vvms_ec1h.ini -t grib t.grib vv.grib
 
 grib_compare result.grib himan_VV-MS_2013012100.grib
 
+if [ $? -eq 0 ];then
+  echo vvms success!
+else
+  echo vvms failed
+  exit 1
+fi
+
 python test.py result.grib himan_VV-MS_2013012100.grib
 
 if [ $? -eq 0 ];then
