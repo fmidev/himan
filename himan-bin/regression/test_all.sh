8df7e091c4336b4d7041186c6002c85c7b53f2b2
Mikko Partio <mikko.partio@fmi.fi>
2013-01-24 08:38:33 +0000

    Save output to log

--- himan-bin/regression/test_all.sh
+++ himan-bin/regression/test_all.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 export HIMAN=$(pwd)/../build/debug/himan
+LOGDIR=/tmp
 
 for d in $(find . -maxdepth 1 -type d ! -name ".*" -print); do
 	echo -- testing $d
 	cd $d
 	for s in $(ls *.sh); do
-		echo running script $s
-		sh $s
+		LOGFILE=$LOGDIR/$s.log
+
+		echo running script $s, log at \'$LOGFILE\'
+		sh $s > $LOGFILE 2>&1
+
+		if [ $? -ne 0 ]; then
+			echo test failed
+		else
+			echo test success
+		fi
 	done
 	cd ..
 		
