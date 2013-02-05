a159f82bf67fc9f7e28124a1e6100414c765ac89
Mikko Partio <mikko.partio@fmi.fi>
2013-02-05 06:34:43 +0000

    Moving cmd line parsing from json_parser

--- himan-bin/source/himan.cpp
+++ himan-bin/source/himan.cpp
@@ -17,17 +17,32 @@
 #include "logger_factory.h"
 #include <vector>
 #include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
 
 #ifdef DEBUG
 #include "timer_factory.h"
 #endif
 
+#define HIMAN_AUXILIARY_INCLUDE
+#include "pcuda.h"
+#undef HIMAN_AUXILIARY_INCLUDE
+
+#define CUDA_NOT_YET_AT_HIMAN_BIN
+
 using namespace himan;
+using namespace std;
 
 void banner();
+std::shared_ptr<configuration> ParseCommandLine(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
+
+	std::unique_ptr<logger> theLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("himan"));
+
+	theLogger->Trace("Parsing command line");
+
+	shared_ptr<configuration> conf = ParseCommandLine(argc, argv);
 
 	/*
 	 * Initialize plugin factory before parsing configuration file. This prevents himan from
@@ -41,11 +56,9 @@ int main(int argc, char** argv)
 
 	std::shared_ptr<plugin::auxiliary_plugin> n = std::dynamic_pointer_cast<plugin::auxiliary_plugin> (plugin_factory::Instance()->Plugin("neons"));
  
-	std::shared_ptr<configuration> theConfiguration = json_parser::Instance()->Parse(argc, argv);
+	json_parser::Instance()->Parse(conf);
 
 	banner();
-
-	std::unique_ptr<logger> theLogger = std::unique_ptr<logger> (logger_factory::Instance()->GetLog("himan"));
 
 	std::vector<std::shared_ptr<plugin::himan_plugin>> thePlugins = plugin_factory::Instance()->Plugins();
 
@@ -78,7 +91,7 @@ int main(int argc, char** argv)
 
 	theLogger->Debug("Requested plugin(s):");
 
-	std::vector<std::string> theRequestedPlugins = theConfiguration->Plugins();
+	std::vector<std::string> theRequestedPlugins = conf->Plugins();
 
 	for (size_t i = 0; i < theRequestedPlugins.size(); i++)
 	{
@@ -104,7 +117,7 @@ int main(int argc, char** argv)
 		t->Start();
 #endif
 
-		thePlugin->Process(theConfiguration);
+		thePlugin->Process(conf);
 
 #ifdef DEBUG
 		t->Stop();
@@ -124,4 +137,184 @@ void banner()
 
 	sleep(1);
 
+}
+
+std::shared_ptr<configuration> ParseCommandLine(int argc, char** argv)
+{
+
+	std::shared_ptr<configuration> conf(new configuration());
+
+	namespace po = boost::program_options;
+
+	po::options_description desc("Allowed options");
+
+	// Can't use required() since it doesn't allow us to use --list-plugins
+
+	std::string outfileType = "";
+	std::string confFile = "";
+	std::vector<std::string> auxFiles;
+	
+	himan::HPDebugState debugState = himan::kDebugMsg;
+
+	int logLevel = 0;
+	short int threadCount = -1;
+
+	desc.add_options()
+	("help,h", "print out help message")
+	("type,t", po::value(&outfileType), "output file type, one of: grib, grib2, netcdf, querydata")
+	("version,v", "display version number")
+	("configuration-file,f", po::value(&confFile), "configuration file")
+	("auxiliary-files,a", po::value<std::vector<std::string> > (&auxFiles), "auxiliary (helper) file(s)")
+	("threads,j", po::value(&threadCount), "number of started threads")
+	("list-plugins,l", "list all defined plugins")
+	("debug-level,d", po::value(&logLevel), "set log level: 0(fatal) 1(error) 2(warning) 3(info) 4(debug) 5(trace)")
+	("no-cuda", "disable cuda extensions")
+	("cuda-properties", "print cuda device properties of platform (if any)")
+	;
+
+	po::positional_options_description p;
+	p.add("auxiliary-files", -1);
+
+	po::variables_map opt;
+	po::store(po::command_line_parser(argc, argv)
+			  .options(desc)
+			  .positional(p)
+			  .run(),
+			  opt);
+
+	po::notify(opt);
+
+	if (threadCount)
+	{
+		conf->ThreadCount(threadCount);
+	}
+
+	if (!confFile.empty())
+	{
+		conf->ConfigurationFile(confFile);
+	}
+
+	if (auxFiles.size())
+	{
+		conf->AuxiliaryFiles(auxFiles);
+	}
+	
+	if (logLevel)
+	{
+		switch (logLevel)
+		{
+		case 0:
+			debugState = kFatalMsg;
+			break;
+		case 1:
+			debugState = kErrorMsg;
+			break;
+		case 2:
+			debugState = kWarningMsg;
+			break;
+		case 3:
+			debugState = kInfoMsg;
+			break;
+		case 4:
+			debugState = kDebugMsg;
+			break;
+		case 5:
+			debugState = kTraceMsg;
+			break;
+		}
+
+	}
+
+	logger_factory::Instance()->DebugState(debugState);
+
+	if (opt.count("version"))
+	{
+		std::cout << "himan version ???" << endl;
+		exit(1);
+	}
+
+	if (opt.count("cuda-properties"))
+	{
+#ifdef CUDA_NOT_YET_AT_HIMAN_BIN
+		std::cout << "Option --cuda-properties not yet implemented" << std::endl;
+#else
+		shared_ptr<plugin::pcuda> p = dynamic_pointer_cast<plugin::pcuda> (plugin_factory::Instance()->Plugin("pcuda"));
+		p->Capabilities();
+#endif
+		exit(1);
+	}
+
+	if (opt.count("no-cuda"))
+	{
+		conf->UseCuda(false);
+	}
+
+	if (!outfileType.empty())
+	{
+		if (outfileType == "grib")
+		{
+			conf->OutputFileType(kGRIB1);
+		}
+		else if (outfileType == "grib2")
+		{
+			conf->OutputFileType(kGRIB2);
+		}
+		else if (outfileType == "netcdf")
+		{
+			conf->OutputFileType(kNetCDF);
+		}
+		else if (outfileType == "querydata")
+		{
+			conf->OutputFileType(kQueryData);
+		}
+		else
+		{
+			throw runtime_error("Invalid file type: " + outfileType);
+		}
+	}
+
+	if (opt.count("help"))
+	{
+		cout << "usage: himan [ options ]" << endl;
+		cout << desc;
+		cout << endl << "Examples:" << endl;
+		cout << "  himan -f etc/tpot.json" << endl;
+		cout << "  himan -f etc/vvmms.json -a file.grib -t querydata" << endl << endl;
+		exit(1);
+	}
+
+	if (opt.count("list-plugins"))
+	{
+
+		vector<shared_ptr<plugin::himan_plugin> > thePlugins = plugin_factory::Instance()->CompiledPlugins();
+
+		cout << "Compiled plugins" << endl;
+
+		for (size_t i = 0; i < thePlugins.size(); i++)
+		{
+			cout << "\t" << thePlugins[i]->ClassName() << "\t(version " << thePlugins[i]->Version() << ")" << endl;
+		}
+
+		thePlugins = plugin_factory::Instance()->InterpretedPlugins();
+
+		cout << "Interpreted plugins" << endl;
+
+		for (size_t i = 0; i < thePlugins.size(); i++)
+		{
+			cout << "\t" << thePlugins[i]->ClassName() << "\t(version " << thePlugins[i]->Version() << ")" << endl;
+		}
+
+		cout << "Auxiliary plugins" << endl;
+
+		thePlugins = plugin_factory::Instance()->AuxiliaryPlugins();
+
+		for (size_t i = 0; i < thePlugins.size(); i++)
+		{
+			cout << "\t" << thePlugins[i]->ClassName() << "\t(version " << thePlugins[i]->Version() << ")" << endl;
+		}
+
+		exit(1);
+	}
+
+	return conf;
 }
