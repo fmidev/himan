c4798f85a249ccd2e4d08c774f9ad36d3b14ae1f
Mikko Partio <mikko.partio@fmi.fi>
2013-02-05 06:52:20 +0000

    Move function Capabilities() inline

--- himan-plugins/include/pcuda.h
+++ himan-plugins/include/pcuda.h
@@ -12,6 +12,10 @@
 #include "auxiliary_plugin.h"
 #include "himan_common.h"
 
+#ifdef HAVE_CUDA
+#include <cuda_runtime_api.h>
+#endif
+
 namespace himan
 {
 namespace plugin
@@ -41,13 +45,65 @@ public:
 
     bool HaveCuda() const;
 
+    void Capabilities() const;
+
 #ifdef HAVE_CUDA
     int LibraryVersion() const;
     int DeviceCount() const;
     HPVersionNumber ComputeCapability() const;
-    void Capabilities() const;
 #endif
 };
+
+inline void pcuda::Capabilities() const
+{
+#ifdef HAVE_CUDA
+    using std::cout;
+    using std::endl;
+
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+
+    cout << "There are " << devCount << " CUDA device(s)" << endl;
+
+    // Iterate through devices
+    for (int i = 0; i < devCount; ++i)
+    {
+        // Get device properties
+        cout << "CUDA Device #" << i << endl;
+
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+
+        cout << "Major revision number:         " << devProp.major << endl
+             << "Minor revision number:         " << devProp.minor << endl
+             << "Name:                          " << devProp.name << endl
+             << "Total global memory:           " << devProp.totalGlobalMem << endl
+             << "Total shared memory per block: " << devProp.sharedMemPerBlock << endl
+             << "Total registers per block:     " << devProp.regsPerBlock << endl
+             << "Warp size:                     " << devProp.warpSize << endl
+             << "Maximum memory pitch:          " << devProp.memPitch << endl
+             << "Maximum threads per block:     " << devProp.maxThreadsPerBlock << endl;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            cout << "Maximum dimension " << i << " of block:  " << devProp.maxThreadsDim[i] << endl;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            cout << "Maximum dimension " << i << " of grid:   " << devProp.maxGridSize[i] << endl;
+        }
+
+        cout << "Clock rate:                    " << devProp.clockRate << endl
+             << "Total constant memory:         " << devProp.totalConstMem << endl
+             << "Texture alignment:             " << devProp.textureAlignment << endl
+             << "Concurrent copy and execution: " << (devProp.deviceOverlap ? "Yes" : "No") << endl
+             << "Number of multiprocessors:     " << devProp.multiProcessorCount << endl
+             << "Kernel execution timeout:      " << (devProp.kernelExecTimeoutEnabled ? "Yes" : "No") << endl;
+
+    }
+#endif
+}
 
 #ifndef HIMAN_AUXILIARY_INCLUDE
 
