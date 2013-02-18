cf9b3c995b8e7c51110c25ca3e91f76647c068cc
Mikko Partio <mikko.partio@fmi.fi>
2013-02-18 10:18:56 +0000

    Function moved to header

--- himan-plugins/source/pcuda.cpp
+++ himan-plugins/source/pcuda.cpp
@@ -34,15 +34,6 @@ bool pcuda::HaveCuda() const
     return !(DeviceCount() == 0);
 }
 
-int pcuda::DeviceCount() const
-{
-    // todo: error checking
-    int devCount;
-    cudaGetDeviceCount(&devCount);
-
-    return devCount;
-}
-
 himan::HPVersionNumber pcuda::ComputeCapability() const
 {
 	// todo: error checking
@@ -52,31 +43,5 @@ himan::HPVersionNumber pcuda::ComputeCapability() const
 
 	return HPVersionNumber(boost::lexical_cast<unsigned short> (devProp.major), boost::lexical_cast<unsigned short> (devProp.minor));
 }
-
-/*
-  There are 1 CUDA devices
-CUDA Device #0
-Major revision number:         2
-Minor revision number:         1
-Name:                          NVS 4200M
-Total global memory:           536018944
-Total shared memory per block: 49152
-Total registers per block:     32768
-Warp size:                     32
-Maximum memory pitch:          2147483647
-Maximum threads per block:     1024
-Maximum dimension 0 of block:  1024
-Maximum dimension 1 of block:  1024
-Maximum dimension 2 of block:  64
-Maximum dimension 0 of grid:   65535
-Maximum dimension 1 of grid:   65535
-Maximum dimension 2 of grid:   65535
-Clock rate:                    1480000
-Total constant memory:         65536
-Texture alignment:             512
-Concurrent copy and execution: Yes
-Number of multiprocessors:     1
-Kernel execution timeout:      Yes
- */
 
 #endif
