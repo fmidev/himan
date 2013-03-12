2d7c07054d289b1cfbf0e40a606dac6d9eac7947
Mikko Partio <mikko.partio@fmi.fi>
2013-03-12 06:21:35 +0000

    Add interpolation to Stagger()

--- himan-lib/include/grid.h
+++ himan-lib/include/grid.h
@@ -47,19 +47,19 @@ class grid
 		grid& operator=(const grid& other) = delete;
 
 
-	    std::string ClassName() const
-	    {
-	        return "himan::grid";
-	    }
+		std::string ClassName() const
+		{
+			return "himan::grid";
+		}
 
-	    HPVersionNumber Version() const
-	    {
-	        return HPVersionNumber(0, 1);
-	    }
+		HPVersionNumber Version() const
+		{
+			return HPVersionNumber(0, 1);
+		}
 
-	    std::ostream& Write(std::ostream& file) const;
+		std::ostream& Write(std::ostream& file) const;
 
-	    /**
+		/**
 		 * @return Number of points along X axis
 		 */
 
@@ -96,145 +96,154 @@ class grid
 
 		std::shared_ptr<d_matrix_t> Data() const;
 
-	    /**
-	     * @brief Replace current data matrix with the function argument
-	     * @param d shared pointer to a data matrix
-	     */
+		/**
+		 * @brief Replace current data matrix with the function argument
+		 * @param d shared pointer to a data matrix
+		 */
 
-	    void Data(std::shared_ptr<d_matrix_t> d);
+		void Data(std::shared_ptr<d_matrix_t> d);
 
 		HPScanningMode ScanningMode() const;
-	    void ScanningMode(HPScanningMode theScanningMode);
+		void ScanningMode(HPScanningMode theScanningMode);
 
-	    /**
-	     * @return True if parameter UV components are grid relative, false if they are earth-relative.
-	     * On parameters with no UV components this has no meaning.
-	     */
+		/**
+		 * @return True if parameter UV components are grid relative, false if they are earth-relative.
+		 * On parameters with no UV components this has no meaning.
+		 */
 
-	    bool UVRelativeToGrid() const;
-	    void UVRelativeToGrid(bool theUVRelativeToGrid);
+		bool UVRelativeToGrid() const;
+		void UVRelativeToGrid(bool theUVRelativeToGrid);
 
-	    /**
-	     * @brief Set the data value pointed by the iterators with a new one
-	     * @return True if assignment was succesfull
-	     */
+		/**
+		 * @brief Set the data value pointed by the iterators with a new one
+		 * @return True if assignment was succesfull
+		 */
 
-	    bool Value(size_t locationIndex, double theValue);
+		bool Value(size_t locationIndex, double theValue);
 
-	    /**
-	     * @return Data value pointed by the iterators
-	     */
+		/**
+		 * @return Data value pointed by the iterators
+		 */
 
-	    double Value(size_t locationIndex) const;
+		double Value(size_t locationIndex) const;
 
-	    /**
-	     * @return Projection type of this info
-	     *
-	     * One info can hold only one type of projection
-	     */
+		/**
+		 * @return Projection type of this info
+		 *
+		 * One info can hold only one type of projection
+		 */
 
-	    HPProjectionType Projection() const;
-	    void Projection(HPProjectionType theProjection);
+		HPProjectionType Projection() const;
+		void Projection(HPProjectionType theProjection);
 
-	    point BottomLeft() const;
-	    point TopRight() const;
+		point BottomLeft() const;
+		point TopRight() const;
 
-	    void BottomLeft(const point& theBottomLeft);
-	    void TopRight(const point& theTopRight);
+		void BottomLeft(const point& theBottomLeft);
+		void TopRight(const point& theTopRight);
 
-	    void Orientation(double theOrientation);
-	    double Orientation() const;
+		void Orientation(double theOrientation);
+		double Orientation() const;
 
-	    point SouthPole() const;
-	    void SouthPole(const point& theSouthPole);
+		point SouthPole() const;
+		void SouthPole(const point& theSouthPole);
 
-	    /**
-	     * @brief Calculates latitude and longitude of first grid point based on the area definition and scanning mode
-	     * @return First grid point of the grid
-	     * @todo How this works with stereographic projection
-	     */
+		/**
+		 * @brief Calculates latitude and longitude of first grid point based on the area definition and scanning mode
+		 * @return First grid point of the grid
+		 * @todo How this works with stereographic projection
+		 */
 
-	    point FirstGridPoint() const;
+		point FirstGridPoint() const;
 
-	    /**
-	     * @brief Calculates latitude and longitude of last grid point based on the area definition and scanning mode
-	     * @return Last grid point of the grid
-	     * @todo How this works with stereographic projection
-	     */
+		/**
+		 * @brief Calculates latitude and longitude of last grid point based on the area definition and scanning mode
+		 * @return Last grid point of the grid
+		 * @todo How this works with stereographic projection
+		 */
 
-	    point LastGridPoint() const;
+		point LastGridPoint() const;
 
-	    /**
-	     * @brief Calculate area coordinates from first gridpoint, scanning mode, grid size and distance between two gridpoints.
-	     *
-	     * This function is the opposite of FirstGridPoint(). NOTE: scanning mode must already be set when calling this function!
-	     *
-	     * @param firstPoint Latitude and longitude of first gridpoint
-	     * @param ni Grid size in X direction
-	     * @param ny Grid size in Y direction
-	     * @param di Distance between two points in X direction
-	     * @param dj Distance between two points in Y direction
-	     *
-	     * @return True if calculation is successful
-	     */
+		/**
+		 * @brief Calculate area coordinates from first gridpoint, scanning mode, grid size and distance between two gridpoints.
+		 *
+		 * This function is the opposite of FirstGridPoint(). NOTE: scanning mode must already be set when calling this function!
+		 *
+		 * @param firstPoint Latitude and longitude of first gridpoint
+		 * @param ni Grid size in X direction
+		 * @param ny Grid size in Y direction
+		 * @param di Distance between two points in X direction
+		 * @param dj Distance between two points in Y direction
+		 *
+		 * @return True if calculation is successful
+		 */
 
-	    bool SetCoordinatesFromFirstGridPoint(const point& firstPoint, size_t ni, size_t nj, double di, double dj);
-
-
-	    /**
-	     * @brief Create a newbase grid (NFmiGrid) from current data
-	     * @return Raw pointer to NFmiGrid
-	     */
-
-	    NFmiGrid* ToNewbaseGrid() const;
+		bool SetCoordinatesFromFirstGridPoint(const point& firstPoint, size_t ni, size_t nj, double di, double dj);
 
 
-	    /**
-	     * @brief Check if grid and area are equal
-	     */
+		/**
+		 * @brief Create a newbase grid (NFmiGrid) from current data
+		 * @return Raw pointer to NFmiGrid
+		 */
 
-	    bool operator==(const grid& other) const;
-	    bool operator!=(const grid& other) const;
+		NFmiGrid* ToNewbaseGrid() const;
 
 
-	    /**
-	     * @brief Stagger a grid
-	     *
-	     * Note: works only for latlon grids.
-	     *
-	     * @param xStaggerFactor Stagger factor in x direction
-	     * @param yStaggerFactor Stagger factor in y direction
-	     */
+		/**
+		 * @brief Check if grid and area are equal
+		 */
 
-	    bool Stagger(double xStaggerFactor, double yStaggerFactor);
+		bool operator==(const grid& other) const;
+		bool operator!=(const grid& other) const;
+
+
+		/**
+		 * @brief Stagger a grid
+		 *
+		 * Staggering is done to x- or y-direction or both, possible values are -0.5, 0 and 0.5 (lengths between grid points).
+		 * When a grid is staggered its area is changed accordingly and all values are linearly interpolated. In other words
+		 * this is just re-gridding but since the interpolation is simple it is efficient to do it this way.
+		 *
+		 * Function is copied from hil_pp to where it's copied from hirlam.
+		 *
+		 * This version of function has not been throughly tested; there might be some problems. Especially the handling of
+		 * outliers can cause problems, they are not extrapolated but copied as-is?
+		 * 
+		 * Note: works only for latlon grids.
+		 *
+		 * @param xStaggerFactor Stagger factor in x direction
+		 * @param yStaggerFactor Stagger factor in y direction
+		 */
+
+		bool Stagger(double xStaggerFactor, double yStaggerFactor);
 
 	private:
 
 		std::shared_ptr<d_matrix_t> itsData;
 
-	    HPScanningMode itsScanningMode; //<! When data is read from files, we need to know what is the scanning mode
+		HPScanningMode itsScanningMode; //<! When data is read from files, we need to know what is the scanning mode
 
-	    bool itsUVRelativeToGrid; //<! If true, wind UV components are relative to grid north and east (ie. are not earth-relative)
+		bool itsUVRelativeToGrid; //<! If true, wind UV components are relative to grid north and east (ie. are not earth-relative)
 
-	    HPProjectionType itsProjection;
+		HPProjectionType itsProjection;
 
-	    point itsBottomLeft;
+		point itsBottomLeft;
 		point itsTopRight;
 		point itsSouthPole;
 
 		double itsOrientation;
 
-	    std::unique_ptr<logger> itsLogger;
+		std::unique_ptr<logger> itsLogger;
 
-	    mutable double itsDi;
-	    mutable double itsDj;
+		mutable double itsDi;
+		mutable double itsDj;
 
 };
 
 inline
 std::ostream& operator<<(std::ostream& file, const grid& ob)
 {
-    return ob.Write(file);
+	return ob.Write(file);
 }
 
 } // namespace himan
--- himan-lib/source/grid.cpp
+++ himan-lib/source/grid.cpp
@@ -58,12 +58,12 @@ grid::grid(const grid& other)
 
 size_t grid::Ni() const
 {
-    return itsData->SizeX();
+	return itsData->SizeX();
 }
 
 size_t grid::Nj() const
 {
-    return itsData->SizeY();
+	return itsData->SizeY();
 }
 
 void grid::Ni(size_t theNi)
@@ -117,12 +117,12 @@ std::shared_ptr<d_matrix_t> grid::Data() const
 
 HPScanningMode grid::ScanningMode() const
 {
-    return itsScanningMode;
+	return itsScanningMode;
 }
 
 void grid::ScanningMode(HPScanningMode theScanningMode)
 {
-    itsScanningMode = theScanningMode;
+	itsScanningMode = theScanningMode;
 }
 
 bool grid::UVRelativeToGrid() const
@@ -137,22 +137,22 @@ void grid::UVRelativeToGrid(bool theUVRelativeToGrid)
 
 bool grid::Value(size_t locationIndex, double theValue)
 {
-    return itsData->Set(locationIndex, theValue) ;
+	return itsData->Set(locationIndex, theValue) ;
 }
 
 double grid::Value(size_t locationIndex) const
 {
-    return itsData->At(locationIndex);
+	return itsData->At(locationIndex);
 }
 
 HPProjectionType grid::Projection() const
 {
-    return itsProjection;
+	return itsProjection;
 }
 
 void grid::Projection(HPProjectionType theProjection)
 {
-    itsProjection = theProjection;
+	itsProjection = theProjection;
 }
 
 point grid::BottomLeft() const
@@ -177,12 +177,12 @@ void grid::TopRight(const point& theTopRight)
 
 void grid::SouthPole(const point& theSouthPole)
 {
-    itsSouthPole = theSouthPole;
+	itsSouthPole = theSouthPole;
 }
 
 point grid::SouthPole() const
 {
-    return itsSouthPole;
+	return itsSouthPole;
 }
 
 point grid::FirstGridPoint() const
@@ -292,102 +292,102 @@ bool grid::SetCoordinatesFromFirstGridPoint(const point& firstPoint, size_t ni, 
 
 double grid::Orientation() const
 {
-    return itsOrientation;
+	return itsOrientation;
 }
 
 void grid::Orientation(double theOrientation)
 {
-    itsOrientation = theOrientation;
+	itsOrientation = theOrientation;
 }
 
 NFmiGrid* grid::ToNewbaseGrid() const
 {
 
-    FmiInterpolationMethod interp = kLinearly;
-    FmiDirection dir = static_cast<FmiDirection> (itsScanningMode);
+	FmiInterpolationMethod interp = kLinearly;
+	FmiDirection dir = static_cast<FmiDirection> (itsScanningMode);
 
-    NFmiArea* theArea = 0;
+	NFmiArea* theArea = 0;
 
-    // Newbase does not understand grib2 longitude coordinates
+	// Newbase does not understand grib2 longitude coordinates
 
-    double bottomLeftLongitude = itsBottomLeft.X();
-    double topRightLongitude = itsTopRight.X();
+	double bottomLeftLongitude = itsBottomLeft.X();
+	double topRightLongitude = itsTopRight.X();
 
-    if (bottomLeftLongitude > 180 || topRightLongitude > 180)
-    {
-        bottomLeftLongitude -= 180;
-        topRightLongitude -= 180;
-    }
+	if (bottomLeftLongitude > 180 || topRightLongitude > 180)
+	{
+		bottomLeftLongitude -= 180;
+		topRightLongitude -= 180;
+	}
 
-    switch (itsProjection)
-    {
-    case kLatLonProjection:
-    {
-        theArea = new NFmiLatLonArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
-                                     NFmiPoint(topRightLongitude, itsTopRight.Y()));
+	switch (itsProjection)
+	{
+	case kLatLonProjection:
+	{
+		theArea = new NFmiLatLonArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
+									 NFmiPoint(topRightLongitude, itsTopRight.Y()));
 
-        break;
-    }
+		break;
+	}
 
-    case kRotatedLatLonProjection:
-    {
-        theArea = new NFmiRotatedLatLonArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
-                                            NFmiPoint(topRightLongitude, itsTopRight.Y()),
-                                            NFmiPoint(itsSouthPole.X(), itsSouthPole.Y()),
-                                            NFmiPoint(0.,0.), // default values
-                                            NFmiPoint(1.,1.), // default values
-                                            true);
-        break;
-    }
+	case kRotatedLatLonProjection:
+	{
+		theArea = new NFmiRotatedLatLonArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
+											NFmiPoint(topRightLongitude, itsTopRight.Y()),
+											NFmiPoint(itsSouthPole.X(), itsSouthPole.Y()),
+											NFmiPoint(0.,0.), // default values
+											NFmiPoint(1.,1.), // default values
+											true);
+		break;
+	}
 
-    case kStereographicProjection:
-    {
-        theArea = new NFmiStereographicArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
-        		                            NFmiPoint(topRightLongitude, itsTopRight.Y()),
-                                            itsOrientation);
-        break;
+	case kStereographicProjection:
+	{
+		theArea = new NFmiStereographicArea(NFmiPoint(bottomLeftLongitude, itsBottomLeft.Y()),
+											NFmiPoint(topRightLongitude, itsTopRight.Y()),
+											itsOrientation);
+		break;
 
-    }
+	}
 
-    default:
-        throw std::runtime_error(ClassName() + ": No supported projection found");
-        break;
-    }
+	default:
+		throw std::runtime_error(ClassName() + ": No supported projection found");
+		break;
+	}
 
-    NFmiGrid* theGrid (new NFmiGrid(theArea, Ni(), Nj(), dir, interp));
+	NFmiGrid* theGrid (new NFmiGrid(theArea, Ni(), Nj(), dir, interp));
 
-    size_t dataSize = itsData->Size();
+	size_t dataSize = itsData->Size();
 
-    if (dataSize)   // Do we have data
-    {
+	if (dataSize)   // Do we have data
+	{
 
-        NFmiDataPool thePool;
+		NFmiDataPool thePool;
 
-        float* arr = new float[dataSize];
+		float* arr = new float[dataSize];
 
-        // convert double array to float
+		// convert double array to float
 
-        for (unsigned int i = 0; i < dataSize; i++)
-        {
-            arr[i] = static_cast<float> (itsData->At(i));
-        }
+		for (unsigned int i = 0; i < dataSize; i++)
+		{
+			arr[i] = static_cast<float> (itsData->At(i));
+		}
 
-        if (!thePool.Init(dataSize, arr))
-        {
-            throw std::runtime_error("DataPool init failed");
-        }
+		if (!thePool.Init(dataSize, arr))
+		{
+			throw std::runtime_error("DataPool init failed");
+		}
 
-        if (!theGrid->Init(&thePool))
-        {
-            throw std::runtime_error("Grid data init failed");
-        }
+		if (!theGrid->Init(&thePool))
+		{
+			throw std::runtime_error("Grid data init failed");
+		}
 
-        delete [] arr;
-    }
+		delete [] arr;
+	}
 
-    delete theArea;
+	delete theArea;
 
-    return theGrid;
+	return theGrid;
 
 }
 
@@ -400,52 +400,52 @@ bool grid::operator==(const grid& other) const
 		return false;
 	}
 
-    if (itsBottomLeft != other.BottomLeft())
-    {
-        itsLogger->Trace("BottomLeft does not match: X " + boost::lexical_cast<std::string> (itsBottomLeft.X()) + " vs " + boost::lexical_cast<std::string> (other.BottomLeft().X()));
-        itsLogger->Trace("BottomLeft does not match: Y " + boost::lexical_cast<std::string> (itsBottomLeft.Y()) + " vs " + boost::lexical_cast<std::string> (other.BottomLeft().Y()));
-        return false;
-    }
+	if (itsBottomLeft != other.BottomLeft())
+	{
+		itsLogger->Trace("BottomLeft does not match: X " + boost::lexical_cast<std::string> (itsBottomLeft.X()) + " vs " + boost::lexical_cast<std::string> (other.BottomLeft().X()));
+		itsLogger->Trace("BottomLeft does not match: Y " + boost::lexical_cast<std::string> (itsBottomLeft.Y()) + " vs " + boost::lexical_cast<std::string> (other.BottomLeft().Y()));
+		return false;
+	}
 
    	if (itsTopRight != other.TopRight())
 	{
 		itsLogger->Trace("TopRight does not match: X " + boost::lexical_cast<std::string> (itsTopRight.X()) + " vs " + boost::lexical_cast<std::string> (other.TopRight().X()));
 		itsLogger->Trace("TopRight does not match: Y " + boost::lexical_cast<std::string> (itsTopRight.Y()) + " vs " + boost::lexical_cast<std::string> (other.TopRight().Y()));
 		return false;
-    }
+	}
 
-    if (itsProjection == kRotatedLatLonProjection)
-    {
+	if (itsProjection == kRotatedLatLonProjection)
+	{
 		if (itsSouthPole != other.SouthPole())
-    	{
-        	itsLogger->Trace("SouthPole does not match: X " + boost::lexical_cast<std::string> (itsSouthPole.X()) + " vs " + boost::lexical_cast<std::string> (other.SouthPole().X()));
-        	itsLogger->Trace("SouthPole does not match: Y " + boost::lexical_cast<std::string> (itsSouthPole.Y()) + " vs " + boost::lexical_cast<std::string> (other.SouthPole().Y()));
-        	return false;
-    	}
-    }
+		{
+			itsLogger->Trace("SouthPole does not match: X " + boost::lexical_cast<std::string> (itsSouthPole.X()) + " vs " + boost::lexical_cast<std::string> (other.SouthPole().X()));
+			itsLogger->Trace("SouthPole does not match: Y " + boost::lexical_cast<std::string> (itsSouthPole.Y()) + " vs " + boost::lexical_cast<std::string> (other.SouthPole().Y()));
+			return false;
+		}
+	}
 
-    if (itsProjection == kStereographicProjection)
-    {
-    	if (itsOrientation != other.Orientation())
-    	{
-    		itsLogger->Trace("Orientations don't match: " + boost::lexical_cast<std::string> (itsOrientation) + " vs " + boost::lexical_cast<std::string> (other.Orientation()));
-    		return false;
-    	}
-    }
+	if (itsProjection == kStereographicProjection)
+	{
+		if (itsOrientation != other.Orientation())
+		{
+			itsLogger->Trace("Orientations don't match: " + boost::lexical_cast<std::string> (itsOrientation) + " vs " + boost::lexical_cast<std::string> (other.Orientation()));
+			return false;
+		}
+	}
 
-    if (Ni() != other.Ni())
-    {
-    	itsLogger->Trace("Grid X-counts don't match: " + boost::lexical_cast<std::string> (Ni()) + " vs " + boost::lexical_cast<std::string> (other.Ni()));
-        return false;
-    }
+	if (Ni() != other.Ni())
+	{
+		itsLogger->Trace("Grid X-counts don't match: " + boost::lexical_cast<std::string> (Ni()) + " vs " + boost::lexical_cast<std::string> (other.Ni()));
+		return false;
+	}
 
-    if (Nj() != other.Nj())
-    {
-    	itsLogger->Trace("Grid Y-counts don't match: " + boost::lexical_cast<std::string> (Nj()) + " vs " + boost::lexical_cast<std::string> (other.Nj()));
-        return false;
-    }
+	if (Nj() != other.Nj())
+	{
+		itsLogger->Trace("Grid Y-counts don't match: " + boost::lexical_cast<std::string> (Nj()) + " vs " + boost::lexical_cast<std::string> (other.Nj()));
+		return false;
+	}
 
-    return true;
+	return true;
 
 }
 
@@ -479,6 +479,177 @@ bool grid::Stagger(double xStaggerFactor, double yStaggerFactor)
 
 	itsBottomLeft = bottomLeft;
 	itsTopRight = topRight;
+
+	assert(itsData->Size() > 0);
+
+	auto newData = std::make_shared<d_matrix_t> (*itsData);
+
+	size_t sizeX = itsData->SizeX(), sizeY = itsData->SizeY();
+
+	const size_t ZI = 1; // Z dimension is always missing here
+	
+	if (xStaggerFactor == -0.5)
+	{
+
+		if (yStaggerFactor == -0.5)
+		{
+			newData->Set(0, 0, ZI, itsData->At(0, 0));
+
+			for (size_t xi = 1; xi < sizeX; xi++)
+			{
+				newData->Set(xi, 0, ZI, 0.5 * (itsData->At(xi-1, 0) + itsData->At(xi, 0)));
+						
+				for (size_t yi = 1; yi < sizeY; yi++)
+				{
+					newData->Set(0, yi, ZI, 0.5 * (itsData->At(0, yi-1) + itsData->At(0, yi)));
+
+					double val = 0.25 * (	itsData->At(xi-1, yi-1) +
+											itsData->At(xi, yi -1) +
+											itsData->At(xi-1, yi) +
+											itsData->At(xi, yi)	);
+					
+					newData->Set(xi, yi, ZI, val);
+				}
+			}
+
+		}
+		else if (yStaggerFactor == 0)
+		{
+			for (size_t yi = 0; yi < sizeY; yi++)
+			{
+				newData->Set(0, yi, ZI, itsData->At(0, yi));
+
+				for (size_t xi = 1; xi < sizeX; xi++)
+				{
+					newData->Set(xi, yi, ZI, 0.5 * (itsData->At(xi-1, yi) + itsData->At(xi, yi)));
+				}
+			}
+
+		}
+		else if (yStaggerFactor == 0.5)
+		{
+			newData->Set(0, sizeY-1, ZI, itsData->At(0, sizeY-1));
+
+			for (size_t xi = 1; xi < sizeX; xi++)
+			{
+				newData->Set(xi, sizeY-1, ZI, 0.5 * (itsData->At(xi-1, sizeY-1) + itsData->At(xi, sizeY-1)));
+
+				for (size_t yi = 0; yi < sizeY-1; yi++)
+				{
+					newData->Set(0, yi, ZI, 0.5 * (itsData->At(0, yi) + itsData->At(0, yi+1)));
+
+					double val = 0.25 * (	itsData->At(xi-1, yi) +
+											itsData->At(xi, yi) +
+											itsData->At(xi-1, yi+1) +
+											itsData->At(xi, yi+1)	);
+
+					newData->Set(xi, yi, ZI, val);
+				}
+			}
+		}
+		else
+		{
+			throw std::runtime_error(ClassName() + ": Invalid y stagger value: " + boost::lexical_cast<std::string> (yStaggerFactor));
+		}
+	}
+	else if (xStaggerFactor == 0)
+	{
+		if (yStaggerFactor == -0.5)
+		{
+			for (size_t xi = 0; xi < sizeX; xi++)
+			{
+				newData->Set(xi, 0, ZI, itsData->At(xi, 0));
+
+				for (size_t yi = 1; yi < sizeY; yi++)
+				{
+					newData->Set(xi, yi, ZI, 0.5 * (itsData->At(xi, yi-1) + itsData->At(xi, yi)));
+				}
+			}
+		}
+		else if (yStaggerFactor == 0.5)
+		{
+			for (size_t xi = 0; xi < sizeX; xi++)
+			{
+				newData->Set(xi, sizeY-1, ZI, itsData->At(xi, sizeY-1));
+
+				for (size_t yi = 0; yi < sizeY-1; yi++)
+				{
+					newData->Set(xi, yi, ZI, 0.5 * (itsData->At(xi, yi) + itsData->At(xi, yi+1)));
+				}
+			}
+		}
+		else
+		{
+			throw std::runtime_error(ClassName() + ": Invalid y stagger value: " + boost::lexical_cast<std::string> (yStaggerFactor));
+		}
+	}
+	else if (xStaggerFactor == 0.5)
+	{
+		if (yStaggerFactor == -0.5)
+		{
+			newData->Set(sizeX-1, 0, ZI, itsData->At(sizeX-1, 0));
+
+			for (size_t xi = 0; xi < sizeX-1; xi++)
+			{
+				newData->Set(xi, 0, ZI, 0.5 * (itsData->At(xi,0) + itsData->At(xi+1, 0)));
+
+				for (size_t yi = 1; yi < sizeY; yi++)
+				{
+					newData->Set(sizeX-1, yi, ZI, 0.5 * (itsData->At(sizeX-1, yi-1) + itsData->At(sizeX-1, yi)));
+
+					double val = 0.25 * (	itsData->At(xi, yi-1) +
+											itsData->At(xi+1, yi-1) +
+											itsData->At(xi, yi) +
+											itsData->At(xi+1, yi)	);
+
+					newData->Set(xi, yi, ZI, val);
+				}
+			}
+		}
+		if (yStaggerFactor == 0)
+		{
+			for (size_t yi = 0; yi < sizeY; yi++)
+			{
+				newData->Set(sizeX-1, yi, 1, itsData->At(sizeX-1, yi));
+		
+				for (size_t xi = 0; xi < sizeX-1; xi++)
+				{
+					newData->Set(xi, yi, ZI, 0.5 * (itsData->At(xi, yi) + itsData->At(xi+1, yi)));
+				}
+			}
+		}
+		else if (yStaggerFactor == 0.5)
+		{
+			newData->Set(sizeX-1, sizeY-1, ZI, itsData->At(sizeX-1, sizeY-1));
+
+			for (size_t xi = 0; xi < sizeX-1; xi++)
+			{
+				newData->Set(xi, sizeY-1, ZI, 0.5 * (itsData->At(xi,sizeY-1) + itsData->At(xi+1, sizeY-1)));
+
+				for (size_t yi = 0; yi < sizeY-1; yi++)
+				{
+					newData->Set(sizeX-1, yi, ZI, 0.5 * (itsData->At(sizeX-1, yi) + itsData->At(sizeX-1, yi+1)));
+
+					double val = 0.25 * (	itsData->At(xi, yi) +
+											itsData->At(xi+1, yi) +
+											itsData->At(xi, yi+1) +
+											itsData->At(xi+1, yi+1)	);
+
+					newData->Set(xi, yi, ZI, val);
+				}
+			}
+		}
+		else
+		{
+			throw std::runtime_error(ClassName() + ": Invalid y stagger value: " + boost::lexical_cast<std::string> (yStaggerFactor));
+		}
+	}
+	else
+	{
+		throw std::runtime_error(ClassName() + ": Invalid x stagger value: " + boost::lexical_cast<std::string> (xStaggerFactor));
+	}
+
+	itsData = newData;
 
 	return true;
 }
