92b8e1b0b9184ba549e7812bdbe9ebf11fdc42bb
Mikko Partio <mikko.partio@fmi.fi>
2013-03-20 06:23:24 +0000

    Whitespaces

--- himan-plugins/include/grib.h
+++ himan-plugins/include/grib.h
@@ -24,57 +24,57 @@ class grib : public auxiliary_plugin
 
 public:
 
-    grib();
+	grib();
 
-    virtual ~grib() {}
+	virtual ~grib() {}
 
-    grib(const grib& other) = delete;
-    grib& operator=(const grib& other) = delete;
+	grib(const grib& other) = delete;
+	grib& operator=(const grib& other) = delete;
 
-    virtual std::string ClassName() const
-    {
-        return "himan::plugin::grib";
-    };
+	virtual std::string ClassName() const
+	{
+		return "himan::plugin::grib";
+	};
 
-    virtual HPPluginClass PluginClass() const
-    {
-        return kAuxiliary;
-    };
+	virtual HPPluginClass PluginClass() const
+	{
+		return kAuxiliary;
+	};
 
-    virtual HPVersionNumber Version() const
-    {
-        return HPVersionNumber(0, 1);
-    }
+	virtual HPVersionNumber Version() const
+	{
+		return HPVersionNumber(0, 1);
+	}
 
-    std::shared_ptr<NFmiGrib> Reader();
+	std::shared_ptr<NFmiGrib> Reader();
 
-    /**
-     * @brief Return all data from a grib file.
-     *
-     * This function reads a grib file and returns the metadata+data (if specified) in a one or
-     * more info class instance(s).
-     *
-     * Function returns a vector because unlike with querydata, one grib file can contain many messages
-     * with totally different areas and projections. A single info-class instance can handle different times,
-     * levels, params and even changing grid size but it cannot handle different sized areas. Therefore from
-     * this function we need to return a vector.
-     *
-     * @param file Input file name
-     * @param options Search options (param, level, time)
-     * @param readContents Specify if data should also be read (and not only metadata)
-     *
-     * @return A vector of shared_ptr'd infos.
-     */
+	/**
+	 * @brief Return all data from a grib file.
+	 *
+	 * This function reads a grib file and returns the metadata+data (if specified) in a one or
+	 * more info class instance(s).
+	 *
+	 * Function returns a vector because unlike with querydata, one grib file can contain many messages
+	 * with totally different areas and projections. A single info-class instance can handle different times,
+	 * levels, params and even changing grid size but it cannot handle different sized areas. Therefore from
+	 * this function we need to return a vector.
+	 *
+	 * @param file Input file name
+	 * @param options Search options (param, level, time)
+	 * @param readContents Specify if data should also be read (and not only metadata)
+	 *
+	 * @return A vector of shared_ptr'd infos.
+	 */
 
-    std::vector<std::shared_ptr<info>> FromFile(const std::string& inputFile, const search_options& options, bool readContents = true);
+	std::vector<std::shared_ptr<info>> FromFile(const std::string& inputFile, const search_options& options, bool readContents = true);
 
-    bool ToFile(std::shared_ptr<info> info, const std::string& outputFile, HPFileType fileType, HPFileWriteOption fileWriteOption);
+	bool ToFile(std::shared_ptr<info> info, const std::string& outputFile, HPFileType fileType, HPFileWriteOption fileWriteOption);
 
 private:
 
-    bool WriteGrib(std::shared_ptr<const info> info, const std::string& outputFile, HPFileType fileType, bool appendToFile = false);
+	bool WriteGrib(std::shared_ptr<const info> info, const std::string& outputFile, HPFileType fileType, bool appendToFile = false);
 
-    std::shared_ptr<NFmiGrib> itsGrib;
+	std::shared_ptr<NFmiGrib> itsGrib;
 
 };
 
@@ -84,7 +84,7 @@ private:
 
 extern "C" std::shared_ptr<himan_plugin> create()
 {
-    return std::shared_ptr<grib> (new grib());
+	return std::shared_ptr<grib> (new grib());
 }
 
 #endif /* HIMAN_AUXILIARY_INCLUDE */
