0d2d082c2445ee5f27afb6cd198e100749f4aa53
Mikko Partio <mikko.partio@fmi.fi>
2013-03-21 12:10:34 +0000

    Changes

--- himan-bin/regression/windvector/windvector.sh
+++ himan-bin/regression/windvector/windvector.sh
@@ -6,20 +6,20 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f DD-D*.grib FF-MS*.grib
+rm -f DD-D*.grib FF-MS*.grib DF-MS*.grib
 
 $HIMAN -d 5 -f windvector_hl_regular.json -t grib hl_regular_source.grib --no-cuda
 
-grib_compare -A 0.01 hl_regular_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_044.grib
+grib_compare -A 0.01 hl_regular_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_006.grib
 
 if [ $? -eq 0 ];then
-  echo windvector/hirlam regular grid wind speed success!
+  echo windvector/hirlam regular grid wind speed success on CPU
 else
-  echo windvector/hirlam regular grid wind speed failed
+  echo windvector/hirlam regular grid wind speed failed on CPU
   exit 1
 fi
 
-grib_compare -A 0.01 hl_regular_result_DD.grib ./DD-D_height_10_rll_1030_816_0_044.grib
+grib_compare -A 0.01 hl_regular_result_DD.grib ./DD-D_height_10_rll_1030_816_0_006.grib
 
 if [ $? -eq 0 ];then
   echo windvector/hirlam regular grid wind direction success!
@@ -27,6 +27,35 @@ else
   echo windvector/hirlam regular grid wind direction failed
   exit 1
 fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  mv ./FF-MS_height_10_rll_1030_816_0_006.grib ./FF-MS_height_10_rll_1030_816_0_006-CPU.grib
+  mv ./DD-D_height_10_rll_1030_816_0_006.grib ./DD-D_height_10_rll_1030_816_0_006-CPU.grib
+
+  $HIMAN -d 5 -f windvector_hl_regular.json -t grib hl_regular_source.grib
+
+  grib_compare -b referenceValue -A 0.01 hl_regular_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_006.grib
+
+  if [ $? -eq 0 ];then
+    echo windvector/hirlam regular grid wind speed success GPU
+  else
+    echo windvector/hirlam regular grid wind speed failed GPU
+    exit 1
+  fi
+
+  grib_compare -A 0.01 hl_regular_result_DD.grib ./DD-D_height_10_rll_1030_816_0_006.grib
+
+  if [ $? -eq 0 ];then
+    echo windvector/hirlam regular grid wind direction success GPU
+  else
+    echo windvector/hirlam regular grid wind direction failed GPU
+    exit 1
+fi
+
+
+fi
+
 
 $HIMAN -d 5 -f windvector_hl_staggered.json -t grib hl_staggered_source.grib --no-cuda
 
