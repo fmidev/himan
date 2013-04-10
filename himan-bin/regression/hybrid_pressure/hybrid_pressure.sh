6955eb127e3ac7ce726b356bb96802e695ab756e
Mikko Aalto <mikko.aalto@fmi.fi>
2013-04-10 13:08:59 +0000

    Test for hybrid_pressure

---
+++ himan-bin/regression/hybrid_pressure/hybrid_pressure.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan"
+fi
+
+rm -f hybrid_pressure_hir.json.grib
+
+$HIMAN -d 5 -f hybrid_pressure_hir.json -t grib q2.grib q3.grib q4.grib p.grib
+
+grib_compare result.grib hybrid_pressure_hir.json.grib
+
+if [ $? -eq 0 ];then
+  echo hybrid_pressure/hirlam success!
+else
+  echo hybrid_pressure/hirlam failed
+  exit 1
+fi
+
