c4da0166d8d45a57a95a48355a13f213fff5e502
Mikko Partio <mikko.partio@fmi.fi>
2013-04-10 10:15:38 +0000

    Test suite for GFS

--- himan-bin/regression/tk2tc/tk2tc.sh
+++ himan-bin/regression/tk2tc/tk2tc.sh
@@ -6,11 +6,13 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f tk2tc_ec1h.json.grib tk2tc_ec1h.json-CPU.grib
+# EC
 
-$HIMAN -d 5 -f tk2tc_ec1h.json -t grib --no-cuda source.grib
+rm -f tk2tc_ec.json.grib tk2tc_ec.json-CPU.grib
 
-grib_compare result.grib tk2tc_ec1h.json.grib
+$HIMAN -d 5 -f tk2tc_ec.json -t grib --no-cuda ec_source.grib
+
+grib_compare ec_result.grib tk2tc_ec.json.grib
 
 if [ $? -eq 0 ];then
   echo tk2tc/ec success!
@@ -21,16 +23,50 @@ fi
 
 if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
 
-  mv tk2tc_ec1h.json.grib tk2tc_ec1h.json-CPU.grib
+  mv tk2tc_ec.json.grib tk2tc_ec.json-CPU.grib
 
-  $HIMAN -d 5 -f tk2tc_ec1h.json -t grib source.grib
+  $HIMAN -d 5 -f tk2tc_ec.json -t grib ec_source.grib
 
-  grib_compare -A 0.0001 tk2tc_ec1h.json.grib tk2tc_ec1h.json-CPU.grib
+  grib_compare -A 0.0001 tk2tc_ec.json.grib tk2tc_ec.json-CPU.grib
 
   if [ $? -eq 0 ];then
     echo tk2tc/ec success on GPU!
   else
     echo tk2tc/ec failed on GPU
+    exit 1
+  fi
+else
+  echo "no cuda device found for cuda tests"
+fi
+
+
+# GFS
+
+rm -f tk2tc_gfs.json.grib2 tk2tc_gfs.json-CPU.grib2
+
+$HIMAN -d 5 -f tk2tc_gfs.json -t grib2 --no-cuda gfs_source.grib2
+
+grib_compare gfs_result.grib2 tk2tc_gfs.json.grib2
+
+if [ $? -eq 0 ];then
+  echo tk2tc/gfs success!
+else
+  echo tk2tc/gfs failed
+  exit 1
+fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  mv tk2tc_gfs.json.grib2 tk2tc_gfs.json-CPU.grib2
+
+  $HIMAN -d 5 -f tk2tc_gfs.json -t grib2 gfs_source.grib2
+
+  grib_compare -A 0.0001 tk2tc_gfs.json.grib2 tk2tc_gfs.json-CPU.grib2
+
+  if [ $? -eq 0 ];then
+    echo tk2tc/gfs success on GPU!
+  else
+    echo tk2tc/gfs failed on GPU
     exit 1
   fi
 else
