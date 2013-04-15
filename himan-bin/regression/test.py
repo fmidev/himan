c9059769ad6b5240036073f6d770bf09dc000c43
Mikko Partio <mikko.partio@fmi.fi>
2013-04-15 14:50:45 +0000

    changes

--- himan-bin/regression/test.py
+++ himan-bin/regression/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import traceback
 import sys
 import gribapi
@@ -6,78 +8,94 @@ import decimal
 from decimal import *
 from gribapi import *
 
-HIL_PP=sys.argv[1]
-HIMAN=sys.argv[2]
+if len(sys.argv) < 3:
+	print "usage: test.py firstfile secondfile"
+	sys.exit(1)
 
-HIMAN_VALUE_SCALE = 1
-MAX_DIFF = 0.01
+FIRST_FILE=sys.argv[1]
+SECOND_FILE=sys.argv[2]
+
+SECOND_VALUE_SCALE = 1
+SECOND_VALUE_BASE = 0
+
+MAX_DIFF = 1
+ALLOWED_ERRORS=50
 
 def read():
-    
-    hilfile = open(HIL_PP)
-    himfile = open(HIMAN)
-    
-    msgno = 0
-    max_found_diff = 0
-    grid_point_of_max_diff = 0
-    msg_of_max_diff = 0
-
-    e = abs(Decimal(str(MAX_DIFF)).as_tuple().exponent)
 	
-    while True:   
+	firstfile = open(FIRST_FILE)
+	secondfile = open(SECOND_FILE)
 
-        hilgrid = grib_new_from_file(hilfile)
-        himgrid = grib_new_from_file(himfile)
+	print "First file: " + FIRST_FILE
+	print "Second file: " + SECOND_FILE
+	
+	msgno = 0
+	max_found_diff = 0
+	grid_point_of_max_diff = 0
+	msg_of_max_diff = 0
 
-        if hilgrid is None or himgrid is None:
-            break
+	e = abs(Decimal(str(MAX_DIFF)).as_tuple().exponent)
+	
+	while True:   
 
-        print "Reading msg " + str(msgno)
+		firstgrid = grib_new_from_file(firstfile)
+		secondgrid = grib_new_from_file(secondfile)
 
-        hilvalues = grib_get_values(hilgrid)
-        himvalues = grib_get_values(himgrid)
+		if firstgrid is None or secondgrid is None:
+			break
 
-        if len(hilvalues) != len(himvalues):
-    	    print "ERROR: grib data lengths are not the same"
-	    sys.exit(1)
+		print "Reading msg " + str(msgno)
 
-        for i in xrange(len(hilvalues)):
-            hilvalue = hilvalues[i]
-	    himvalue = himvalues[i]
+		firstvalues = grib_get_values(firstgrid)
+		secondvalues = grib_get_values(secondgrid)
 
-            himvalue = HIMAN_VALUE_SCALE * himvalue
-    
-            diff = abs(himvalue-hilvalue)
-	    diff = round(diff, e)
+		if len(firstvalues) != len(secondvalues):
+			print "ERROR: grib data lengths are not the same"
+			sys.exit(1)
 
-            if diff > MAX_DIFF:
-                print "Max diff exceeded: " + str(MAX_DIFF)
-	        print "Grid point: " + str(i) + " hilvalue: " + str(hilvalue) + ", himvalue: " + str(himvalue) + " diff: " + str(diff)
-	        sys.exit(1)
-	    
-            if diff > max_found_diff:
-	        max_found_diff = diff
-	        grid_point_of_max_diff = i
-		msg_of_max_diff = msgno
+		errors=0
+
+		for i in xrange(len(firstvalues)):
+			firstvalue = firstvalues[i]
+			secondvalue = secondvalues[i]
+
+			secondvalue = SECOND_VALUE_BASE + SECOND_VALUE_SCALE * secondvalue
+	
+			diff = abs(secondvalue-firstvalue)
+			diff = round(diff, e)
+
+			if diff > MAX_DIFF:
+				print "Max diff (" + str(MAX_DIFF) + ") exceeded at grid point: " + str(i) + " (#" + str(i+1) + ") first value: " + str(firstvalue) + ", secondvalue: " + str(secondvalue) + " diff: " + str(diff)
+				if ALLOWED_ERRORS > 0:
+					errors = errors+1
+					if errors > ALLOWED_ERRORS:
+						print "Too many errors, exiting"
+						sys.exit(1)
+				else:
+					sys.exit(1)
+
+			if diff > max_found_diff:
+				max_found_diff = diff
+				grid_point_of_max_diff = i
+				msg_of_max_diff = msgno
 		
-        msgno = msgno+1
+		msgno = msgno+1
 	
-        grib_release(hilgrid)
-        grib_release(himgrid)     
-        
-    print "Grib data is equal enough (largest diff allowed: " + str(MAX_DIFF) + ", largest diff found: " + str(max_found_diff) + ", gridpoint " + str(grid_point_of_max_diff) + ", message " + str(msg_of_max_diff) + ")"
-    
-          
-    hilfile.close()
-    himfile.close()
+		grib_release(firstgrid)
+		grib_release(secondgrid)	 
+		
+		print "Grib data is equal enough (largest diff allowed: " + str(MAX_DIFF) + ", largest diff found: " + str(max_found_diff) + ", gridpoint " + str(grid_point_of_max_diff) + ", message " + str(msg_of_max_diff) + " allowed errors " + str(errors) + "/" + str(ALLOWED_ERRORS) + ")"
+	
+	firstfile.close()
+	secondfile.close()
 
 def main():
-    try:
-        read()
-    except GribInternalError,err:
-        print >>sys.stderr,err.msg
+	try:
+		read()
+	except GribInternalError,err:
+		print >>sys.stderr,err.msg
 
-    return 0
+	return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+	sys.exit(main())
