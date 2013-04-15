5ea517f8920ca89d2a5d57b65c3c3c43d66cccf5
Mikko Partio <mikko.partio@fmi.fi>
2013-04-15 11:21:13 +0000

    fix file names

--- himan-bin/regression/windvector/windvector.sh
+++ himan-bin/regression/windvector/windvector.sh
@@ -8,7 +8,7 @@ fi
 
 rm -f DD-D*.grib FF-MS*.grib DF-MS*.grib
 
-$HIMAN -d 5 -f windvector_hl_regular.json -t grib hl_regular_source.grib --no-cuda
+$HIMAN -d 5 -f windvector_hl_regular.json -t grib hl_regular_source.grib -s hl_reg_nocuda --no-cuda
 
 grib_compare -A 0.01 hl_regular_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_006.grib
 
@@ -33,7 +33,7 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
   mv ./FF-MS_height_10_rll_1030_816_0_006.grib ./FF-MS_height_10_rll_1030_816_0_006-CPU.grib
   mv ./DD-D_height_10_rll_1030_816_0_006.grib ./DD-D_height_10_rll_1030_816_0_006-CPU.grib
 
-  $HIMAN -d 5 -f windvector_hl_regular.json -t grib hl_regular_source.grib
+  $HIMAN -d 5 -f windvector_hl_regular.json -t grib -s hl_reg_cuda hl_regular_source.grib
 
   grib_compare -b referenceValue -A 0.01 hl_regular_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_006.grib
 
@@ -52,28 +52,25 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
     echo windvector/hirlam regular grid wind direction failed GPU
     exit 1
 fi
-
-
 fi
 
+$HIMAN -d 5 -f windvector_hl_staggered.json -t grib hl_staggered_source.grib --no-cuda -s hl_stag_nocuda
 
-$HIMAN -d 5 -f windvector_hl_staggered.json -t grib hl_staggered_source.grib --no-cuda
-
-grib_compare -A 0.01 hl_staggered_result_FF.grib ./FF-MS_height_10_rll_1030_816_0_044.grib
+grib_compare -A 0.01 hl_staggered_result_FF.grib ./FF-MS_hybrid_55_polster_290_225_0_025.grib
 
 if [ $? -eq 0 ];then
-  echo windvector/hirlam regular grid wind speed success!
+  echo windvector/hirlam staggered grid wind speed success!
 else
-  echo windvector/hirlam regular grid wind speed failed
+  echo windvector/hirlam staggered grid wind speed failed
   exit 1
 fi
 
-grib_compare -A 0.01 hl_regular_result_DD.grib ./DD-D_height_10_rll_1030_816_0_044.grib
+grib_compare -A 0.01 hl_staggered_result_DD.grib ./DD-D_hybrid_55_polster_290_225_0_025.grib
 
 if [ $? -eq 0 ];then
-  echo windvector/hirlam regular grid wind direction success!
+  echo windvector/hirlam staggered grid wind direction success!
 else
-  echo windvector/hirlam regular grid wind direction failed
+  echo windvector/hirlam staggered grid wind direction failed
   exit 1
 fi
 
