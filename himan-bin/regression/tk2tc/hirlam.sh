2461ba4e2b60a6adc2cd0628f8dd0a84b17ba2f1
Mikko Partio <mikko.partio@fmi.fi>
2013-05-02 12:12:23 +0000

    separating each model to a separate script

---
+++ himan-bin/regression/tk2tc/hirlam.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan"
+fi
+
+rm -f tk2tc_hl.json.grib tk2tc_hl.json-CPU.grib
+
+$HIMAN -d 5 -f tk2tc_hl.json -t grib --no-cuda -s tk2tc_hl_nocuda hl_source.grib
+
+grib_compare hl_result.grib tk2tc_hl.json.grib
+
+if [ $? -eq 0 ];then
+  echo tk2tc/hl success!
+else
+  echo tk2tc/hl failed
+  exit 1
+fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  mv tk2tc_hl.json.grib tk2tc_hl.json-CPU.grib
+
+  $HIMAN -d 5 -f tk2tc_hl.json -t grib -s tk2tc_hl_cuda hl_source.grib
+
+  grib_compare -A 0.0001 tk2tc_hl.json.grib tk2tc_hl.json-CPU.grib
+
+  if [ $? -eq 0 ];then
+    echo tk2tc/hl success on GPU!
+  else
+    echo tk2tc/hl failed on GPU
+    exit 1
+  fi
+else
+  echo "no cuda device found for cuda tests"
+fi
+
