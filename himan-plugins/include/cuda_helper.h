d94818a6fd23234c0e0f2e512347c530e9b32141
Mikko Partio <mikko.partio@fmi.fi>
2013-05-02 10:22:34 +0000

    Support bitmaps

--- himan-plugins/include/cuda_helper.h
+++ himan-plugins/include/cuda_helper.h
@@ -1,9 +1,7 @@
 #ifndef CUDA_HELPER_H
 #define CUDA_HELPER_H
 
-#include <cuda_runtime.h>
-
-const float kFloatMissing = 32700.f;
+#ifdef HAVE_CUDA
 
 void CheckCudaError(cudaError_t errarg, const char* file, const int line);
 void CheckCudaErrorString(const char* errstr, const char* file,	const int line);
@@ -41,76 +39,242 @@ inline void CheckCudaErrorString(const char* errstr, const char* file,	const int
 	}
 }
 
-inline __device__ void GetBitValue(const unsigned char* d_p, long bitp, int *val)
+#ifdef __CUDACC__
+
+#include "packed_data.h"
+
+namespace himan
 {
-	d_p += (bitp >> 3);
-	*val = (*d_p&(1<<(7-(bitp%8))));
+	
+const float kFloatMissing = 32700.f;
+
+#define BitMask1(i)	(1u << i)
+#define BitTest(n,i)	!!((n) & BitMask1(i))
+
+inline __host__ void UnpackBitmap(const unsigned char* __restrict__ bitmap, int* unpacked, size_t len)
+{
+	size_t i, v = 1, idx = 0;
+	short j = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 7; j >= 0; j--)
+		{
+			if (BitTest(bitmap[i], j))
+			{
+				unpacked[idx] = v++;
+			}
+      idx++;
+    }
+  }
+}
+
+inline __device__ void GetBitValue(const unsigned char* p, long bitp, int *val)
+{
+	p += (bitp >> 3);
+	*val = (*p&(1<<(7-(bitp%8))));
+}
+
+inline __device__ void SetBitOn(unsigned char* p, long bitp)
+{
+  p += bitp/8;
+  *p |= (1u << (7-((bitp)%8)));
+}
+
+inline __device__ void SetBitOff( unsigned char* p, long bitp)
+{
+  p += bitp/8;
+  *p &= ~(1u << (7-((bitp)%8)));
 }
 
 inline __device__ void SimpleUnpackFullBytes(const unsigned char* __restrict__ d_p,
 											double* __restrict__ d_u,
-											size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+											const int* __restrict__ d_bm,
+											size_t values_len,
+											int bpv, double bsf, double dsf, double rv, bool hasBitmap, int idx)
 {
 	int bc;
 	unsigned long lvalue;
 
 	int l = bpv/8;
-	size_t o = idx*l;
 
 	if (idx < values_len)
 	{
-		lvalue	= 0;
-		lvalue	<<= 8;
-		lvalue |= d_p[o++] ;
+		int bm = idx;
+		int value_found = 1;
 
-		for ( bc=1; bc<l; bc++ )
+		if (hasBitmap)
 		{
-			lvalue <<= 8;
-			lvalue |= d_p[o++] ;
+			bm = d_bm[idx];
+
+			if (bm == 0)
+			{
+				d_u[idx] = kFloatMissing;
+				value_found = 0;
+			}
+			else
+			{
+				bm--;
+			}
 		}
 
-		d_u[idx] = ((lvalue*bsf)+rv)*dsf;
+		if (value_found)
+		{
+			size_t o = bm*l;
+
+			lvalue	= 0;
+			lvalue	<<= 8;
+			lvalue |= d_p[o++] ;
+
+			for ( bc=1; bc<l; bc++ )
+			{
+				lvalue <<= 8;
+				lvalue |= d_p[o++] ;
+			}
+
+			d_u[idx] = ((lvalue*bsf)+rv)*dsf;
+		}
 	}
 }
 
 inline __device__ void SimpleUnpackUnevenBytes(const unsigned char* __restrict__ d_p,
-											double* __restrict__ d_u,
-											size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+												double* __restrict__ d_u,
+												const int* __restrict__ d_bm,
+												size_t values_len, int bpv, double bsf, double dsf, double rv, bool hasBitmap, int idx)
 {
 	int j=0;
 	unsigned long lvalue;
-	long bitp=bpv*idx;
 
 	if (idx < values_len)
 	{
-		lvalue=0;
+		int bm = idx;
+		int value_found = 1;
 
-		for(j=0; j< bpv;j++)
+		/*
+		 * Check if bitmap is set.
+		 * If bitmap is set and indicates that value for this element is missing, do
+		 * not proceed to calculating phase.
+		 *
+		 * If bitmap is set and indicates that value exists for this element, the index
+		 * for the actual data is the one indicated by the bitmap array. From this index
+		 * we reduce one (1) because that one is added to the value in unpack_bitmap.
+		 */
+		
+		if (hasBitmap)
 		{
-			lvalue <<= 1;
-			int val;
+			bm = d_bm[idx];
 
-			GetBitValue(d_p, bitp, &val);
-
-			if (val) lvalue += 1;
-
-			bitp += 1;
+			if (bm == 0)
+			{
+				d_u[idx] = kFloatMissing;
+				value_found = 0;
+			}
+			else
+			{
+				bm--;
+			}
 		}
 
-		d_u[idx] = ((lvalue*bsf)+rv)*dsf;
-	}
+		if (value_found)
+		{
+			long bitp=bpv*bm;
 
+			lvalue=0;
+
+			for(j=0; j< bpv;j++)
+			{
+				lvalue <<= 1;
+				int val;
+
+				GetBitValue(d_p, bitp, &val);
+
+				if (val) lvalue += 1;
+
+				bitp += 1;
+			}
+			d_u[idx] = ((lvalue*bsf)+rv)*dsf;
+		}
+	}
 }
 
-inline __device__ void SimpleUnpack(const unsigned char* d_p, double* d_u, size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+__global__ void SimpleUnpack(const unsigned char* d_p, double* d_u, const int* d_bm, himan::simple_packed_coefficients coeff, size_t N, bool hasBitmap)
 {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (idx < N)
+	{
+		if (coeff.bitsPerValue % 8)
+		{
+			SimpleUnpackUnevenBytes(d_p, d_u, d_bm, N,coeff.bitsPerValue, coeff.binaryScaleFactor, coeff.decimalScaleFactor, coeff.referenceValue, hasBitmap, idx);
+		}
+		else
+		{
+			SimpleUnpackFullBytes(d_p, d_u, d_bm, N, coeff.bitsPerValue, coeff.binaryScaleFactor, coeff.decimalScaleFactor, coeff.referenceValue, hasBitmap, idx);
+		}
+	}
+}
+
+// ----------- NOTE -----------
+// PACKING FUNCTION DO NOT WORK YET
+
+inline __device__ void SimplePackUnevenBytes(const double* d_u, unsigned char* d_p,
+									size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+{
+
+	double x=(((d_u[idx]*dsf)-rv)*bsf)+0.5;
+	long bitp=bpv*idx;
+
+	long  i = 0;
+
+	for (i=bpv-1; i >= 0; i--)
+	{
+		if(BitTest(static_cast<unsigned long> (x),i))
+		{
+			SetBitOn(d_p, bitp);
+		}
+		else
+		{
+			SetBitOff(d_p, bitp);
+		}
+	}
+}
+
+inline __device__ void SimplePackFullBytes(const double* d_u, unsigned char* d_p,
+									size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+{
+
+	int blen=0;
+	unsigned char* encoded = d_p + idx * static_cast<int> (bpv/8);
+
+	blen = bpv;
+	double x = ((((d_u[idx]*dsf)-rv)*bsf)+0.5);
+	unsigned long unsigned_val = (unsigned long)x;
+	while(blen >= 8)
+	{
+		blen -= 8;
+		*encoded = (unsigned_val >> blen);
+		encoded++;
+		//*off += idx*8;
+
+	}
+}
+
+inline __device__ void SimplePack(const double* d_u, unsigned char* d_p,
+									size_t values_len, int bpv, double bsf, double dsf, double rv, int idx)
+{
+
 	if (bpv%8)
 	{
-		SimpleUnpackUnevenBytes(d_p, d_u, values_len, bpv, bsf, dsf, rv, idx);
+		SimplePackUnevenBytes(d_u, d_p, values_len, bpv, bsf, dsf, rv, idx);
 	}
 	else
 	{
-		SimpleUnpackFullBytes(d_p, d_u, values_len, bpv, bsf, dsf, rv, idx);
+		SimplePackFullBytes(d_u, d_p, values_len, bpv, bsf, dsf, rv, idx);
 	}
 }
-#endif
+
+} // namespace himan
+
+#endif // __CUDACC__
+#endif // HAVE_CUDA
+#endif // CUDA_HELPER_H
