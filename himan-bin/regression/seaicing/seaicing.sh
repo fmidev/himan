a8ad178f59f09c04babf8ba73805fb9170505f1f
Mikko Aalto <mikko.aalto@fmi.fi>
2013-08-08 11:36:53 +0000

    Initial import for seaicing regression test

---
+++ himan-bin/regression/seaicing/seaicing.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan"
+fi
+
+rm -f seaicing_hir1h.json.grib
+
+$HIMAN -d 5 -f seaicing_hir1h.json -t grib t2m.grib t0m.grib ff10.grib
+
+grib_compare result.grib seaicing_hir1h.json.grib
+
+if [ $? -eq 0 ];then
+  echo seaicing/hirlam success!
+else
+  echo seaicing/hirlam failed
+  exit 1
+fi
+
