755feaad46ab2b140dfbbd8b925a2660483b72dc
Mikko Partio <mikko.partio@fmi.fi>
2013-11-08 12:42:22 +0000

    Adding tests

--- himan-bin/regression/precipitation/hirlam.sh
+++ himan-bin/regression/precipitation/hirlam.sh
@@ -40,4 +40,21 @@ else
   exit 1
 fi
 
+$HIMAN -d 5 -f precipitation_hl_rate.json -t grib hl_source_rate.grib --no-cuda -s hl_nocuda
+
+grib_compare hl_result_rrr_24.grib RRR-KGM2_height_0_rll_1030_816_0_024.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/hirlam failed on CPU
+  exit 1
+fi
+
+grib_compare hl_result_rrr_25.grib RRR-KGM2_height_0_rll_1030_816_0_025.grib
+
+if [ $? -eq 0 ];then
+  echo precipitation/hirlam success on CPU!
+else
+  echo precipitation/hirlam failed on CPU
+  exit 1
+fi
 
