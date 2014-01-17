0e4198d1d64b62300e728ccb0b0ebfb01c24ff9d
Jussi Perämäki <jussi.peramaki@fmi.fi>
2014-01-17 13:13:10 +0000

    regression test for ncl

---
+++ himan-tests/ncl/ncl.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan-bin/himan"
+fi
+
+rm -f HL-M_*
+
+$HIMAN -d 5 -f ncl.json -t grib height_himan.grib t.grib
+
+grib_compare result.grib H0C-M_height_0_rll_1030_816_0_001.grib
+
+if [ $? -eq 0 ];then
+  echo ncl/hirlam success!
+else
+  echo ncl/hirlam failed
+  exit 1
+fi
