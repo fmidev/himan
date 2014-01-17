6917cbd7565eb9bf06c6ced9d74dc1b68fdc783f
Mikko Partio <mikko.partio@fmi.fi>
2014-01-17 06:51:16 +0000

    Modified test to accomodate the changes in hybrid_height wrt hirlam

--- himan-tests/hybrid_height/hybrid_height.sh
+++ himan-tests/hybrid_height/hybrid_height.sh
@@ -6,11 +6,11 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f hybrid_height.json.grib
+rm -f HL-M_*
 
-$HIMAN -d 5 -f hybrid_height.json -t grib t3.grib prev_t3.grib p3.grib prev_p3.grib prev_h3.grib
+$HIMAN -d 5 -f hybrid_height.json -t grib hl_source_geop.grib
 
-grib_compare result.grib hybrid_height.json.grib
+grib_compare result.grib HL-M_hybrid_64_polster_290_225_0_003.grib
 
 if [ $? -eq 0 ];then
   echo hybrid_height/hirlam success!
