701a1a87bc7bdf21b170c2f0b6722e39f2b6de9c
Mikko Partio <mikko.partio@fmi.fi>
2014-02-18 10:19:56 +0000

    Add rate test for EC and radiation results for harmonie

--- himan-tests/split_sum/ecmwf.sh
+++ himan-tests/split_sum/ecmwf.sh
@@ -33,3 +33,42 @@ else
   exit 1
 fi
 
+$HIMAN -d 5 -f precipitation_ec_rate_1.json -t grib ec_source_rate_1.grib --no-cuda -s ec_nocuda
+
+grib_compare ec_result_rate_1_4.grib ./RRR-KGM2_ground_0_rll_161_177_0_004.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/ecmwf failed on CPU
+  exit 1
+fi
+
+grib_compare ec_result_rate_1_5.grib ./RRR-KGM2_ground_0_rll_161_177_0_005.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/ecmwf failed on CPU
+  exit 1
+fi
+
+grib_compare ec_result_rate_1_6.grib ./RRR-KGM2_ground_0_rll_161_177_0_006.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/ecmwf failed on CPU
+  exit 1
+fi
+
+$HIMAN -d 5 -f precipitation_ec_rate_6.json -t grib ec_source_rate_6.grib --no-cuda -s ec_nocuda
+
+grib_compare ec_result_rate_6_164.grib ./RRR-KGM2_ground_0_rll_233_231_0_164.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/ecmwf failed on CPU
+  exit 1
+fi
+
+grib_compare ec_result_rate_6_165.grib ./RRR-KGM2_ground_0_rll_233_231_0_165.grib
+
+if [ $? -ne 0 ];then
+  echo precipitation/ecmwf failed on CPU
+  exit 1
+fi
+
