048ea5dae2666c5e171db8eccc9b83def1e9cba6
Mikko Partio <mikko.partio@fmi.fi>
2014-02-18 08:53:44 +0000

    Add a few tests for util::

--- himan-tests/unit-tests/source/unit_util.cpp
+++ himan-tests/unit-tests/source/unit_util.cpp
@@ -13,10 +13,10 @@ BOOST_AUTO_TEST_CASE(LCL_SLOW)
 
         // "slow" calculation of LCL
 
-	std::vector<double> LCL = himan::util::LCL(850,16.5828,-1.45402);
+	std::vector<double> LCL = himan::util::LCL(85000,273.15 + 16.5828, 273.15 + -1.45402);
 
-	BOOST_CHECK_CLOSE(LCL[0], 648.296, kEpsilon);
-	BOOST_CHECK_CLOSE(LCL[1], -5.01719, kEpsilon);
+	BOOST_CHECK_CLOSE(LCL[0], 64829.6, kEpsilon);
+	BOOST_CHECK_CLOSE(LCL[1], 273.15 + -5.01719, kEpsilon);
 }
 
 BOOST_AUTO_TEST_CASE(LCL_FAST)
@@ -24,8 +24,36 @@ BOOST_AUTO_TEST_CASE(LCL_FAST)
 
         // fast calculation of LCL
 
-	std::vector<double> LCL = himan::util::LCL(850,16.6453,-1.29777);
+	std::vector<double> LCL = himan::util::LCL(85000,273.15 + 16.6453, 273.15 + -1.29777);
 
-	BOOST_CHECK_CLOSE(LCL[0], 648.793, kEpsilon);
-	BOOST_CHECK_CLOSE(LCL[1], -4.90058, kEpsilon);
+	BOOST_CHECK_CLOSE(LCL[0], 64878.5986, kEpsilon);
+	BOOST_CHECK_CLOSE(LCL[1], 273.15 + -4.90058, kEpsilon);
+}
+
+BOOST_AUTO_TEST_CASE(SATURATED_MIXING_RATIO)
+{
+
+	// saturated mixing ratio
+
+	double r = util::MixingRatio(290, 98000);
+
+	BOOST_CHECK_CLOSE(r, 12.4395, kEpsilon);
+
+}
+
+BOOST_AUTO_TEST_CASE(ES)
+{
+
+	// water vapour pressure
+
+	double E = util::Es(285);
+
+	BOOST_CHECK_CLOSE(E, 1389.859, kEpsilon);
+
+	// negative temperatures
+
+	E = util::Es(266);
+
+	BOOST_CHECK_CLOSE(E, 333.356, kEpsilon);
+
 }
