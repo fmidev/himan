bfbdf6e5dc49bddb26f11b18ab8c9e04253da2b2
Mikko Partio <mikko.partio@fmi.fi>
2014-02-18 07:45:53 +0000

    Initial import

---
+++ himan-lib/source/simple_packed.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file simple_packed.cpp
+ *
+ * @date Feb 18, 2014
+ * @author partio
+ */
+
+#include "simple_packed.h"
+#include "cuda_helper.h"
+
+void himan::simple_packed::Unpack(double* arr, size_t N)
+{
+	if (!packedLength)
+	{
+		return;
+	}
+
+	assert(N == unpackedLength);
+	
+	double* d_arr = Unpack(0); // use 0-stream
+
+	CUDA_CHECK(cudaMemcpy(arr, d_arr, sizeof(double) * N, cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaFree(d_arr));
+
+}
