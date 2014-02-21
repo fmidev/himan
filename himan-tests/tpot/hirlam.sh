5ee3055e35d2d73f983d5dfca56263475669ac75
Mikko Partio <mikko.partio@fmi.fi>
2014-02-21 05:56:51 +0000

    thetaw & thetae tests

--- himan-tests/tpot/hirlam.sh
+++ himan-tests/tpot/hirlam.sh
@@ -6,7 +6,7 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f TP-K*.grib TPW-K*.grib
+rm -f TPE-K*.grib TP-K*.grib TPW-K*.grib
 
 $HIMAN -d 5 -f tpot_hl.json -t grib source_hl.grib --no-cuda -s stat
 
@@ -26,8 +26,35 @@ else
   exit 1
 fi
 
-echo "thetaw results are not complete yet"
-exit 0
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+  
+  rm -f TP-K*.grib TPW-K*.grib
+
+  $HIMAN -d 5 -f tpot_hl.json -s cuda source_hl.grib
+
+  grib_compare -A 0.001 result_hl_theta_4.grib ./TP-K_pressure_850_rll_1030_816_0_004.grib
+  
+  if [ $? -ne 0 ];then
+    echo tpot/hl failed on GPU
+    exit 1
+  fi
+
+  grib_compare -A 0.001 result_hl_theta_5.grib ./TP-K_pressure_850_rll_1030_816_0_005.grib
+
+  if [ $? -eq 0 ];then
+    echo tpot/hl success on GPU!
+  else
+    echo tpot/hl failed on GPU
+    exit 1
+  fi
+
+else
+  echo "no cuda device found for cuda tests"
+fi
+
+# THETA W
+
+$HIMAN -d 5 -f tpot_hl_thetaw.json -t grib source_hl.grib --no-cuda -s stat
 
 grib_compare result_hl_thetaw_4.grib ./TPW-K_pressure_850_rll_1030_816_0_004.grib
 
@@ -45,31 +72,24 @@ else
   exit 1
 fi
 
-exit 0
+# THETA E
 
-if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+$HIMAN -d 5 -f tpot_hl_thetae.json -t grib source_hl_thetae.grib --no-cuda -s stat
 
-  mv ./TPW-K_pressure_850_rll_582_448_0_043.grib ./TPW-K_pressure_850_rll_582_448_0_043-CPU.grib
-  mv ./TPW-K_pressure_700_rll_582_448_0_043.grib ./TPW-K_pressure_700_rll_582_448_0_043-CPU.grib
+grib_compare result_hl_thetae.grib ./TPE-K_pressure_850_rll_1030_816_0_005.grib
 
-  $HIMAN -d 5 -f tpot_hl.json -t grib source.grib
-
-  grib_compare hl_result_700.grib ./TPW-K_pressure_850_rll_582_448_0_043.grib
-
-  if [ $? -ne 0 ];then
-    echo tpot/hl failed on GPU
-    exit 1
-  fi
-
-  grib_compare hl_result_850.grib ./TPW-K_pressure_850_rll_582_448_0_043-CPU.grib
-
-  if [ $? -eq 0 ];then
-    echo tpot/hl success on GPU!
-  else
-    echo tpot/hl failed on GPU
-    exit 1
-  fi
-else
-  echo "no cuda device found for cuda tests"
+if [ $? -ne 0 ];then
+  echo tpot/hl thetae failed on CPU
+  exit 1
 fi
+
+grib_compare result_hl_thetae.grib ./TPE-K_pressure_850_rll_1030_816_0_005.grib
+
+if [ $? -eq 0 ];then
+  echo tpot/hl thetae success on CPU!
+else
+  echo tpot/hl thetae failed on CPU
+  exit 1
+fi
+
 
