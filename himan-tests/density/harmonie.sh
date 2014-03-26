aee9ebdedc35bbb1b8342198fdd3d5844d9d513c
Unknown Author <unknown@fmi.fi>
2014-03-26 09:23:42 +0000

    updated result files of density and precipitation rate to bug-fixed versions

--- himan-tests/density/harmonie.sh
+++ himan-tests/density/harmonie.sh
@@ -6,17 +6,17 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan-bin/build/release/himan"
 fi
 
-rm -f RHO-KGM3*.grib
+#rm -f RHO-KGM3*.grib
 
 $HIMAN -d 5 -f density_harmonie.json -t grib --no-cuda harmonie_p_source.grib harmonie_t_source.grib
 
 grib_compare ./RHO-KGM3_hybrid_65_rll_290_594_0_360.grib result_harmonie.grib 
 
 if [ $? -eq 0 ];then
-  echo relative humidity/harmonie success on CPU!
+  echo density/harmonie success on CPU!
 else
-  echo relative humidity/harmonie failed on CPU
+  echo density/harmonie failed on CPU
   exit 1
 fi
 
-rm -f RHO-KGM3*.grib
+#rm -f RHO-KGM3*.grib
--- himan-tests/density/hirlam.sh
+++ himan-tests/density/hirlam.sh
@@ -13,9 +13,9 @@ $HIMAN -d 5 -f density_hirlam.json -t grib --no-cuda hirlam_p_source.grib hirlam
 grib_compare RHO-KGM3_hybrid_65_rll_1030_816_0_006.grib result_hirlam.grib 
 
 if [ $? -eq 0 ];then
-  echo relative humidity/hirlam success on CPU!
+  echo density/hirlam success on CPU!
 else
-  echo relative humidity/hirlam failed on CPU
+  echo density/hirlam failed on CPU
   exit 1
 fi
 
