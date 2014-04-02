e5aa38172ff95e4089cff23e455cd597ad44f7d2
Unknown Author <unknown@fmi.fi>
2014-04-02 05:57:07 +0000

    Added abolute humidity plug-in. Parameter ABSH-KGM3 not in neons database yet. When added plug-in should work.

---
+++ himan-plugins/include/absolute_humidity.h
@@ -0,0 +1,73 @@
+/*
+ * absolute_humidity.h
+ *
+ *  Created on: Mar 27, 2014
+ *      Author: Tack
+ */
+
+#ifndef absolute_humidity_H
+#define absolute_humidity_H
+
+#include "compiled_plugin.h"
+#include "compiled_plugin_base.h"
+#include <math.h> // fmax function
+
+namespace himan
+{
+namespace plugin
+{
+
+/**
+ * @class instant_precipitation
+ *
+ * @brief Calculates the absolute humidity of air by the formula
+ *          1. absolute_humidity = rho*(WMR+SMR)
+ *        where rho is density, WMR is mixing ratio for water and SMR is mixing ratio for solid precipitation.
+ *
+ *        In case of negative mixing ratios the value is corrected to 0.0 kg/kg.
+ *
+ */
+
+class absolute_humidity : public compiled_plugin, private compiled_plugin_base
+{
+public:
+    absolute_humidity();
+
+    inline virtual ~absolute_humidity() {}
+
+    absolute_humidity(const absolute_humidity& other) = delete;
+    absolute_humidity& operator=(const absolute_humidity& other) = delete;
+
+    virtual void Process(std::shared_ptr<const plugin_configuration> conf);
+
+    virtual std::string ClassName() const
+    {
+        return "himan::plugin::absolute_humidity";
+    }
+
+    virtual HPPluginClass PluginClass() const
+    {
+        return kCompiled;
+    }
+
+    virtual HPVersionNumber Version() const
+    {
+        return HPVersionNumber(0, 1);
+    }
+
+private:
+    virtual void Calculate(std::shared_ptr<info> theTargetInfo, unsigned short theThreadIndex);
+
+};
+
+// the class factory
+
+extern "C" std::shared_ptr<himan_plugin> create()
+{
+    return std::shared_ptr<absolute_humidity> (new absolute_humidity());
+}
+
+} // namespace plugin
+} // namespace himan
+
+#endif /* absolute_humidity_H */
--- himan-tests/precipitation-rate/harmonie.sh
+++ himan-tests/precipitation-rate/harmonie.sh
@@ -6,20 +6,18 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan-bin/build/release/himan"
 fi
 
-rm -f RHO-KGM3*.grib RRR*.grib
+#rm -f RHO-KGM3*.grib ABS*.grib
 
-$HIMAN -d 4 -f precipitation_rate_harmonie.json -t grib harmonie_p_source.grib harmonie_t_source.grib harmonie_rain_source.grib harmonie_snow_source.grib harmonie_graupel_source.grib
+$HIMAN -d 4 -f absolute_humidity_harmonie.json -t grib harmonie_p_source.grib harmonie_t_source.grib harmonie_rain_source.grib harmonie_snow_source.grib harmonie_graupel_source.grib
 
 grib_compare ./RRRS-KGM2_hybrid_60_rll_290_594_0_360.grib result_solidpr_harmonie.grib
 VAR_1=$?
-grib_compare ./RRR-KGM2_hybrid_60_rll_290_594_0_360.grib result_rain_harmonie.grib
-VAR_2=$?
 
-if [ $VAR_1 -eq 0 -a $VAR_2 -eq 0 ];then
-  echo precipitation-rate/harmonie success on CPU!
+if [ $VAR_1 -eq 0 ];then
+  echo absolute_humidity/harmonie success on CPU!
 else
-  echo precipitation-rate/harmonie failed on CPU
+  echo absolute_humidity/harmonie failed on CPU
   exit 1
 fi
 
-rm -f RHO-KGM3*.grib RRR*.grib
+#rm -f RHO-KGM3*.grib RRR*.grib
