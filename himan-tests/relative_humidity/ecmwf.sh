5274e49927e202861ffdbbd85f37d07c5f03b9ed
Unknown Author <unknown@fmi.fi>
2014-04-25 10:03:56 +0000

    Added CUDA test.

--- himan-tests/relative_humidity/ecmwf.sh
+++ himan-tests/relative_humidity/ecmwf.sh
@@ -19,3 +19,19 @@ else
   exit 1
 fi
 
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+rm -f RH-PRCNT_height_2_rll_233_231_0_006.grib
+
+  $HIMAN -s cuda -d 5 -f relative_humidity_ecmwf.json -t grib source_ecmwf.grib
+
+grib_compare RH-PRCNT_height_2_rll_233_231_0_006.grib result_ecmwf.grib 
+
+  if [ $? -eq 0 ];then
+    echo relative_humidity/ecmwf success on GPU!
+  else
+    echo relative_humidity/ecmwf failed on GPU
+  fi
+else
+  echo "no cuda device found for cuda tests"
+fi
--- himan-tests/relative_humidity/harmonie.sh
+++ himan-tests/relative_humidity/harmonie.sh
@@ -32,3 +32,36 @@ else
   echo relative humidity/harmonie failed on CPU
   exit 1
 fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+rm -f RH-PRCNT*.grib P-HPA*grib
+
+  $HIMAN -s cuda -d 5 -f relative_humidity_harmonie.json -t grib source_harmonie.grib
+
+grib_compare ./RH-PRCNT_hybrid_44_rll_290_594_0_1860.grib result_harmonie_1860.grib 
+
+if [ $? -ne 0 ];then
+  echo relative humidity/harmonie failed on GPU
+  exit 1
+fi
+
+grib_compare ./RH-PRCNT_hybrid_44_rll_290_594_0_1875.grib result_harmonie_1875.grib 
+
+  if [ $? -ne 0 ];then
+    echo relative_humidity/harmonie failed on GPU
+    exit 1
+  fi
+
+grib_compare ./RH-PRCNT_hybrid_44_rll_290_594_0_1890.grib result_harmonie_1890.grib 
+
+if [ $? -eq 0 ];then
+  echo relative humidity/harmonie success on GPU!
+else
+  echo relative humidity/harmonie failed on GPU
+  exit 1
+fi
+
+else
+  echo "no cuda device found for cuda tests"
+fi
