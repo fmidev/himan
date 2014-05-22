6e4c21aea94146d6c424115cf2032a0ab1f8672e
Mikko Partio <mikko.partio@fmi.fi>
2014-05-22 07:49:31 +0000

    updated results

--- himan-tests/preform_pressure/hirlam.sh
+++ himan-tests/preform_pressure/hirlam.sh
@@ -10,7 +10,7 @@ rm -f PREC*grib
 
 $HIMAN -d 5 -f preform_hirlam.json --no-cuda -s hirlam-stat -t grib  hirlam-source.grib
 
-grib_compare hirlam-result.grib PRECFORM-N_height_0_rll_1030_816_0_004.grib
+grib_compare hirlam-result.grib ./PRECFORM-N_height_0_rll_1030_816_0_009.grib
 
 if [ $? -ne 0 ];then
   echo preform_pressure/hirlam failed on CPU
