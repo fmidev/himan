a8827d2bb85159cad00ccfb8afdc47ab50a7860a
Mikko Partio <mikko.partio@fmi.fi>
2014-08-28 06:07:15 +0000

    Improved tests for harmonie

--- himan-tests/dewpoint/harmonie.sh
+++ himan-tests/dewpoint/harmonie.sh
@@ -6,32 +6,46 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan-bin/himan"
 fi
 
-rm -f dewpoint_arome.json.grib dewpoint_arome.json-CPU.grib
+rm -f TD-C*grib
 
-$HIMAN -s no-cuda -d 5 -f dewpoint_arome.json -t grib source.grib --no-cuda
+$HIMAN -s no-cuda -d 5 -f dewpoint_harmonie.json -t grib source.grib --no-cuda
 
-grib_compare result.grib dewpoint_arome.json.grib
+grib_compare harmonie_result_height.grib TD-C_height_2_rll_720_800_0_1440.grib
+
+if [ $? -ne 0 ];then
+  echo dewpoint/harmonie failed on CPU
+  exit 1
+fi
+
+grib_compare harmonie_result_pressure.grib TD-C_pressure_700_rll_720_800_0_1440.grib
 
 if [ $? -eq 0 ];then
-  echo dewpoint/arome success on CPU!
+  echo dewpoint/harmonie success on CPU!
 else
-  echo dewpoint/arome failed on CPU
+  echo dewpoint/harmonie failed on CPU
   exit 1
 fi
 
 if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
 
-  mv dewpoint_arome.json.grib dewpoint_arome.json-CPU.grib
+  rm -f TD-C_*.grib
 
-  $HIMAN -s cuda -d 5 -f dewpoint_arome.json -t grib source.grib
+  $HIMAN -s cuda -d 5 -f dewpoint_harmonie.json -t grib source.grib
 
-  grib_compare -A 0.001 dewpoint_arome.json.grib dewpoint_arome.json-CPU.grib
+  grib_compare -A 0.001 harmonie_result_height.grib TD-C_height_2_rll_720_800_0_1440.grib
+
+  if [ $? -ne 0 ];then
+    echo dewpoint/harmonie failed on GPU
+  fi
+
+  grib_compare -A 0.001 harmonie_result_pressure.grib TD-C_pressure_700_rll_720_800_0_1440.grib
 
   if [ $? -eq 0 ];then
-    echo dewpoint/arome success on GPU!
+    echo dewpoint/harmonie success on GPU!
   else
-    echo dewpoint/arome failed on GPU
+    echo dewpoint/harmonie failed on GPU
   fi
+
 else
   echo "no cuda device found for cuda tests"
 fi
