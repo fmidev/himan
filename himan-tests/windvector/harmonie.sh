31b7021088cf18612d1c1a8aa336bc3ef98e735f
Mikko Partio <mikko.partio@fmi.fi>
2014-08-28 12:46:06 +0000

    updated test for geometry HARMONIE022

--- himan-tests/windvector/harmonie.sh
+++ himan-tests/windvector/harmonie.sh
@@ -3,14 +3,14 @@
 set -x
 
 if [ -z "$HIMAN" ]; then
-	export HIMAN="../../himan"
+	export HIMAN="../../himan-bin/himan"
 fi
 
 rm -f FFG-MS*.grib
 
 $HIMAN -d 5 -f windvector_harmonie.json -t grib harmonie_gust_source.grib -s harmonie_nocuda --no-cuda
 
-grib_compare -A 0.01 harmonie_gust_result.grib ./FFG-MS_height_10_rll_290_594_0_360.grib
+grib_compare -A 0.01 harmonie_gust_result.grib ./FFG-MS_height_10_rll_720_800_0_360.grib
 
 if [ $? -eq 0 ];then
   echo windvector/harmonie gust speed success on CPU
@@ -25,7 +25,7 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
 
   $HIMAN -d 5 -f windvector_harmonie.json -t grib -s harmonie_cuda harmonie_gust_source.grib
 
-  grib_compare -b referenceValue -A 0.001 harmonie_gust_result.grib ./FFG-MS_height_10_rll_290_594_0_360.grib
+  grib_compare -b referenceValue -A 0.001 harmonie_gust_result.grib ./FFG-MS_height_10_rll_720_800_0_360.grib
 
   if [ $? -eq 0 ];then
     echo windvector/harmonie gust speed success GPU
@@ -40,7 +40,7 @@ rm -f FF-MS*grib DD-D*grib
 
 $HIMAN -d 5 -f windvector_harmonie_wind.json -t grib harmonie_wind_source.grib -s harmonie_nocuda --no-cuda
 
-grib_compare -A 0.01 harmonie_wind_result_FF.grib ./FF-MS_hybrid_15_rll_290_594_0_120.grib
+grib_compare -A 0.01 harmonie_wind_result_FF.grib ./FF-MS_hybrid_15_rll_720_800_0_120.grib
 
 if [ $? -eq 0 ];then
   echo windvector/harmonie wind speed success on CPU
@@ -49,7 +49,7 @@ else
   exit 1
 fi
 
-grib_compare -A 1 harmonie_wind_result_DD.grib ./DD-D_hybrid_15_rll_290_594_0_120.grib
+grib_compare -A 1 harmonie_wind_result_DD.grib ./DD-D_hybrid_15_rll_720_800_0_120.grib
 
 if [ $? -eq 0 ];then
   echo windvector/harmonie wind direction success on CPU
@@ -64,7 +64,7 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
 
   $HIMAN -d 5 -f windvector_harmonie_wind.json -t grib -s harmonie_cuda harmonie_wind_source.grib
 
-  grib_compare -b referenceValue -A 0.001 harmonie_wind_result_FF.grib ./FF-MS_hybrid_15_rll_290_594_0_120.grib
+  grib_compare -b referenceValue -A 0.001 harmonie_wind_result_FF.grib ./FF-MS_hybrid_15_rll_720_800_0_120.grib
 
   if [ $? -eq 0 ];then
     echo windvector/harmonie wind speed success GPU
@@ -73,7 +73,7 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
     exit 1
   fi
 
-  grib_compare -A 1 harmonie_wind_result_DD.grib ./DD-D_hybrid_15_rll_290_594_0_120.grib
+  grib_compare -A 1 harmonie_wind_result_DD.grib ./DD-D_hybrid_15_rll_720_800_0_120.grib
 
   if [ $? -eq 0 ];then
     echo windvector/harmonie wind direction success on GPU
