61f7d8797d3ec9ffd59562032eaebe7724389362
Jussi Perämäki <jussi.peramaki@fmi.fi>
2014-09-19 12:05:30 +0000

    small modifications

--- himan-tests/ncl/hirlam.sh
+++ himan-tests/ncl/hirlam.sh
@@ -8,7 +8,7 @@ fi
 
 rm -f H0C-M_*
 
-$HIMAN -d 5 -f ncl.json -t grib height_himan.grib t.grib
+$HIMAN -d 5 -f hirlam.json -t grib height_himan.grib t.grib
 
 grib_compare result.grib H0C-M_height_0_rll_1030_816_0_001.grib
 
