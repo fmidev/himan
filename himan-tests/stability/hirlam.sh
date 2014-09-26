d927ae825500268038f76fce14e3429b6c99eee3
Mikko Partio <mikko.partio@fmi.fi>
2014-09-26 05:04:13 +0000

    updating test to reflect changes in plugin

--- himan-tests/stability/hirlam.sh
+++ himan-tests/stability/hirlam.sh
@@ -17,19 +17,19 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 
-grib_compare hirlam-result-si.grib SI-N_height_0_rll_1030_816_0_010.grib
+#grib_compare hirlam-result-si.grib SI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+#if [ $? -ne 0 ];then
+#  echo stability/hirlam failed on CPU
+#  exit 1
+#fi
 
 #grib_compare hirlam-result-li.grib LI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+#if [ $? -ne 0 ];then
+#  echo stability/hirlam failed on CPU
+#  exit 1
+#fi
 
 grib_compare hirlam-result-vti.grib VTI-N_height_0_rll_1030_816_0_010.grib
 
@@ -63,46 +63,46 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
   grib_compare hirlam-result-kindex.grib KINDEX-N_height_0_rll_1030_816_0_010.grib
 
   if [ $? -ne 0 ];then
-    echo stability/hirlam failed on CPU
+    echo stability/hirlam failed on GPU
     exit 1
   fi
 
-grib_compare hirlam-result-si.grib SI-N_height_0_rll_1030_816_0_010.grib
+#grib_compare hirlam-result-si.grib SI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+#if [ $? -ne 0 ];then
+#  echo stability/hirlam failed on CPU
+#  exit 1
+#fi
 
 #grib_compare hirlam-result-li.grib LI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+#if [ $? -ne 0 ];then
+#  echo stability/hirlam failed on CPU
+#  exit 1
+#fi
 
-grib_compare hirlam-result-vti.grib VTI-N_height_0_rll_1030_816_0_010.grib
+  grib_compare hirlam-result-vti.grib VTI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+  if [ $? -ne 0 ];then
+    echo stability/hirlam failed on GPU
+    exit 1
+  fi
 
-grib_compare hirlam-result-cti.grib CTI-N_height_0_rll_1030_816_0_010.grib
+  grib_compare hirlam-result-cti.grib CTI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -ne 0 ];then
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+  if [ $? -ne 0 ];then
+    echo stability/hirlam failed on GPU
+    exit 1
+  fi
 
-grib_compare hirlam-result-tti.grib TTI-N_height_0_rll_1030_816_0_010.grib
+  grib_compare hirlam-result-tti.grib TTI-N_height_0_rll_1030_816_0_010.grib
 
-if [ $? -eq 0 ];then
-  echo stability/hirlam success on CPU!
-else
-  echo stability/hirlam failed on CPU
-  exit 1
-fi
+  if [ $? -eq 0 ];then
+    echo stability/hirlam success on GPU!
+  else
+    echo stability/hirlam failed on GPU
+    exit 1
+  fi
 
 else
   echo "no cuda device found for cuda tests"
