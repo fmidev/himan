bd8c28a1723c3834eef66851dfcd353119dfab02
Mikko Partio <mikko.partio@fmi.fi>
2014-09-26 07:36:04 +0000

    reworked unit tests for modifier

--- himan-tests/unit-tests/source/unit_modifier.cpp
+++ himan-tests/unit-tests/source/unit_modifier.cpp
@@ -9,7 +9,7 @@ using namespace std;
 using namespace himan;
 
 const double kEpsilon = 1e-3;
-const size_t arr_size = 20;
+const size_t arr_size = 9;
 const size_t level_count = 4;
 
 vector<vector<double>> values_all;
@@ -22,8 +22,8 @@ void vdump(const vector<double>& vec)
 	for (size_t i = 0; i < vec.size(); i++, j++)
 	{
 		
-		cout << vec[i] << "\t" ;
-		if (j > 3)
+		cout << " " << vec[i];
+		if (j >= 2)
 		{
 			cout << endl;
 			j = -1;
@@ -34,12 +34,10 @@ void vdump(const vector<double>& vec)
 void dump(const vector<vector<double>>& vec)
 {
 	
-	for (size_t i = 0; i < vec.size(); i++)
+	for (size_t i = 0; i < level_count; i++)
 	{
-		vector<double> data = vec[i];
-
-		cout << "\t=== Level: " << i <<  " ===" << endl;
-		vdump(data);
+		cout << "=== Level: " << level_count-i <<  " ===" << endl;
+		vdump(vec[i]);
 	}
 }
 
@@ -50,44 +48,100 @@ void init()
 	{
 		return;
 	}
+	
+	// HEIGHTS
+	
+	// Level 4, closest to ground, in meters
+	
+	vector<double> heights = {
+		15., 13., 19.,
+		14., 12., 31.,
+		16., 5.,  22.
+	};
 
-	values_all.resize(level_count);
-	heights_all.resize(level_count);
+	heights_all.push_back(heights);
+	
+	// Level 3
+	
+	heights = {
+		27., 33., 29.,
+		24., 42., 41.,
+		29., 17., 32.
+	};
+	
+	heights_all.push_back(heights);
 
-	size_t factor = 0;
+	// Level 2
+	
+	heights = {
+		107., 167., 130.,
+		kFloatMissing, 152., 170.,
+		139., 120., 151.
+	};
+	
+	heights_all.push_back(heights);
 
-	for (size_t i = 0; i < level_count; i++)
-	{
-		vector<double> values, heights;
+	// Level 1
+	
+	heights = {
+		402., 280., 133.,
+		464., 412., 661.,
+		466., 119., kFloatMissing
+	};
+	
+	heights_all.push_back(heights);
 
-		if (i == 3)
-		{
-			factor = 2;
-		}
+	// VALUES
+	
+	// Level 4, closest to ground
+	
+	vector<double> values = {
+		-5., 15., kFloatMissing,
+		-8., 2., 1.,
+		-1., 5,  2.
+	};
 
-		for (size_t j = 0; j < arr_size; j++)
-		{
-	   		values.push_back(static_cast<double> (6*i+5*j+51 - 3 * j * factor));
-	  		heights.push_back(static_cast<double> ((1000+(j*3)) - i*75));
-		}
+	values_all.push_back(values);
+	
+	// Level 3
+	
+	values = {
+		-15., 18., 7,
+		-18., 11., 11.,
+		-10., 6,  -2.
+	};
+	
+	values_all.push_back(values);
 
-		values_all[i] = values;
-		heights_all[i] = heights;
-	}
-/*	
-	for (size_t j = 0; j < arr_size; j++)
-	{
-		lowerHeight.push_back();
-		upperHeight.push_back();
-	}
-*/
-/*
-	cout << "\t=== VALUES ===\n";
+	// Level 2
+	
+	values = {
+		-23., 19., kFloatMissing,
+		-28., 16., 6.,
+		-30., 6,  -1.
+	};
+	
+	values_all.push_back(values);
+
+	// Level 1
+	
+	values = {
+		-26., 20., 7.,
+		-23., 16., 19.,
+		-1., kFloatMissing,  -10.
+	};
+
+	values_all.push_back(values);
+	
+	assert(values_all.size() == 4);
+	assert(heights_all.size() == 4);
+	
+	cout << "=== VALUES ===\n";
 	dump(values_all);
-	cout << "\t===HEIGHTS ===\n";
+	cout << "===HEIGHTS ===\n";
 	dump(heights_all);
 	cout << endl;
-*/
+
 }
 
 BOOST_AUTO_TEST_CASE(MODIFIER_MIN)
@@ -104,9 +158,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MIN)
 
 	auto result = mod->Result();
 
-	// vdump(result);
-	BOOST_REQUIRE(result[1] == 56);
-	BOOST_REQUIRE(result[19] == 50);
+	//vdump(result);
+	BOOST_REQUIRE(result[0] == -26);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[7] == 5);
 }
 
 BOOST_AUTO_TEST_CASE(MODIFIER_MAX)
@@ -123,9 +178,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MAX)
 
 	auto result = mod->Result();
 
-	// vdump(result);
-	BOOST_REQUIRE(result[0] == 69);
-	BOOST_REQUIRE(result[7] == 98);
+	//vdump(result);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[3] == -8);
+	BOOST_REQUIRE(result[7] == 6);
 
 }
 
@@ -144,9 +200,14 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MAXMIN)
 	auto result = mod->Result();
 
 	//vdump(result);
-	BOOST_REQUIRE(result[19] == 50);
-	BOOST_REQUIRE(result[arr_size + 19] == 158);
-
+	// mins
+	BOOST_REQUIRE(result[0] == -26);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[7] == 5);
+	// maxs
+	BOOST_REQUIRE(result[arr_size + 2] == 7);
+	BOOST_REQUIRE(result[arr_size + 3] == -8);
+	BOOST_REQUIRE(result[arr_size + 7] == 6);
 }
 
 BOOST_AUTO_TEST_CASE(MODIFIER_SUM)
@@ -163,10 +224,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_SUM)
 
 	auto result = mod->Result();
 
-	// vdump(result);
-	BOOST_REQUIRE(result[2] == 268);
-	BOOST_REQUIRE(result[18] == 492);
-
+	//vdump(result);
+	BOOST_REQUIRE(result[0] == -69);
+	BOOST_REQUIRE(result[2] == 14);
+	BOOST_REQUIRE(result[8] == -1);
 }
 
 BOOST_AUTO_TEST_CASE(MODIFIER_MEAN)
@@ -183,9 +244,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MEAN)
 
 	auto result = mod->Result();
 
-	// vdump(result);
-	BOOST_REQUIRE(result[2] == 68);
-	BOOST_REQUIRE(result[18] == 132);
+	//vdump(result);
+	BOOST_CHECK_CLOSE(result[0], -22.9134, kEpsilon);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_CHECK_CLOSE(result[8], -1.38372, kEpsilon);
 
 }
 
@@ -197,13 +259,12 @@ BOOST_AUTO_TEST_CASE(MODIFIER_COUNT)
 	auto mod = unique_ptr<modifier_count> (new modifier_count);
 
 	vector<double> findv = {
-		55, 15, 0, 0, 60,
-		52, 3, 15, 10, 100,
-		103, 0, 116, 50, 50,
-		50, 10, 50, 1130, 111
+		-5, 21, 7,
+		-24, 3, 15,
+		-3, kFloatMissing, 116
 	};
 
-	// vdump(findv);
+	//vdump(findv);
 
 	mod->FindValue(findv);
 
@@ -216,8 +277,9 @@ BOOST_AUTO_TEST_CASE(MODIFIER_COUNT)
 
 	// vdump(values);
 	BOOST_REQUIRE(values[0] == 1);
-	BOOST_REQUIRE(values[10] == 2);
-	BOOST_REQUIRE(values[15] == 0);
+	BOOST_REQUIRE(values[1] == 0);
+	BOOST_REQUIRE(values[6] == 2);
+	BOOST_REQUIRE(values[7] == 0);
 	
 }
 
@@ -229,13 +291,12 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 	auto mod = unique_ptr<modifier_findheight> (new modifier_findheight);
 
 	vector<double> findv = {
-		55, 15, 0, 0, 60,
-		52, 3, 15, 10, 100,
-		103, 0, 116, 50, 50,
-		50, 10, 50, 1130, 111
+		-23, 15, 0,
+		-20, 3, 10,
+		-3, 0, 116
 	};
 
-	// vdump(findv);
+	//vdump(findv);
 
 	mod->FindValue(findv);
 
@@ -247,9 +308,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 	auto values = mod->Result();
 
 	//vdump(values);
-	BOOST_REQUIRE(values[0] == 950);
+	BOOST_REQUIRE(values[0] == 107);
 	BOOST_REQUIRE(values[2] == kFloatMissing);
-	BOOST_CHECK_CLOSE(values[19], 874.361, kEpsilon);
+	BOOST_CHECK_CLOSE(values[3], 200, kEpsilon);
+	BOOST_CHECK_CLOSE(values[6], 18.8888, kEpsilon);
 
 	mod->FindNth(2);
 	mod->Clear();
@@ -261,10 +323,11 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 
 	values = mod->Result();
 
-	// vdump(values);
+	//vdump(values);
 
 	BOOST_REQUIRE(values[0] == kFloatMissing);
-	BOOST_CHECK_CLOSE(values[10], 866.111, kEpsilon);
+	BOOST_CHECK_CLOSE(values[5], 66.8, kEpsilon);
+	BOOST_CHECK_CLOSE(values[6], 443.448, kEpsilon);
 
 	mod->FindNth(0);
 	mod->Clear();
@@ -276,11 +339,11 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 
 	values = mod->Result();
 
-	// vdump(values);
+	//vdump(values);
 
-	BOOST_REQUIRE(values[0] == 950);
-	BOOST_CHECK_CLOSE(values[10], 866.111, kEpsilon);
-	BOOST_CHECK_CLOSE(values[19], 874.361, kEpsilon);
+	BOOST_REQUIRE(values[0] == 107);
+	BOOST_REQUIRE(values[2] == kFloatMissing);
+	BOOST_CHECK_CLOSE(values[5], 321.077, kEpsilon);
 
 }
 
@@ -292,13 +355,12 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDVALUE)
 	auto mod = unique_ptr<modifier_findvalue> (new modifier_findvalue);
 
 	vector<double> findh = {
-		997, 0, 0, 0, 60,
-		52, 3, 15, 900, 100,
-		103, 0, 116, 50, 50,
-		50, 10, 50, 1130, 111
+		50, 0, 51,
+		52, 3, 15, 
+		103, 0, 416, 
 	};
 
-	//vdump(findh);
+//	vdump(findh);
 
 	mod->FindValue(findh);
 
@@ -309,17 +371,10 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDVALUE)
 
 	auto values = mod->Result();
 
-	//vdump(values);
-	BOOST_REQUIRE(values[0] == 51.24);
-	BOOST_REQUIRE(values[2] == kFloatMissing);
-	BOOST_REQUIRE(values[8] == 100.92);
+//	vdump(values);
+	BOOST_REQUIRE(values[0] == -17.3);
+	BOOST_REQUIRE(values[1] == 15); // clamp
+	BOOST_REQUIRE(values[2] == 7);
+	BOOST_REQUIRE(values[8] == kFloatMissing);
 
-	mod->FindNth(0);
-	mod->Clear();
-
-	for (size_t i = 0; i < level_count; i++)
-	{
-		mod->Process(values_all[i], heights_all[i]);
-	}
-	
 }
