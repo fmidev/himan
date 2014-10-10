3c9d5b0e61906ec1ac59b9b04442adf7181f63e0
Jussi Perämäki <jussi.peramaki@fmi.fi>
2014-10-10 10:09:55 +0000

    ec test for ncl

---
+++ himan-tests/ncl/ec.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan-bin/himan"
+fi
+
+rm -f H0C-M_*
+
+$HIMAN -d 5 -f ec.json -t grib ec_source.grib2
+
+grib_compare ec_result.grib ./H0C-M_height_0_rll_465_461_0_003.grib
+
+if [ $? -eq 0 ];then
+  echo ncl/ec success!
+else
+  echo ncl/ec failed
+  exit 1
+fi
