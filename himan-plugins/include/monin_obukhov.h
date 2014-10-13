f9b3387a92f2d2ff88ca653b0b3627916fd14a33
Andreas Tack <andreas.tack@fmi.fi>
2014-10-13 07:52:37 +0000

    Add monin-obukhov length plug-in

---
+++ himan-plugins/include/monin_obukhov.h
@@ -0,0 +1,68 @@
+/*
+ * monin_obukhov.h
+ *
+ *  Created on: Aug 8, 2014
+ *      Author: Tack
+ */
+
+#ifndef MONIN_OBUKHOV_H
+#define MONIN_OBUKHOV_H
+
+#include "compiled_plugin.h"
+#include "compiled_plugin_base.h"
+
+namespace himan
+{
+namespace plugin
+{
+
+/**
+ * @class monin_obukhov
+ *
+ * @brief Calculate the inverse of the monin-obukhov length.
+ *
+ */
+
+class monin_obukhov : public compiled_plugin, private compiled_plugin_base
+{
+public:
+    monin_obukhov();
+
+    inline virtual ~monin_obukhov() {}
+
+    monin_obukhov(const monin_obukhov& other) = delete;
+    monin_obukhov& operator=(const monin_obukhov& other) = delete;
+
+    virtual void Process(std::shared_ptr<const plugin_configuration> conf);
+
+    virtual std::string ClassName() const
+    {
+        return "himan::plugin::monin_obukhov";
+    }
+
+    virtual HPPluginClass PluginClass() const
+    {
+        return kCompiled;
+    }
+
+    virtual HPVersionNumber Version() const
+    {
+        return HPVersionNumber(0, 1);
+    }
+
+private:
+    virtual void Calculate(std::shared_ptr<info> theTargetInfo, unsigned short theThreadIndex);
+
+};
+
+// the class factory
+
+extern "C" std::shared_ptr<himan_plugin> create()
+{
+    return std::shared_ptr<monin_obukhov> (new monin_obukhov());
+}
+
+} // namespace plugin
+} // namespace himan
+
+#endif /* MONIN_OBUKHOV_H */
---
+++ himan-plugins/source/monin_obukhov.cpp
@@ -0,0 +1,130 @@
+/**
+ * @file monin_obukhov.cpp
+ *
+ * Claculates the inverse of the Monin-Obukhov length.
+ *
+ * @date Aug 8, 2014
+ * @author Tack
+ */
+
+#include <boost/lexical_cast.hpp>
+
+#include "monin_obukhov.h"
+#include "logger_factory.h"
+#include "level.h"
+#include "forecast_time.h"
+#include "logger_factory.h"
+
+using namespace std;
+using namespace himan::plugin;
+
+monin_obukhov::monin_obukhov()
+{
+	itsClearTextFormula = "1/L = -(k*g*Q)/(u*^3*T)";
+
+	itsLogger = logger_factory::Instance()->GetLog("monin_obukhov");
+}
+
+void monin_obukhov::Process(std::shared_ptr<const plugin_configuration> conf)
+{
+	Init(conf);
+
+	/*
+	 * Set target parameter properties
+	 * - name PARM_NAME, this name is found from neons. For example: T-K
+	 * - univ_id UNIV_ID, newbase-id, ie code table 204
+	 * - grib1 id must be in database
+	 * - grib2 descriptor X'Y'Z, http://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_table4-2.shtml
+	 *
+	 */
+
+	param theRequestedParam("MOL-M", 1204);
+
+	// If this param is also used as a source param for other calculations
+
+	SetParams({theRequestedParam});
+
+	Start();
+}
+
+/*
+ * Calculate()
+ *
+ * This function does the actual calculation.
+ */
+
+void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short threadIndex)
+{
+
+	/*
+	 * Required source parameters
+	 *
+	 * eg. param PParam("P-Pa"); for pressure in pascals
+	 *
+	 */
+
+	const param TParam("T-K"); // ground Temperature
+	const param QParam("FLSEN-JM2"); // surface heat flux
+	const param U_SParam("FRVEL-MS"); // friction velocity
+
+	// ----	
+
+	auto myThreadedLogger = logger_factory::Instance()->GetLog("monin_obukhov Thread #" + boost::lexical_cast<string> (threadIndex));
+
+	// Current time and level
+	
+	forecast_time forecastTime = myTargetInfo->Time();
+	level forecastLevel = level(himan::kHeight, 0, "Height"); 
+	
+	myThreadedLogger->Debug("Calculating time " + static_cast<string> (*forecastTime.ValidDateTime()) + " level " + static_cast<string> (forecastLevel));
+
+	info_t TInfo = Fetch(forecastTime, forecastLevel, TParam, false);
+	info_t QInfo = Fetch(forecastTime, forecastLevel, QParam, false);
+	info_t U_SInfo = Fetch(forecastTime, forecastLevel, U_SParam, false);
+
+	double ForecastStepSize = 3600; //step size in seconds
+
+	if (!TInfo || !QInfo || !U_SInfo)
+	{
+		myThreadedLogger->Info("Skipping step " + boost::lexical_cast<string> (forecastTime.Step()) + ", level " + static_cast<string> (forecastLevel));
+
+		if (itsConfiguration->StatisticsEnabled())
+		{
+			// When time or level is skipped, all values are missing
+			itsConfiguration->Statistics()->AddToMissingCount(myTargetInfo->Data()->Size());
+			itsConfiguration->Statistics()->AddToValueCount(myTargetInfo->Data()->Size());
+		}
+
+		return;
+
+	}
+
+	string deviceType = "CPU";
+
+	LOCKSTEP(myTargetInfo, TInfo, QInfo, U_SInfo)
+	{
+
+		double T = TInfo->Value();
+		double Q = QInfo->Value()/ForecastStepSize; // divide by time step to obtain Watts
+		double U_S = U_SInfo->Value();
+		
+		double mol(kFloatMissing);
+
+		if (T == kFloatMissing || Q == kFloatMissing || U_S == kFloatMissing)
+		{
+			continue;
+		}
+
+		/* Calculation of the inverse of Monin-Obukhov length to avoid division by 0 */
+		
+		if (U_S != 0.0)
+		{
+			mol = -constants::kG * constants::kK * Q / (U_S * U_S * U_S * T);
+		}
+		myTargetInfo->Value(mol);
+
+	}
+
+	myThreadedLogger->Info("[" + deviceType + "] Missing values: " + boost::lexical_cast<string> (myTargetInfo->Data()->MissingCount()) + "/" + boost::lexical_cast<string> (myTargetInfo->Data()->Size()));
+
+}
