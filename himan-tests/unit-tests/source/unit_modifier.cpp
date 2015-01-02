688672ddd89e8e480c1207a1fc6939b0051f64b8
Mikko Partio <mikko.partio@fmi.fi>
2015-01-02 08:57:14 +0000

    new tests

--- himan-tests/unit-tests/source/unit_modifier.cpp
+++ himan-tests/unit-tests/source/unit_modifier.cpp
@@ -13,7 +13,8 @@ const size_t arr_size = 9;
 const size_t level_count = 4;
 
 vector<vector<double>> values_all;
-vector<vector<double>> heights_all;
+vector<vector<double>> heights_all_meters;
+vector<vector<double>> heights_all_pascals; // hpa actually
 
 void vdump(const vector<double>& vec)
 {
@@ -41,58 +42,8 @@ void dump(const vector<vector<double>>& vec)
 	}
 }
 
-void init()
+void init_values()
 {
-
-	if (values_all.size() > 0)
-	{
-		return;
-	}
-	
-	// HEIGHTS
-	
-	// Level 4, closest to ground, in meters
-	
-	vector<double> heights = {
-		15., 13., 19.,
-		14., 12., 31.,
-		16., 5.,  22.
-	};
-
-	heights_all.push_back(heights);
-	
-	// Level 3
-	
-	heights = {
-		27., 33., 29.,
-		24., 42., 41.,
-		29., 17., 32.
-	};
-	
-	heights_all.push_back(heights);
-
-	// Level 2
-	
-	heights = {
-		107., 167., 130.,
-		kFloatMissing, 152., 170.,
-		139., 120., 151.
-	};
-	
-	heights_all.push_back(heights);
-
-	// Level 1
-	
-	heights = {
-		402., 280., 133.,
-		464., 412., 661.,
-		466., 119., kFloatMissing
-	};
-	
-	heights_all.push_back(heights);
-
-	// VALUES
-	
 	// Level 4, closest to ground
 	
 	vector<double> values = {
@@ -134,14 +85,126 @@ void init()
 	values_all.push_back(values);
 	
 	assert(values_all.size() == 4);
-	assert(heights_all.size() == 4);
-	
+
 	cout << "=== VALUES ===\n";
 	dump(values_all);
-	cout << "===HEIGHTS ===\n";
-	dump(heights_all);
 	cout << endl;
+}
 
+void init_heights_in_pascals()
+{
+	// Level 4, closest to ground, in hectopascals
+	
+	vector<double> heights = {
+		995., 1000., 991.,
+		1023., 1023., 1025.,
+		994., 990.,  1000.
+	};
+
+	heights_all_pascals.push_back(heights);
+	
+	// Level 3
+	
+	heights = {
+		920., 930, 915.,
+		940., 1000., 1000,
+		901., 899., 950.
+	};
+	
+	heights_all_pascals.push_back(heights);
+
+	// Level 2
+	
+	heights = {
+		850., 850, 910.,
+		kFloatMissing, 900., 920.,
+		880., 800., 830.
+	};
+	
+	heights_all_pascals.push_back(heights);
+
+	// Level 1
+	
+	heights = {
+		700., 700., 710.,
+		730., 730., 500.,
+		680., 600., kFloatMissing
+	};
+	
+	heights_all_pascals.push_back(heights);
+
+
+	assert(heights_all_pascals.size() == 4);
+	
+	
+	cout << "=== HEIGHTS IN HECTOPASCALS ===\n";
+	dump(heights_all_pascals);
+	cout << endl;
+}
+
+void init_heights_in_meters()
+{
+
+	// Level 4, closest to ground, in meters
+	
+	vector<double> heights = {
+		15., 13., 19.,
+		14., 12., 31.,
+		16., 5.,  22.
+	};
+
+	heights_all_meters.push_back(heights);
+	
+	// Level 3
+	
+	heights = {
+		27., 33., 29.,
+		24., 42., 41.,
+		29., 17., 32.
+	};
+	
+	heights_all_meters.push_back(heights);
+
+	// Level 2
+	
+	heights = {
+		107., 167., 130.,
+		kFloatMissing, 152., 170.,
+		139., 120., 151.
+	};
+	
+	heights_all_meters.push_back(heights);
+
+	// Level 1
+	
+	heights = {
+		402., 280., 133.,
+		464., 412., 661.,
+		466., 119., kFloatMissing
+	};
+	
+	heights_all_meters.push_back(heights);
+
+
+	assert(heights_all_meters.size() == 4);
+	
+	
+	cout << "=== HEIGHTS IN METERS ===\n";
+	dump(heights_all_meters);
+	cout << endl;
+}
+
+void init()
+{
+
+	if (values_all.size() > 0)
+	{
+		return;
+	}
+
+	init_values();
+	init_heights_in_meters();
+	init_heights_in_pascals();
 }
 
 BOOST_AUTO_TEST_CASE(MODIFIER_MIN)
@@ -149,14 +212,36 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MIN)
 
 	init();
 
-	auto mod = unique_ptr<modifier_min> (new modifier_min);
+	modifier_min mod;
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto result = mod->Result();
+	auto result = mod.Result();
+
+	//vdump(result);
+	BOOST_REQUIRE(result[0] == -26);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[7] == 5);
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_MIN_PA)
+{
+
+	init();
+
+	modifier_min mod;
+
+	mod.HeightInMeters(false);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto result = mod.Result();
 
 	//vdump(result);
 	BOOST_REQUIRE(result[0] == -26);
@@ -169,14 +254,36 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MAX)
 
 	init();
 
-	auto mod = unique_ptr<modifier_max> (new modifier_max);
+	modifier_max mod;
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto result = mod->Result();
+	auto result = mod.Result();
+
+	//vdump(result);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[3] == -8);
+	BOOST_REQUIRE(result[7] == 6);
+
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_MAX_PA)
+{
+
+	init();
+
+	modifier_max mod;
+	mod.HeightInMeters(false);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto result = mod.Result();
 
 	//vdump(result);
 	BOOST_REQUIRE(result[2] == 7);
@@ -190,14 +297,40 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MAXMIN)
 
 	init();
 
-	auto mod = unique_ptr<modifier_maxmin> (new modifier_maxmin);
+	modifier_maxmin mod;
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto result = mod->Result();
+	auto result = mod.Result();
+
+	//vdump(result);
+	// mins
+	BOOST_REQUIRE(result[0] == -26);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_REQUIRE(result[7] == 5);
+	// maxs
+	BOOST_REQUIRE(result[arr_size + 2] == 7);
+	BOOST_REQUIRE(result[arr_size + 3] == -8);
+	BOOST_REQUIRE(result[arr_size + 7] == 6);
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_MAXMIN_PA)
+{
+
+	init();
+
+	modifier_maxmin mod;
+	mod.HeightInMeters(false);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto result = mod.Result();
 
 	//vdump(result);
 	// mins
@@ -215,14 +348,35 @@ BOOST_AUTO_TEST_CASE(MODIFIER_SUM)
 
 	init();
 
-	auto mod = unique_ptr<modifier_sum> (new modifier_sum);
+	modifier_sum mod;
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto result = mod->Result();
+	auto result = mod.Result();
+
+	//vdump(result);
+	BOOST_REQUIRE(result[0] == -69);
+	BOOST_REQUIRE(result[2] == 14);
+	BOOST_REQUIRE(result[8] == -1);
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_SUM_PA)
+{
+
+	init();
+
+	modifier_sum mod;
+	mod.HeightInMeters(false);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto result = mod.Result();
 
 	//vdump(result);
 	BOOST_REQUIRE(result[0] == -69);
@@ -235,14 +389,14 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MEAN)
 
 	init();
 
-	auto mod = unique_ptr<modifier_mean> (new modifier_mean);
+	modifier_mean mod;
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto result = mod->Result();
+	auto result = mod.Result();
 
 	//vdump(result);
 	BOOST_CHECK_CLOSE(result[0], -22.9134, kEpsilon);
@@ -251,12 +405,34 @@ BOOST_AUTO_TEST_CASE(MODIFIER_MEAN)
 
 }
 
+BOOST_AUTO_TEST_CASE(MODIFIER_MEAN_PA)
+{
+
+	init();
+
+	modifier_mean mod;
+	mod.HeightInMeters(false);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto result = mod.Result();
+
+	//vdump(result);
+	BOOST_CHECK_CLOSE(result[0], -19.50847, kEpsilon);
+	BOOST_REQUIRE(result[2] == 7);
+	BOOST_CHECK_CLOSE(result[8], -1.05882, kEpsilon);
+
+}
+
 BOOST_AUTO_TEST_CASE(MODIFIER_COUNT)
 {
 
 	init();
 
-	auto mod = unique_ptr<modifier_count> (new modifier_count);
+	modifier_count mod;
 
 	vector<double> findv = {
 		-5, 21, 7,
@@ -266,16 +442,49 @@ BOOST_AUTO_TEST_CASE(MODIFIER_COUNT)
 
 	//vdump(findv);
 
-	mod->FindValue(findv);
+	mod.FindValue(findv);
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto values = mod->Result();
+	auto values = mod.Result();
 
 	// vdump(values);
+	BOOST_REQUIRE(values[0] == 1);
+	BOOST_REQUIRE(values[1] == 0);
+	BOOST_REQUIRE(values[6] == 2);
+	BOOST_REQUIRE(values[7] == 0);
+	
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_COUNT_PA)
+{
+
+	init();
+
+	modifier_count mod;
+	mod.HeightInMeters(false);
+
+	vector<double> findv = {
+		-5, 21, 7,
+		-24, 3, 15,
+		-3, kFloatMissing, 116
+	};
+
+	//vdump(findv);
+
+	mod.FindValue(findv);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto values = mod.Result();
+
+	//vdump(values);
 	BOOST_REQUIRE(values[0] == 1);
 	BOOST_REQUIRE(values[1] == 0);
 	BOOST_REQUIRE(values[6] == 2);
@@ -288,7 +497,7 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 
 	init();
 
-	auto mod = unique_ptr<modifier_findheight> (new modifier_findheight);
+	modifier_findheight mod;
 
 	vector<double> findv = {
 		-23, 15, 0,
@@ -298,14 +507,14 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 
 	//vdump(findv);
 
-	mod->FindValue(findv);
+	mod.FindValue(findv);
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto values = mod->Result();
+	auto values = mod.Result();
 
 	//vdump(values);
 	BOOST_REQUIRE(values[0] == 107);
@@ -313,15 +522,15 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 	BOOST_CHECK_CLOSE(values[3], 200, kEpsilon);
 	BOOST_CHECK_CLOSE(values[6], 18.8888, kEpsilon);
 
-	mod->FindNth(2);
-	mod->Clear();
+	mod.FindNth(2);
+	mod.Clear();
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	values = mod->Result();
+	values = mod.Result();
 
 	//vdump(values);
 
@@ -329,15 +538,15 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 	BOOST_CHECK_CLOSE(values[5], 66.8, kEpsilon);
 	BOOST_CHECK_CLOSE(values[6], 443.448, kEpsilon);
 
-	mod->FindNth(0);
-	mod->Clear();
+	mod.FindNth(0);
+	mod.Clear();
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	values = mod->Result();
+	values = mod.Result();
 
 	//vdump(values);
 
@@ -347,12 +556,77 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT)
 
 }
 
+BOOST_AUTO_TEST_CASE(MODIFIER_FINDHEIGHT_PA)
+{
+
+	init();
+
+	modifier_findheight mod;
+	mod.HeightInMeters(false);
+
+	vector<double> findv = {
+		-23, 15, 0,
+		-20, 3, 10,
+		-3, 0, 116
+	};
+
+	//vdump(findv);
+
+	mod.FindValue(findv);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto values = mod.Result();
+
+	//vdump(values);
+	BOOST_REQUIRE(values[0] == 850);
+	BOOST_REQUIRE(values[2] == kFloatMissing);
+	BOOST_CHECK_CLOSE(values[3], 856, kEpsilon);
+	BOOST_CHECK_CLOSE(values[6], 973.33333, kEpsilon);
+
+	mod.FindNth(2);
+	mod.Clear();
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	values = mod.Result();
+
+	//vdump(values);
+
+	BOOST_REQUIRE(values[0] == kFloatMissing);
+	BOOST_CHECK_CLOSE(values[5], 984, kEpsilon);
+	BOOST_CHECK_CLOSE(values[6], 693.79310, kEpsilon);
+
+	mod.FindNth(0);
+	mod.Clear();
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	values = mod.Result();
+
+	//vdump(values);
+
+	BOOST_REQUIRE(values[0] == 850);
+	BOOST_REQUIRE(values[2] == kFloatMissing);
+	BOOST_CHECK_CLOSE(values[5], 790.76923, kEpsilon);
+
+}
+
 BOOST_AUTO_TEST_CASE(MODIFIER_FINDVALUE)
 {
 
 	init();
 
-	auto mod = unique_ptr<modifier_findvalue> (new modifier_findvalue);
+	modifier_findvalue mod;
 
 	vector<double> findh = {
 		50, 0, 51,
@@ -362,19 +636,52 @@ BOOST_AUTO_TEST_CASE(MODIFIER_FINDVALUE)
 
 //	vdump(findh);
 
-	mod->FindValue(findh);
+	mod.FindValue(findh);
 
 	for (size_t i = 0; i < level_count; i++)
 	{
-		mod->Process(values_all[i], heights_all[i]);
+		mod.Process(values_all[i], heights_all_meters[i]);
 	}
 
-	auto values = mod->Result();
+	auto values = mod.Result();
 
-//	vdump(values);
+	//vdump(values);
 	BOOST_REQUIRE(values[0] == -17.3);
 	BOOST_REQUIRE(values[1] == 15); // clamp
 	BOOST_REQUIRE(values[2] == 7);
 	BOOST_REQUIRE(values[8] == kFloatMissing);
+
+}
+
+BOOST_AUTO_TEST_CASE(MODIFIER_FINDVALUE_PA)
+{
+
+	init();
+
+	modifier_findvalue mod;
+	mod.HeightInMeters(false);
+
+	vector<double> findh = {
+		995, 1010, 700,
+		852, 733, 850, 
+		690, 899, 975, 
+	};
+
+//	vdump(findh);
+
+	mod.FindValue(findh);
+
+	for (size_t i = 0; i < level_count; i++)
+	{
+		mod.Process(values_all[i], heights_all_pascals[i]);
+	}
+
+	auto values = mod.Result();
+
+	//vdump(values);
+	BOOST_REQUIRE(values[0] == -5);
+	BOOST_REQUIRE(values[1] == kFloatMissing); 
+	BOOST_CHECK_CLOSE(values[3], -20.0952, kEpsilon);
+	BOOST_REQUIRE(values[8] == 0);
 
 }
