4e91b651f36c0a336105769e322876666e9886a4
Mikko Partio <mikko.partio@fmi.fi>
2015-01-02 12:30:24 +0000

    add test for luatool

---
+++ himan-tests/luatool/npres.lua
@@ -0,0 +1,39 @@
+logger:Info("Calculating N at " .. current_level:GetValue() .. " hPa")
+
+nparam = param("N-PRCNT")
+
+hitool:SetHeightUnit(HPParameterUnit.kHPa)
+data = hitool:VerticalValue(nparam, current_level:GetValue())
+
+scale = false
+
+if not data then
+	nparam = param("N-0TO1")
+	logger:Info("Trying N-0TO1")
+
+	data = hitool:VerticalValue(nparam, current_level:GetValue())
+	
+	if not data then
+		logger:Error("Data not found")
+		return
+	end
+
+	scale = true
+end
+
+if scale then
+	logger:Info("Scaling data")
+	for i = 1, #data do
+		local val = data[i]
+		if value then
+			data[i] = val * 100 -- scale to percents
+		end
+	end
+end
+
+print("data size: " .. #data)
+result:SetParam(nparam)
+result:SetValues(data)
+
+logger:Info("Writing results")
+luatool:WriteToFile(result)
