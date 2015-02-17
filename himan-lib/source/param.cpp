0e310f1156a6b58764ab1be0f9524b520a10dfcc
Mikko Partio <mikko.partio@fmi.fi>
2015-02-17 07:42:33 +0000

    Table version was missing from debug output

--- himan-lib/source/param.cpp
+++ himan-lib/source/param.cpp
@@ -363,6 +363,7 @@ ostream& param::Write(ostream& file) const
 	file << "__itsGribParameter__ " << itsGribParameter << endl;
 	file << "__itsGribCategory__ " << itsGribCategory << endl;
 	file << "__itsGribDiscipline__ " << itsGribDiscipline << endl;
+	file << "__itsGribTableVersion__ " << itsGribTableVersion << endl;
 	file << "__itsUnit__ " << itsUnit << endl;
 	file << "__itsVersion__ " << itsVersion << endl;
 	file << "__itsInterpolationMethod__ " << HPInterpolationMethodToString.at(itsInterpolationMethod) << endl;
