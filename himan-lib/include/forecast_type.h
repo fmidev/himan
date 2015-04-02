ab7c9781753857da10e0a4c6998a957969859694
Mikko Partio <mikko.partio@fmi.fi>
2015-04-02 12:12:44 +0000

    initial import for a small class describing a forecast type (deterministic, eps, etc)

---
+++ himan-lib/include/forecast_type.h
@@ -0,0 +1,66 @@
+/** 
+ * @file   forecast_type.h
+ * @author partio
+ *
+ * @date   February 26, 2015, 4:27 PM
+ * 
+ * @brief Simple wrapper for forecast types which are defined in radon database.
+ * 
+ * Current values are:
+ * 
+ * 0: unknown type
+ * 1: deterministic forecast
+ * 2: analysis
+ * 3: eps control forecast
+ * 4: eps perturbation
+ * 
+ */
+
+#ifndef FORECAST_TYPE_H
+#define	FORECAST_TYPE_H
+
+#include "himan_common.h"
+
+namespace himan
+{
+class forecast_type
+{
+
+public:
+
+	forecast_type() {}
+	forecast_type(HPForecastType theType);
+	forecast_type(HPForecastType theType, double theValue);
+	
+	bool operator==(const forecast_type& theType) const;
+	bool operator!=(const forecast_type& theType) const;
+
+	std::string ClassName() const
+    {
+        return "himan::forecast_type";
+    }
+	
+	HPForecastType Type() const;
+	void Type(HPForecastType theType);
+	
+	double Value() const;
+	void Value(double theValue);
+	
+	std::ostream& Write(std::ostream& file) const;
+
+private:
+	HPForecastType itsForecastType;
+	double itsForecastTypeValue;
+	
+};
+
+inline
+std::ostream& operator<<(std::ostream& file, const forecast_type& ob)
+{
+    return ob.Write(file);
+}
+
+} // namespace himan
+
+#endif	/* FORECAST_TYPE_H */
+
---
+++ himan-lib/source/forecast_type.cpp
@@ -0,0 +1,58 @@
+#include <forecast_type.h>
+
+using namespace himan;
+
+forecast_type::forecast_type(HPForecastType theType)
+	: itsForecastType(theType)
+	, itsForecastTypeValue(kHPMissingValue)
+{}
+
+forecast_type::forecast_type(HPForecastType theType, double theValue)
+	: itsForecastType(theType)
+	, itsForecastTypeValue(theValue)
+{}
+
+HPForecastType forecast_type::Type() const
+{
+	return itsForecastType;
+}
+
+void forecast_type::Type(HPForecastType theForecastType)
+{
+	itsForecastType = theForecastType;
+}
+
+double forecast_type::Value() const
+{
+	return itsForecastTypeValue;
+}
+
+void forecast_type::Value(double theValue)
+{
+	itsForecastTypeValue = theValue;
+}
+
+std::ostream& forecast_type::Write(std::ostream& file) const
+{
+
+    file << "<" << ClassName() << ">" << std::endl;
+    file << "__itsForecastType__ " << HPForecastTypeToString.at(itsForecastType) << std::endl;
+    file << "__itsForecastTypeValue__ " << itsForecastTypeValue << std::endl;
+
+    return file;
+}
+
+bool forecast_type::operator==(const forecast_type& other) const
+{
+    if (this == &other)
+    {
+        return true;
+    }
+
+    return (itsForecastType == other.itsForecastType && itsForecastTypeValue == other.itsForecastTypeValue);
+}
+
+bool forecast_type::operator!=(const forecast_type& other) const
+{
+    return !(*this == other);
+}
