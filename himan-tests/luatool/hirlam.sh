b4b9227b5515164d4fb6239da1e38bed1f45db27
Mikko Partio <mikko.partio@fmi.fi>
2015-05-11 14:15:54 +0000

    test for pressure level interpolation

---
+++ himan-tests/luatool/hirlam.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$HIMAN" ]; then
+	export HIMAN="../../himan-bin/himan"
+fi
+
+rm -f N*grib
+
+$HIMAN -d 5 -f hirlam.json hirlam_npres_source.grib -s stat --no-cuda
+
+grib_compare hirlam_npres_result.grib ./N-0TO1_pressure_600_rll_1030_816_0_041.grib
+
+if [ $? -eq 0 ];then
+  echo luatool/hirlam success on CPU!
+else
+  echo luatool/hirlam failed on CPU
+  exit 1
+fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  rm -f DPDEF*grib
+
+  $HIMAN -s stat -d 5 -f hirlam.json hirlam_npres_source.grib
+
+  grib_compare -A 0.001 hirlam_npres_result.grib ./N-0TO1_pressure_600_rll_1030_816_0_041.grib
+
+  if [ $? -eq 0 ];then
+    echo luatool/hirlam success on GPU!
+  else
+    echo luatool/hirlam failed on GPU
+  fi
+else
+  echo "no cuda device found for cuda tests"
+fi
+
