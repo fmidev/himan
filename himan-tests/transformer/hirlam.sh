caee1269c58cbfa6524def5c3018ac16316e4877
Mikko Partio <mikko.partio@fmi.fi>
2015-06-26 10:07:46 +0000

    Add GPU test

--- himan-tests/transformer/hirlam.sh
+++ himan-tests/transformer/hirlam.sh
@@ -37,7 +37,7 @@ else
   echo "no cuda device found for cuda tests"
 fi
 
-$HIMAN -d 5 -f tk2tc_hl_pres.json -t grib --no-cuda -s tk2tc_hl_pres_nocuda hl_source_pres.grib
+$HIMAN -d 5 -f tk2tc_hl_pres.json --no-cuda -s tk2tc_hl_pres_nocuda hl_source_pres.grib
 
 grib_compare -A 0.0001 hl_result_pres.grib ./T-C_pressure_850_rll_582_448_0_001.grib
 
@@ -47,4 +47,23 @@ else
   echo tk2tc_pres/hl failed
   exit 1
 fi
+
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  rm -f T-C_*.grib
+
+  $HIMAN -d 5 -f tk2tc_hl_pres.json -s tk2tc_hl_pres_cuda hl_source_pres.grib
+
+  grib_compare -A 0.0001 hl_result_pres.grib ./T-C_pressure_850_rll_582_448_0_001.grib
+
+  if [ $? -eq 0 ];then
+    echo tk2tc_pres/hl success on GPU!
+  else
+    echo tk2tc_pres/hl failed on GPU
+    exit 1
+  fi
+else
+  echo "no cuda device found for cuda tests"
+fi
+
 
