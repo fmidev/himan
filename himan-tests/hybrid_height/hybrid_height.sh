d8e56a3400c9fedd5d932e9ee339a96bdb50e352
Mikko Partio <mikko.partio@fmi.fi>
2015-08-18 07:39:03 +0000

    new interpolation methods, commit part 3

--- himan-tests/hybrid_height/hybrid_height.sh
+++ himan-tests/hybrid_height/hybrid_height.sh
@@ -24,7 +24,7 @@ if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
 
   $HIMAN -d 5 -f hybrid_height.json -t grib hl_source_geop.grib -s stat
 
-  grib_compare result_gpu.grib HL-M_hybrid_64_polster_290_225_0_003.grib
+  grib_compare -b referenceValue -A 0.1 result_gpu.grib HL-M_hybrid_64_polster_290_225_0_003.grib
 
   if [ $? -eq 0 ];then
     echo hybrid_height/hirlam success on gpu!
