5c78e312695c3d2286d2e5163b4ae17e6d586c60
Päivi Junila <paivi.junila@fmi.fi>
2015-12-09 13:35:23 +0000

    new version of boundary-layer turbulence, using land-sea mask

--- himan-tests/bl_turbulence/bl_turbulence.lua
+++ himan-tests/bl_turbulence/bl_turbulence.lua
@@ -3,8 +3,7 @@
 -- Requirements:
 -- - surface (10m) wind speed
 -- - TKE and wind on model levels (+model level heights) in the surface layer (0...500m)
--- - land/sea/lake differentiation (topography)
--- - note: lakes should be included in "over sea" calculation, but topography probably >0 over lakes (?)
+-- - land/sea/lake differentiation 
 -- 
 -- The parameter gets (integer) values between 0-5:
 --   0 = nil turbulence/smooth
@@ -20,12 +19,12 @@
 -- junila 11/2015
  
 logger:Info("Calculating boundary layer turbulence")
+
+turbparam = param("BLTURB-N")
   
 local kFloatMissing = kFloatMissing
  
 -- Surface (10m) wind speed in m/s
--- ws = param("FF-MS")
--- wsms = hitool:VerticalValue(ws,10)
 local ws = param("FF-MS")
 local l = level(HPLevelType.kHeight, 10) 
 
@@ -35,10 +34,11 @@ if not wsms then
     logger:Error("No data found")
     return
 end
+
  
 -- Max tke in the layer 0-500m (note: max tke is generally found at the 1st model level above the surface)
 local tke = param("TKEN-JKG")
-local maxtke = hitool:VerticalMaximum(tke,0,500)
+local maxtke = hitool:VerticalMaximum(tke,10,500)
 
 if not maxtke then
     logger:Error("No data found")
@@ -47,14 +47,17 @@ end
  
 logger:Info("Calculating wind shear")
 
--- Low level wind shear 0-1000ft (0-304.8m) scaled to kt/1000ft [kt]
-local dz = 304.8
+-- Low level wind shear 0-1000ft (de facto: 10-304.8m) scaled to kt/1000ft [kt]
+-- Possible to use u and v components of wind as they are in model output (rotated latlon)
+-- because only change in wind direction and speed is needed, not absolutely correct direction
+
+local dz = 304.8 - 10.0
 local U_HIR = param("U-MS")
 local V_HIR = param("V-MS")
 local u_dz = hitool:VerticalValue(U_HIR,dz)
-local u_0 = hitool:VerticalValue(U_HIR,0)
+local u_0 = hitool:VerticalValue(U_HIR,10)
 local v_dz = hitool:VerticalValue(V_HIR,dz)
-local v_0 = hitool:VerticalValue(U_HIR,0)
+local v_0 = hitool:VerticalValue(V_HIR,10)
 
 if not u_dz or not u_0 or not v_dz or not v_0 then
     logger:Error("No data found")
@@ -82,13 +85,12 @@ end
     end
 
 
--- TOPO  = topography [m] (TOPO<=0 over sea)
--- [m2/s2] -> [m]
+-- land-sea-mask, proportion from 0 to 1 where 1=land, 0=sea
+local landseamask = param("LC-0TO1")
+local lev = level(HPLevelType.kHeight, 0) 
+local lcmask = luatool:Fetch(current_time, lev, landseamask)
 
-local geopotential = param("Z-M2S2")
-local topo = hitool:VerticalValue(geopotential,0)
-
-if not topo then
+if not lcmask then
     logger:Error("No data found")
     return
 end
@@ -98,72 +100,53 @@ end
 
 	local turbulence = {}
 	
-	for i = 1, #maxtke do
-			
+	for i = 1, #maxtke do			
 		local wskt = wsms[i]/0.514
-		local TOPO = topo[i]/9.81
+		local lc = lcmask[i]
 		local maxTKE = maxtke[i] 
 		local shear = windshear[i]
-
 		local turb = kFloatMissing
-
 		if wskt ~= kFloatMissing and TOPO ~= kFloatMissing and maxTKE ~= kFloatMissing and shear ~= kFloatMissing then
-
 		turb = 0
-
 			-- Turbulence over land areas
-
-				if TOPO>0 then
-
+				if lc > 0.5 then
   					-- MOD over land
   					if wskt>13 and maxTKE>2.5 then
-
  				   		if wskt>16 then
-				    		  turb = 3 
-				    		elseif maxTKE>3.5 or shear>25 then
+				    		  turb = 3
+						end
+				    		if maxTKE>3.5 or shear>25 then
 				    		  turb = 3 
 				    		end
-
- 				 	elseif wskt>10 and shear*maxTKE>65 then
+					end
+ 				 	if wskt>10 and shear*maxTKE>65 then
  				  		turb = 3
- 				 	end
-	 
+ 				 	end	 
 				  	-- SEV over land
 				  	if wskt>=20 and maxTKE>3 then
-
 				    		if wskt>22 then
+				    		  turb = 5
+						end 
+				    		if maxTKE>4 then
 				    		  turb = 5 
-				    		elseif maxTKE>4 then
-				    		  turb = 5 
-				    		end
-			
-				  	end
-
-			-- Turbulence at sea (or over lakes)
-
-				else
-
-				  	-- MOD over sea
-				  	if wskt>=35 and maxTKE>3 then
+				    		end			
+				  	end					
+				end
+			-- Turbulence at sea (or over lakes)	
+				if lc <= 0.5 then  
+				  	if wskt>=35 and maxTKE>3 then  -- MOD over sea
 				  		  turb = 3 
-				  	end  
-	
-				 	 -- SEV over sea
- 				 	if wskt>=50 and maxTKE>4 then
+				  	end  	
+ 				 	if wskt>=50 and maxTKE>4 then  -- SEV over sea
  				 		  turb = 5 
  				 	end
-
-				end
-				
+				end				
 		end
-
 	turbulence[i]= turb
-	
-	
- 	
 	end
   
-result:SetParam(param("BLTURB-N"))
+    
+result:SetParam(turbparam)
 
 result:SetValues(turbulence)
  
