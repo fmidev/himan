d14336ec65f29a931c5af87bb1572ca37aeb8950
Mikko Partio <mikko.partio@fmi.fi>
2015-12-31 12:20:38 +0000

    Updating test data for harmonie and/or accumulated parameters (metadata change)

--- himan-tests/density/ECMWF.sh
+++ himan-tests/density/ECMWF.sh
@@ -13,9 +13,9 @@ $HIMAN -d 5 -f density_ECMWF.json -t grib --no-cuda ecmwf_pressure_lvl_source.gr
 grib_compare -A 0.001 ./RHO-KGM3_pressure_850_rll_233_231_0_006.grib result_ecmwf_pressure_lvl.grib 
 
 if [ $? -eq 0 ];then
-  echo density/hirlam success on CPU!
+  echo density/ecmwf success on CPU!
 else
-  echo density/hirlam failed on CPU
+  echo density/ecmwf failed on CPU
   exit 1
 fi
 
