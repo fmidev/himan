5c14bb94de707886edef371190effaf2ab4550b0
Päivi Junila <paivi.junila@fmi.fi>
2016-01-07 13:07:29 +0000

    adding bl_turbulence test fot hirlam

--- himan-tests/bl_turbulence/HIRLAM.sh
+++ himan-tests/bl_turbulence/HIRLAM.sh
@@ -12,9 +12,9 @@ $HIMAN -d 5 -f bl_turbulence_hirlam.json -t grib --no-cuda hirlam_source.grib
 
 grib_compare -A 0.001 BLTURB-N_ground_0_rll_1030_816_0_001.grib hirlam_result.grib
 if [ $? -eq 0 ];then
-  echo ceiling/hirlam success on CPU!
+  echo bl_turbulence/hirlam success on CPU!
 else
-  echo ceiling/hirlam failed on CPU
+  echo bl_turbulence/hirlam failed on CPU
   exit 1
 fi
 
