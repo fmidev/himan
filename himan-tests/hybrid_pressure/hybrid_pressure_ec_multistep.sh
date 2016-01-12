048dabbb752e988f2fc695b703bffea0c35430cd
Mikko Partio <mikko.partio@fmi.fi>
2016-01-12 08:14:35 +0000

    sort

--- himan-tests/hybrid_pressure/hybrid_pressure_ec_multistep.sh
+++ himan-tests/hybrid_pressure/hybrid_pressure_ec_multistep.sh
@@ -10,7 +10,9 @@ rm -f ecmwf-multistep.json.grib
 
 $HIMAN -N -d 4 -f ecmwf-multistep.json lnsp_ec.grib2 q_ec.grib2 --no-cuda source_ecmwf_multistep.grib
 
-grib_compare -A 0.001 result_ec_multistep.grib ecmwf-multistep.json.grib
+grib_copy -B stepRange,level ecmwf-multistep.json.grib sorted.grib
+
+grib_compare -r -A 0.001 result_ec_multistep.grib ecmwf-multistep.json.grib
 
 if [ $? -eq 0 ];then
   echo hybrid_pressure_multistep/ec success!
