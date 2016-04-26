28d60477fdcd88ec099f47280bd6195065560317
Mikko Partio <mikko.partio@fmi.fi>
2016-04-26 09:28:16 +0000

    update test, add cuda grib unpacking test which triggered some problems (HIMAN-120)

--- himan-tests/gust/ecmwf.sh
+++ himan-tests/gust/ecmwf.sh
@@ -19,3 +19,19 @@ else
   exit 1
 fi
 
+if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
+
+  rm -f FFG2-MS_height_10_rll_661_576_0_005.grib
+
+  $HIMAN -d 4 -j 1 -f gust_ecmwf.json -t grib source.grib source.grib2
+
+  grib_compare -A 0.5 FFG2-MS_height_10_rll_661_576_0_005.grib result.grib
+
+  if [ $? -eq 0 ];then
+    echo gust/ecmwf success on GPU!
+  else
+    echo gust/ecmwf failed on GPU
+    exit 1
+  fi
+
+fi
