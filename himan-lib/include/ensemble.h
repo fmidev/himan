943c7a44a867be59177adb276121af93f5489930
Mikko Vanhatalo <mikko.vanhatalo@fmi.fi>
2016-06-02 11:26:29 +0000

    Initial commit of ensemble-helper

---
+++ himan-lib/include/ensemble.h
@@ -0,0 +1,81 @@
+//
+// @file ensemble.h
+//
+// @date June 2, 2016
+// @author vanhatam
+//
+
+#ifndef ENSEMBLE_H
+#define ENSEMBLE_H
+
+#include "himan_common.h"
+#include "param.h"
+#include "level.h"
+#include "forecast_time.h"
+#include "plugin_configuration.h"
+
+namespace himan
+{
+
+// ensemble is a thin layer on top of the usual himan data utilities.
+// It is used to make working with ensemble forecasts a bit nicer and
+// feel more like working with deterministic forecasts
+
+struct ensemble
+{
+	ensemble(const param& parameter, size_t forecastCount);
+	~ensemble();
+
+	// @brief Fetch the specified forecasts for the ensemble
+	void Fetch(std::shared_ptr<const plugin_configuration> config, const forecast_time& time, const level& forecastLevel);
+
+	// @brief Reset the location of all the ensembles
+	void ResetLocation();
+
+	// @brief Increment the location of all the ensembles,
+	// if any of these fails, then all fail
+	bool NextLocation();
+
+	// @brief Returns the current value of the specified forecast of the ensemble
+	double Value(size_t forecastIndex) const;
+
+	// @brief Returns the current values of the ensemble
+	std::vector<double> Values() const;
+
+	std::string ClassName() const;
+
+	size_t Size() const;
+
+	// @brief The parameter of the ensemble
+	param m_param;
+
+	// @brief The number of forecasts in this ensemble
+    size_t m_forecastCount;
+
+	// @brief Initialized perturbations forecast_types used by Fetch()
+    std::vector<forecast_type> m_perturbations;
+
+	// @brief Forecasts acquired with Fetch(), each call of Fetch() will overwrite the previous results
+	std::vector<info_t> m_forecasts;
+};
+
+inline size_t ensemble::Size() const
+{
+	return m_forecastCount;
+}
+
+inline double ensemble::Value(size_t forecastIndex) const
+{
+	return m_forecasts[forecastIndex]->Value();
+}
+
+inline std::string ensemble::ClassName() const
+{
+	return "himan::ensemble";
+}
+
+
+} // namespace himan
+
+// ENSEMBLE_H
+#endif
---
+++ himan-lib/source/ensemble.cpp
@@ -0,0 +1,101 @@
+//
+// @file ensemble.cpp
+//
+// @date June 2, 2016
+// @author vanhatam
+//
+
+#include "ensemble.h"
+#include "fetcher.h"
+#include "plugin_factory.h"
+
+#include <stdint.h>
+#include <stddef.h>
+
+namespace himan
+{
+
+ensemble::ensemble(const param& parameter, size_t forecastCount)
+	: m_param(parameter)
+	, m_forecastCount(forecastCount)
+{
+	m_perturbations = std::vector<forecast_type> (forecastCount - 1); // forecast count includes the control forecast
+	m_forecasts = std::vector<info_t> (forecastCount);
+
+	int perturbationNumber = 1;
+	for (auto & p : m_perturbations)
+	{
+		p = forecast_type (kEpsPerturbation, static_cast<double>(perturbationNumber));
+		perturbationNumber++;
+	}
+}
+
+ensemble::~ensemble()
+{
+}
+
+void ensemble::Fetch(std::shared_ptr<const plugin_configuration> config, const forecast_time& time, const level& forecastLevel)
+{
+	// NOTE should this be stored some where else? Every time you call Fetch(), the instantiation will happen
+	auto f = GET_PLUGIN(fetcher);
+
+	try
+	{
+		// First get the control forecast
+		m_forecasts[0] = f->Fetch(config, time, forecastLevel, m_param, forecast_type(kEpsControl), false);
+
+		// Then get the perturbations
+		for (size_t i = 1; i < m_perturbations.size() + 1; i++)
+		{
+			m_forecasts[i] = f->Fetch(config, time, forecastLevel, m_param, m_perturbations[i-1], false);
+		}
+	}
+	catch (HPExceptionType& e)
+	{
+		if (e != kFileDataNotFound)
+		{
+			throw std::runtime_error("Ensemble: unable to proceed");
+		} 
+		else
+		{
+			// NOTE let the plugin decide what to do with missing data
+			throw e;
+		}
+	}
+}
+
+void ensemble::ResetLocation()
+{
+	for (size_t i = 0; i < m_forecasts.size(); i++) 
+	{
+		assert(m_forecasts[i]);
+
+		m_forecasts[i]->ResetLocation();
+	}
+}
+
+bool ensemble::NextLocation()
+{
+	for (size_t i = 0; i < m_forecasts.size(); i++)
+	{
+		assert(m_forecasts[i]);
+
+		if (!m_forecasts[i]->NextLocation())
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+std::vector<double> ensemble::Values() const
+{
+	std::vector<double> ret (m_forecastCount);
+	for (auto& f : m_forecasts)
+	{
+		ret.push_back(f->Value());
+	}
+	return ret;
+}
+
+} // namespace himan
