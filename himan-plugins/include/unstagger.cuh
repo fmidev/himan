c814d6ab8a77e7626c59a3c2f95fbdf7921ebb9c
Andreas <andreas.tack@fmi.fi>
2016-08-23 11:12:09 +0300

    run clang on unstagger files

--- himan-plugins/include/unstagger.cuh
+++ himan-plugins/include/unstagger.cuh
@@ -3,15 +3,15 @@
  * Author: tack
  *
  * Created on July 19, 2016
- * 
+ *
  * List of extern functions compiled by nvcc for plugins (compiled by gcc)
  */
 
 #ifndef UNSTAGGER_CUDA_H
 #define UNSTAGGER_CUDA_H
 #ifdef HAVE_CUDA
-#include <vector>
 #include <utility>
+#include <vector>
 
 namespace himan
 {
@@ -19,18 +19,15 @@ namespace plugin
 {
 namespace unstagger_cuda
 {
-
 // initialize interpolation matrices
 void Init(size_t NX, size_t NY);
 
 // unstagger
 std::pair<std::vector<double>, std::vector<double>> Process(std::vector<double>& U_in, std::vector<double>& V_in);
 
-} // namespace unstagger_cuda
-} // namespace plugin
-} // namespace himan
+}  // namespace unstagger_cuda
+}  // namespace plugin
+}  // namespace himan
 
-#endif  /* HAVE_CUDA */
-#endif  /* UNSTAGGER_CUDA_H */
-
-
+#endif /* HAVE_CUDA */
+#endif /* UNSTAGGER_CUDA_H */
