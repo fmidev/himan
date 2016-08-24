e7af14c9c3f8bd3b7d5cf30d4d899c09c336f8ee
Andreas <andreas.tack@fmi.fi>
2016-08-24 13:39:39 +0300

    review round 1 feedback implementation

--- himan-plugins/source/unstagger.cu
+++ himan-plugins/source/unstagger.cu
@@ -1,10 +1,110 @@
+/**
+ * @file unstagger.cpp
+ *
+ * Calculate the co-located velocity field for U and V
+ *
+ * @date Oct 16, 2014
+ * @author Tack
+
+  Description:
+
+  The grid in a staggered arrangement consists of central points ('o') where most parameter values 
+  e.g. temperature and pressure are stored. The u-velocity points ('u') are shifted east by a half 
+  grid spacing and v-velocity points ('v') shifted south by a half grid spacing. A example on a 3x3
+  grid is given below.
+  
+  o---u---o---u---o---u
+  |       |       |    
+  v       v       v 
+  |       |       |  
+  o---u---o---u---o---u
+  |       |       |
+  v       v       v
+  |       |       |
+  o---u---o---u---o---u
+  |       |       |
+  v       v       v 
+
+  If collocation of all parameters is required, u- and v-velocity needs to be unstaggered. The 
+  unstaggering is done by solving a set of linear equations. For the example grid shown above 
+  the linear equations are.
+
+  Case u:                                 Case v:
+
+    1*u(1,1)              = o(1,1)          1*v(1,1)              = o(1,1)
+  0.5*u(1,1) + 0.5*u(1,2) = o(1,2)          1*v(1,2)              = o(1,2)
+  0.5*u(1,2) + 0.5*u(1,3) = o(1,3)          1*v(1,3)              = o(1,3)
+    1*u(2,1)              = o(2,1)        0.5*v(1,1) + 0.5*v(2,1) = o(2,1)
+  0.5*u(2,1) + 0.5*u(2,2) = o(2,2)        0.5*v(1,2) + 0.5*v(2,2) = o(2,2)
+  0.5*u(2,2) + 0.5*u(2,3) = o(2,3)        0.5*v(1,3) + 0.5*v(2,3) = o(2,3)
+    1*u(3,1)              = o(3,1)        0.5*v(2,1) + 0.5*v(3,1) = o(3,1)
+  0.5*u(3,1) + 0.5*u(3,2) = o(3,2)        0.5*v(2,2) + 0.5*v(3,2) = o(3,2)
+  0.5*u(3,2) + 0.5*u(3,3) = o(3,3)        0.5*v(2,3) + 0.5*v(3,3) = o(3,3)
+
+  These equations can be re-written in matrix-vector form
+
+                   |-      -|
+                   | u(1,1) |
+                   |   .    |
+                   |   .    |
+                   | u(3,3) |
+                   |-      -|
+
+  |-           -|  |-      -|
+  | w11 ... w19 |  | o(1,1) |
+  | .       .   |  |   .    |
+  | .       .   |  |   .    |
+  | w91 ... w99 |  | o(3,3) |
+  |-           -|  |-      -|
+
+  where the weighting matrices for the u- and v-case are diagonal matrices.
+
+  The diagonal matrix to unstagger the u-velocity field has the form
+
+        | 2 0 0 0 0 0 0 0 0 |
+        | 1 1 0 0 0 0 0 0 0 |
+        | 0 1 1 0 0 0 0 0 0 |
+        | 0 0 0 2 0 0 0 0 0 |
+  1/2 * | 0 0 0 1 1 0 0 0 0 |  = U_unstagger
+        | 0 0 0 0 1 1 0 0 0 |
+        | 0 0 0 0 0 0 2 0 0 |
+        | 0 0 0 0 0 0 1 1 0 |
+        | 0 0 0 0 0 0 0 1 1 |
+
+  and the diagonal matrix to unstagger the v-velocity
+
+        | 2 0 0 0 0 0 0 0 0 |
+        | 0 2 0 0 0 0 0 0 0 |
+        | 0 0 2 0 0 0 0 0 0 |
+        | 1 0 0 1 0 0 0 0 0 |
+  1/2 * | 0 1 0 0 1 0 0 0 0 |  = V_unstagger
+        | 0 0 1 0 0 1 0 0 0 |
+        | 0 0 0 1 0 0 1 0 0 |
+        | 0 0 0 0 1 0 0 1 0 |
+        | 0 0 0 0 0 1 0 0 1 |
+
+  In this form the problem can be solved efficiently using a sparse matrix library
+  matrix-vector multiplication. In this implementation CUSP is used for that purpose.
+
+  **/
+
 #include "cuda_plugin_helper.h"
 #include "unstagger.cuh"
+#ifdef DEBUG
+#undef DEBUG
 #include <cusp/array1d.h>
 #include <cusp/array2d.h>
 #include <cusp/dia_matrix.h>
 #include <cusp/multiply.h>
 #include <thrust/system/cuda/execution_policy.h>
+#define DEBUG
+#else
+#include <cusp/array1d.h>
+#include <cusp/array2d.h>
+#include <cusp/dia_matrix.h>
+#include <cusp/multiply.h>
+#include <thrust/system/cuda/execution_policy.h>
+#endif
 
 cusp::dia_matrix<int, double, cusp::device_memory> U_unstagger;
 cusp::dia_matrix<int, double, cusp::device_memory> V_unstagger;
@@ -64,16 +164,16 @@ std::pair<std::vector<double>, std::vector<double>> himan::plugin::unstagger_cud
 	CUDA_CHECK(cudaHostRegister(V_out.data(), sizeof(double) * N, 0));
 
 	// create 1d arrays on device
-	double* d_U = 0;      // pointer to device memory pointing to incoming data of U
-	double* d_V = 0;      // pointer to device memory pointing to incoming data of V
-	double* d_U_out = 0;  // pointer to device memory to unstaggered data of U
-	double* d_V_out = 0;  // pointer to device memory to unstaggered data of V
+	double* d_U = nullptr;      // pointer to device memory pointing to incoming data of U
+	double* d_V = nullptr;      // pointer to device memory pointing to incoming data of V
+	double* d_U_out = nullptr;  // pointer to device memory to unstaggered data of U
+	double* d_V_out = nullptr;  // pointer to device memory to unstaggered data of V
 
 	// allocate memory
-	CUDA_CHECK(cudaMalloc((double**)&d_U, sizeof(double) * N));
-	CUDA_CHECK(cudaMalloc((double**)&d_V, sizeof(double) * N));
-	CUDA_CHECK(cudaMalloc((double**)&d_U_out, sizeof(double) * N));
-	CUDA_CHECK(cudaMalloc((double**)&d_V_out, sizeof(double) * N));
+	CUDA_CHECK(cudaMalloc((void**)&d_U, sizeof(double) * N));
+	CUDA_CHECK(cudaMalloc((void**)&d_V, sizeof(double) * N));
+	CUDA_CHECK(cudaMalloc((void**)&d_U_out, sizeof(double) * N));
+	CUDA_CHECK(cudaMalloc((void**)&d_V_out, sizeof(double) * N));
 
 	// copy data to device
 	CUDA_CHECK(cudaMemcpyAsync(d_U, U_in.data(), sizeof(double) * N, cudaMemcpyHostToDevice, stream));
@@ -86,10 +186,10 @@ std::pair<std::vector<double>, std::vector<double>> himan::plugin::unstagger_cud
 	thrust::device_ptr<double> dt_V_out = thrust::device_pointer_cast(d_V_out);
 
 	// create cusp::array1d
-	auto U_device = cusp::array1d_view<thrust::device_ptr<double>>(dt_U, dt_U + N);              // create array1d
-	auto V_device = cusp::array1d_view<thrust::device_ptr<double>>(dt_V, dt_V + N);              // create array1d
-	auto U_device_out = cusp::array1d_view<thrust::device_ptr<double>>(dt_U_out, dt_U_out + N);  // create array1d
-	auto V_device_out = cusp::array1d_view<thrust::device_ptr<double>>(dt_V_out, dt_V_out + N);  // create array1d
+	auto U_device = cusp::array1d_view<thrust::device_ptr<double>>(dt_U, dt_U + N);              
+	auto V_device = cusp::array1d_view<thrust::device_ptr<double>>(dt_V, dt_V + N);              
+	auto U_device_out = cusp::array1d_view<thrust::device_ptr<double>>(dt_U_out, dt_U_out + N);  
+	auto V_device_out = cusp::array1d_view<thrust::device_ptr<double>>(dt_V_out, dt_V_out + N);  
 
 	// perform the unstagger operation
 	cusp::multiply(thrust::cuda::par.on(stream), U_unstagger, U_device, U_device_out);
