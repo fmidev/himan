41ff48d4869d09c5bcbe7425ae641c06f9a46279
Mikko Partio <mikko.partio@fmi.fi>
2016-10-21 14:22:40 +0300

    Add time_ensemble classes which were left out from the previous commit :/

---
+++ himan-lib/include/time_ensemble.h
@@ -0,0 +1,27 @@
+/*
+ * File:   time_ensemble.h
+ * Author: partio
+ *
+ * Created on October 20, 2016, 1:51 PM
+ */
+
+#ifndef TIME_ENSEMBLE_H
+#define TIME_ENSEMBLE_H
+
+#include "ensemble.h"
+
+namespace himan
+{
+class time_ensemble : public ensemble
+{
+   public:
+	time_ensemble(const param& parameter, size_t ensembleSize, HPTimeResolution timeSpan = kYearResolution);
+
+	void Fetch(std::shared_ptr<const plugin_configuration> config, const forecast_time& time,
+	           const level& forecastLevel) override;
+
+   private:
+	HPTimeResolution itsTimeSpan;
+};
+}
+#endif /* TIME_ENSEMBLE_H */
