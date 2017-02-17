a9a81fb3d44fc116b9c370a27d0f5f89366ed0ff
Mikko Partio <mikko.partio@fmi.fi>
2017-02-17 09:28:10 +0200

    Fine tuning time_ensemble leap year handling (hopefully for the last time)

--- himan-lib/source/raw_time.cpp
+++ himan-lib/source/raw_time.cpp
@@ -24,7 +24,6 @@ raw_time::raw_time(const std::string& theDateTime, const std::string& theTimeMas
 }
 
 raw_time::raw_time(const raw_time& other) : itsDateTime(other.itsDateTime) {}
-
 raw_time& raw_time::operator=(const raw_time& other)
 {
 	itsDateTime = other.itsDateTime;
@@ -43,11 +42,8 @@ bool raw_time::operator==(const raw_time& other) const
 }
 
 bool raw_time::operator!=(const raw_time& other) const { return !(*this == other); }
-
 raw_time::operator std::string() const { return String("%Y%m%d%H%M"); }
-
 std::string raw_time::String(const std::string& theTimeMask) const { return FormatTime(itsDateTime, theTimeMask); }
-
 std::string raw_time::FormatTime(boost::posix_time::ptime theFormattedDateTime, const std::string& theTimeMask) const
 {
 	if (theFormattedDateTime == boost::date_time::not_a_date_time)
@@ -112,6 +108,10 @@ bool raw_time::Adjust(HPTimeResolution timeResolution, int theValue)
 }
 
 bool raw_time::Empty() const { return (itsDateTime == boost::posix_time::not_a_date_time); }
+bool raw_time::IsLeapYear() const
+{
+	return boost::gregorian::gregorian_calendar::is_leap_year(itsDateTime.date().year());
+}
 
 std::ostream& raw_time::Write(std::ostream& file) const
 {
