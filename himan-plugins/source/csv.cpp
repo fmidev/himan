c171247bbe643c80d97d6430e404ce733269731f
Mikko Partio <mikko.partio@fmi.fi>
2017-04-13 12:46:22 +0300

    Structural modifications to csv plugin
    * Remove template-based csv reader
    * Support forecast type id & value
    * General code cleanup
    * Support full auxiliary file cache read
    * Change supported CSV format

--- himan-plugins/source/csv.cpp
+++ himan-plugins/source/csv.cpp
@@ -1,61 +1,15 @@
-/**
- * @file csv.cpp
- *
- */
-
 #include "csv.h"
-#include "csv_v3.h"
 #include "logger_factory.h"
 #include "point_list.h"
+#include "util.h"
+#include <algorithm>
 #include <boost/filesystem.hpp>
-#include <boost/foreach.hpp>
 #include <fstream>
 
 using namespace std;
 using namespace himan::plugin;
 
-typedef tuple<int,     // station id
-              string,  // station name
-              double,  // longitude
-              double,  // latitude
-              string,  // origintime as timestamp
-              string,  // forecasttime as timestamp
-              string,  // level name
-              double,  // level value
-              string,  // parameter name
-              double>  // value
-    record;
-
-typedef io::CSVReader<10,  // column count
-                      io::trim_chars<' ', '\t'>, io::no_quote_escape<','>, io::throw_on_overflow, io::no_comment>
-    csv_reader;
-
-bool GetLine(csv_reader& in, record& line)
-{
-	int station_id;
-	string station_name;
-	double longitude;
-	double latitude;
-	string origintime;
-	string forecasttime;
-	string level_name;
-	double level_value;
-	string parameter_name;
-	double value;
-
-	if (in.read_row(station_id, station_name, longitude, latitude, origintime, forecasttime, level_name, level_value,
-	                parameter_name, value))
-	{
-		line = make_tuple(station_id, station_name, longitude, latitude, origintime, forecasttime, level_name,
-		                  level_value, parameter_name, value);
-		return true;
-	}
-
-	return false;
-}
-
 csv::csv() { itsLogger = std::unique_ptr<logger>(logger_factory::Instance()->GetLog("csv")); }
-
 bool csv::ToFile(info& theInfo, string& theOutputFile)
 {
 	if (theInfo.Grid()->Class() != kIrregularGrid)
@@ -75,30 +29,22 @@ bool csv::ToFile(info& theInfo, string& theOutputFile)
 	       "value"
 	    << endl;
 
-	for (theInfo.ResetTime(); theInfo.NextTime();)
+	theInfo.First();
+	theInfo.ResetParam();
+
+	const auto originTime = theInfo.Time().OriginDateTime().String();
+	while (theInfo.Next())
 	{
-		forecast_time time = theInfo.Time();
-
-		for (theInfo.ResetLevel(); theInfo.NextLevel();)
+		for (theInfo.ResetLocation(); theInfo.NextLocation();)
 		{
-			level lev = theInfo.Level();
-
-			for (theInfo.ResetParam(); theInfo.NextParam();)
-			{
-				param par = theInfo.Param();
-
-				for (theInfo.ResetLocation(); theInfo.NextLocation();)
-				{
-					station s = theInfo.Station();
-					out << s.Id() << "," << s.Name() << "," << s.X() << "," << s.Y() << ","
-					    << time.OriginDateTime().String() << "," << time.ValidDateTime().String() << ","
-					    << HPLevelTypeToString.at(lev.Type()) << "," << lev.Value() << "," << par.Name() << ","
-					    << theInfo.Value() << endl;
-				}
-
-				out.flush();
-			}
+			station s = theInfo.Station();
+			out << s.Id() << ","  // << s.X() << "," << s.Y() << ","
+			    << originTime << "," << theInfo.Time().ValidDateTime().String() << ","
+			    << HPLevelTypeToString.at(theInfo.Level().Type()) << "," << theInfo.Level().Value() << ","
+			    << theInfo.Param().Name() << "," << theInfo.Value() << endl;
 		}
+
+		out.flush();
 	}
 
 	aTimer->Stop();
@@ -112,205 +58,147 @@ bool csv::ToFile(info& theInfo, string& theOutputFile)
 	return true;
 }
 
-shared_ptr<himan::info> csv::FromFile(const string& inputFile, const search_options& options) const
+shared_ptr<himan::info> csv::FromFile(const string& inputFile, const search_options& options,
+                                      bool readIfNotMatching) const
 {
-	info_t ret = make_shared<info>();
+	info_t all, requested;
 
-	csv_reader in(inputFile);
+	vector<string> lines;
+	string line;
 
-	in.read_header(io::ignore_no_column, "station_id", "station_name", "longitude", "latitude", "origintime",
-	               "forecasttime", "level_name", "level_value", "parameter_name", "value");
+	ifstream infile(inputFile);
 
-	if (!in.has_column("station_id"))
+	while (getline(infile, line))
 	{
-		itsLogger->Error("CSV file does not have column station_id");
-		throw kFileDataNotFound;
+		lines.push_back(line);
 	}
 
-	record line;
+	all = util::CSVToInfo(lines);
+
+	if (readIfNotMatching)
+	{
+		return all;
+	}
 
 	vector<forecast_time> times;
 	vector<param> params;
 	vector<level> levels;
-	vector<station> stats;
+	vector<station> stations;
+	vector<forecast_type> ftypes;
 
 	forecast_time optsTime(options.time);
 
-	// First create descriptors
-	while (GetLine(in, line))
+	all->First();
+	all->ResetParam();
+
+	// Remove those dimensions that are not requested
+	while (all->Next())
 	{
-		/* Validate time */
+		if (all->Param() != options.param)
+		{
+			itsLogger->Debug("Param does not match");
+			itsLogger->Debug(options.param.Name() + " vs " + all->Param().Name());
+		}
+		else if (find(params.begin(), params.end(), all->Param()) == params.end())
+		{
+			params.push_back(all->Param());
+		}
 
-		forecast_time f(get<4>(line), get<5>(line));
+		if (all->Level() != options.level)
+		{
+			itsLogger->Debug("Level does not match");
+			itsLogger->Debug(static_cast<string>(options.level) + " vs " + static_cast<string>(all->Level()));
+		}
+		else if (find(levels.begin(), levels.end(), all->Level()) == levels.end())
+		{
+			levels.push_back(all->Level());
+		}
 
-		if (f != options.time)
+		if (all->Time() != options.time)
 		{
 			itsLogger->Debug("Time does not match");
 			itsLogger->Debug("Origin time " + static_cast<string>(optsTime.OriginDateTime()) + " vs " +
-			                 static_cast<string>(f.OriginDateTime()));
+			                 static_cast<string>(all->Time().OriginDateTime()));
 			itsLogger->Debug("Forecast time: " + static_cast<string>(optsTime.ValidDateTime()) + " vs " +
-			                 static_cast<string>(f.ValidDateTime()));
-
-			continue;
+			                 static_cast<string>(all->Time().ValidDateTime()));
+		}
+		else if (find(times.begin(), times.end(), all->Time()) == times.end())
+		{
+			times.push_back(all->Time());
 		}
 
-		string level_name = get<6>(line);
-		boost::algorithm::to_lower(level_name);
-
-		/* Validate level */
-		level l(HPStringToLevelType.at(level_name), get<7>(line));
-
-		if (l != options.level)
+		if (all->ForecastType() != options.ftype)
 		{
-			itsLogger->Debug("Level does not match");
-			itsLogger->Debug(static_cast<string>(options.level) + " vs " + static_cast<string>(l));
-
-			continue;
+			itsLogger->Debug("Forecast type does not match");
+			itsLogger->Debug(static_cast<string>(options.ftype) + " vs " + static_cast<string>(all->ForecastType()));
+		}
+		else if (find(ftypes.begin(), ftypes.end(), all->ForecastType()) == ftypes.end())
+		{
+			ftypes.push_back(all->ForecastType());
 		}
 
-		/* Validate param */
-
-		param p(get<8>(line));
-
-		if (p != options.param)
+		for (all->ResetLocation(); all->NextLocation();)
 		{
-			itsLogger->Debug("Param does not match");
-			itsLogger->Debug(options.param.Name() + " vs " + p.Name());
-
-			continue;
-		}
-
-		bool found = false;
-
-		/* Prevent duplicates */
-
-		BOOST_FOREACH (const forecast_time& t, times)
-		{
-			if (f == t)
+			// no station in options struct ...
+			if (find(stations.begin(), stations.end(), all->Station()) == stations.end())
 			{
-				found = true;
-				break;
+				stations.push_back(all->Station());
 			}
 		}
-
-		if (!found) times.push_back(f);
-
-		found = false;
-
-		BOOST_FOREACH (const level& t, levels)
-		{
-			if (l == t)
-			{
-				found = true;
-				break;
-			}
-		}
-
-		if (!found) levels.push_back(l);
-
-		found = false;
-
-		BOOST_FOREACH (const param& t, params)
-		{
-			if (p == t)
-			{
-				found = true;
-				break;
-			}
-		}
-
-		if (!found) params.push_back(p);
-
-		// Add location information
-
-		station s(get<0>(line), get<1>(line), get<2>(line), get<3>(line));
-
-		found = false;
-
-		BOOST_FOREACH (const station& stat, stats)
-		{
-			if (stat == s)
-			{
-				found = true;
-				break;
-			}
-		}
-
-		if (!found) stats.push_back(s);
 	}
 
-	if (times.size() == 0 || params.size() == 0 || levels.size() == 0)
+	if (times.size() == 0 || params.size() == 0 || levels.size() == 0 || ftypes.size() == 0)
 	{
 		itsLogger->Error("Did not find valid data from file '" + inputFile + "'");
 		throw kFileDataNotFound;
 	}
 
-	ret->Producer(options.prod);
+	requested = make_shared<info>();
+	requested->Producer(options.prod);
 
-	assert(times.size());
-	assert(params.size());
-	assert(levels.size());
-
-	ret->Times(times);
-	ret->Params(params);
-	ret->Levels(levels);
-
-	vector<forecast_type> ftypes;
-	ftypes.push_back(forecast_type(kDeterministic));
-
-	ret->ForecastTypes(ftypes);
+	requested->Times(times);
+	requested->Params(params);
+	requested->Levels(levels);
+	requested->ForecastTypes(ftypes);
 
 	auto base = unique_ptr<grid>(new point_list());  // placeholder
-	base->Type(kLatitudeLongitude);
-	base->Class(kIrregularGrid);
 
-	ret->Create(base.get());
+	requested->Create(base.get(), true);
+	requested->First();
+	requested->ResetParam();
 
-	dynamic_cast<point_list*>(ret->Grid())->Stations(stats);
+	while (requested->Next())
+	{
+		dynamic_cast<point_list*>(requested->Grid())->Stations(stations);
+	}
 
 	itsLogger->Debug("Read " + boost::lexical_cast<string>(times.size()) + " times, " +
-	                 boost::lexical_cast<string>(levels.size()) + " levels and " +
+	                 boost::lexical_cast<string>(levels.size()) + " levels, " +
+	                 boost::lexical_cast<string>(ftypes.size()) + " forecast types and " +
 	                 boost::lexical_cast<string>(params.size()) + " params from file '" + inputFile + "'");
 
-	// Then set grids
+	requested->First();
+	requested->ResetParam();
 
-	// The csv library used is sub-standard in that it doesn't allow rewinding of the
-	// file. It does provide functions to set and get file line number, but that doesn't
-	// affect the reading of the file!
-
-	csv_reader in2(inputFile);
-	in2.read_header(io::ignore_no_column, "station_id", "station_name", "longitude", "latitude", "origintime",
-	                "forecasttime", "level_name", "level_value", "parameter_name", "value");
-
-	int counter = 0;
-
-	while (GetLine(in2, line))
+	while (requested->Next())
 	{
-		forecast_time f(get<4>(line), get<5>(line));
+		if (!all->Param(requested->Param())) throw runtime_error("Impossible error occurred");
+		if (!all->Level(requested->Level())) throw runtime_error("Impossible error occurred");
+		if (!all->Time(requested->Time())) throw runtime_error("Impossible error occurred");
+		if (!all->ForecastType(requested->ForecastType())) throw runtime_error("Impossible error occurred");
 
-		string level_name = get<6>(line);
-		boost::algorithm::to_lower(level_name);
-
-		level l(HPStringToLevelType.at(level_name), get<7>(line));
-		param p(get<8>(line));
-
-		station s(get<0>(line), get<1>(line), get<2>(line), get<3>(line));
-
-		if (!ret->Param(p)) continue;
-		if (!ret->Time(f)) continue;
-		if (!ret->Level(l)) continue;
-
-		for (size_t i = 0; i < stats.size(); i++)
+		for (requested->ResetLocation(); requested->NextLocation();)
 		{
-			if (s == stats[i])
+			for (all->ResetLocation(); all->NextLocation();)
 			{
-				// Add the data point
-				ret->Grid()->Value(i, get<9>(line));
-				counter++;
+				if (requested->Station() == all->Station())
+				{
+					requested->Value(all->Value());
+					break;
+				}
 			}
 		}
 	}
 
-	itsLogger->Debug("Read " + boost::lexical_cast<string>(counter) + " lines of data");
-	return ret;
+	return requested;
 }
