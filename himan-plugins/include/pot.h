8a3f930f473b1bc0570efa83cfcb98b86ac88ea0
Andreas <Andreas.Tack@fmi.fi>
2017-06-06 17:25:20 +0300

    POT v2.1

--- himan-plugins/include/pot.h
+++ himan-plugins/include/pot.h
@@ -14,6 +14,38 @@ namespace himan
 namespace plugin
 {
 /**
+ * @class time_series
+ *
+ * @brief contains a series of consecutive time instances for a given parameter field
+ * This in preliminarily implemented with the POT plugin while the final implementation
+ * will be part of himan-lib.
+ */
+
+class time_series
+{
+   public:
+	time_series() = default;
+	time_series(param theParam, size_t expectedSize);
+	~time_series() = default;
+	time_series(const time_series& other) = default;
+	time_series& operator=(const time_series& other) = default;
+
+	void Fetch(std::shared_ptr<const plugin_configuration> config, forecast_time start_time,
+	           const HPTimeResolution& timeRes, int stepSize, int numSteps, const level& forecastLevel,
+	           const forecast_type& requestedType, bool readPackedData);
+
+	param Param() const { return itsParam; };
+	void Param(param theParam);
+
+	size_t Size() const { return itsInfos.size(); };
+	std::vector<info_t>::iterator begin() { return itsInfos.begin(); };
+	std::vector<info_t>::iterator end() { return itsInfos.end(); };
+   private:
+	param itsParam;
+	std::vector<info_t> itsInfos;
+};
+
+/**
  * @class pot
  *
  * @brief Calculate the thunder probability
@@ -22,7 +54,7 @@ namespace plugin
 
 class pot : public compiled_plugin, private compiled_plugin_base
 {
-public:
+   public:
 	pot();
 
 	inline virtual ~pot() {}
@@ -34,7 +66,7 @@ public:
 	virtual std::string ClassName() const { return "himan::plugin::pot"; }
 	virtual HPPluginClass PluginClass() const { return kCompiled; }
 	virtual HPVersionNumber Version() const { return HPVersionNumber(0, 1); }
-private:
+   private:
 	virtual void Calculate(std::shared_ptr<info> theTargetInfo, unsigned short theThreadIndex);
 };
 
