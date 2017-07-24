784edf0ca49cfa150aa7f8b4ea27b6508c3a13cf
Mikko Vanhatalo <mikko.vanhatalo@fmi.fi>
2017-07-24 14:58:34 +0300

    monin_obukhov: preparatory cleanup

--- himan-plugins/source/monin_obukhov.cpp
+++ himan-plugins/source/monin_obukhov.cpp
@@ -5,8 +5,6 @@
  *
  */
 
-#include <boost/lexical_cast.hpp>
-
 #include "forecast_time.h"
 #include "level.h"
 #include "logger.h"
@@ -27,51 +25,26 @@ void monin_obukhov::Process(std::shared_ptr<const plugin_configuration> conf)
 {
 	Init(conf);
 
-	/*
-	 * Set target parameter properties
-	 * - name PARM_NAME, this name is found from neons. For example: T-K
-	 * - univ_id UNIV_ID, newbase-id, ie code table 204
-	 * - grib1 id must be in database
-	 * - grib2 descriptor X'Y'Z, http://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_table4-2.shtml
-	 *
-	 */
-
 	param theRequestedParam("MOL-M", 1204);
-
-	// If this param is also used as a source param for other calculations
 
 	SetParams({theRequestedParam});
 
 	Start();
 }
 
-/*
- * Calculate()
- *
- * This function does the actual calculation.
- */
-
 void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short threadIndex)
 {
-	/*
-	 * Required source parameters
-	 *
-	 * eg. param PParam("P-Pa"); for pressure in pascals
-	 *
-	 */
-
 	const param TParam("T-K");          // ground Temperature
 	const param SHFParam("FLSEN-JM2");  // accumulated surface sensible heat flux
 	const param LHFParam("FLLAT-JM2");  // accumulated surface latent heat flux
 	const param U_SParam("FRVEL-MS");   // friction velocity
 	const param PParam("P-PA");
-	// ----
 
-	auto myThreadedLogger = logger("monin_obukhov Thread #" + boost::lexical_cast<string>(threadIndex));
+	auto myThreadedLogger = logger("monin_obukhov Thread #" + std::to_string(threadIndex));
 
 	// Prev/current time and level
 
-	int paramStep = 1;  // myTargetInfo->Param().Aggregation().TimeResolutionValue();
+	int paramStep = 1;
 	HPTimeResolution timeResolution = myTargetInfo->Time().StepResolution();
 
 	forecast_time forecastTime = myTargetInfo->Time();
@@ -80,8 +53,8 @@ void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short thre
 	forecast_type forecastType = myTargetInfo->ForecastType();
 
 	level forecastLevel = level(himan::kHeight, 0, "Height");
-	myThreadedLogger.Debug("Calculating time " + static_cast<string>(forecastTime.ValidDateTime()) + " level " +
-						   static_cast<string>(forecastLevel));
+	myThreadedLogger.Info("Calculating time " + static_cast<string>(forecastTime.ValidDateTime()) + " level " +
+						  static_cast<string>(forecastLevel));
 
 	info_t TInfo = Fetch(forecastTime, forecastLevel, TParam, forecastType, false);
 	info_t SHFInfo = Fetch(forecastTime, forecastLevel, SHFParam, forecastType, false);
@@ -105,7 +78,7 @@ void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short thre
 
 	if (!TInfo || !SHFInfo || !U_SInfo || !PInfo || !PrevSHFInfo || !LHFInfo || !PrevLHFInfo)
 	{
-		myThreadedLogger.Info("Skipping step " + boost::lexical_cast<string>(forecastTime.Step()) + ", level " +
+		myThreadedLogger.Info("Skipping step " + std::to_string(forecastTime.Step()) + ", level " +
 							  static_cast<string>(forecastLevel));
 		return;
 	}
@@ -132,7 +105,7 @@ void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short thre
 		SHF /= forecastStepSize;  // divide by time step to obtain Watts/m2
 		LHF /= forecastStepSize;  // divide by time step to obtain Watts/m2
 
-		/* Calculation of the inverse of Monin-Obukhov length to avoid division by 0 */
+		// Calculation of the inverse of Monin-Obukhov length to avoid division by 0
 
 		if (U_S != 0.0)
 		{
@@ -151,6 +124,6 @@ void monin_obukhov::Calculate(shared_ptr<info> myTargetInfo, unsigned short thre
 	}
 
 	myThreadedLogger.Info("[" + deviceType + "] Missing values: " +
-						  boost::lexical_cast<string>(myTargetInfo->Data().MissingCount()) + "/" +
-						  boost::lexical_cast<string>(myTargetInfo->Data().Size()));
+						  std::to_string(myTargetInfo->Data().MissingCount()) + "/" +
+						  std::to_string(myTargetInfo->Data().Size()));
 }
