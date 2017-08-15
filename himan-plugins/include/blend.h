7601d6e9df04591198ad8878815e5d75984dfd4e
Mikko Vanhatalo <mikko.vanhatalo@fmi.fi>
2017-08-15 16:13:05 +0300

    blend: clang-format header

--- himan-plugins/include/blend.h
+++ himan-plugins/include/blend.h
@@ -8,20 +8,19 @@ namespace himan
 {
 namespace plugin
 {
-
-// Gather producer, geometry, and time information
+// Gather producer, geometry, and time information.
 struct meta
 {
-    producer prod;
-    std::string geom;
-    forecast_type type;
-    forecast_time ftime;
-    level lvl;
+	producer prod;
+	std::string geom;
+	forecast_type type;
+	forecast_time ftime;
+	level lvl;
 };
 
 class blend : public compiled_plugin, private compiled_plugin_base
 {
-public:
+   public:
 	blend();
 	virtual ~blend();
 
@@ -34,21 +33,21 @@ public:
 	virtual HPPluginClass PluginClass() const { return kCompiled; }
 	virtual HPVersionNumber Version() const { return HPVersionNumber(1, 1); }
 
-protected:
+   protected:
 	virtual void Calculate(std::shared_ptr<info> targetInfo, unsigned short threadIndex);
 	virtual void WriteToFile(const info& targetInfo, write_options opts = write_options()) override;
 
-private:
-    void SetupOutputForecastTypes(std::shared_ptr<info> Info);
+   private:
+	void SetupOutputForecastTypes(std::shared_ptr<info> Info);
 
-    std::vector<meta> itsMetaOpts;
+	std::vector<meta> itsMetaOpts;
 	std::vector<double> itsWeights;
 };
 
 extern "C" std::shared_ptr<himan_plugin> create() { return std::shared_ptr<himan_plugin>(new blend()); }
 
-} // namespace plugin
-} // namespace himan
+}  // namespace plugin
+}  // namespace himan
 
 // BLEND_H
 #endif
