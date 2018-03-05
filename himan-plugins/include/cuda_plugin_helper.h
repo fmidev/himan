55cbd6011fa313f27331af4cad0b25e275fb949a
Mikko Partio <mikko.partio@fmi.fi>
2018-03-05 22:23:02 +0200

    Add new common functionality to cuda processing.
    These couple of new functions will hopefully replace eventually
    the old PrepareInfo() and ReleaseInfo() functions. They have been put to
    a new namespace himan::cuda, which hopefully makes a little bit less easier
    to distinguish from the old functions.
    
    The new functions are created because with them it's easier to incorporate
    single precision processing in cuda plugins.
    
    Also with this change it's easier to have more intelligence in the cuda
    plugin versions: originally they were just 'dumb' calculation platforms
    where even the source data was pre-fetched at cpu code. In many cases though
    the source data is not known during compile time, so we have to be able
    to dynamically fetch new data in gpu code. Another benefit is that we can
    stop using 'info_simple': now that nvcc supports C++11 (that was not the case
    earlier) we can expose 'info' class to cuda directly.
    
    The definitions of functions have been copied from cape-plugin where
    the single precision functionality was originally implemented.

--- himan-plugins/include/cuda_plugin_helper.h
+++ himan-plugins/include/cuda_plugin_helper.h
@@ -9,7 +9,9 @@
 #ifdef HAVE_CUDA
 
 #include "cuda_helper.h"
+#include "info.h"
 #include "info_simple.h"
+#include "plugin_configuration.h"
 #include "simple_packed.h"
 
 namespace himan
@@ -90,6 +92,21 @@ __global__ void Fill(T* devptr, size_t N, T fillValue)
 		devptr[idx] = fillValue;
 	}
 }
+
+namespace cuda
+{
+template <typename T>
+void PrepareInfo(std::shared_ptr<himan::info> info, T* d_ret, cudaStream_t& stream);
+
+template <typename T>
+void ReleaseInfo(std::shared_ptr<himan::info> info, T* d_ret, cudaStream_t& stream);
+
+std::shared_ptr<himan::info> Fetch(const std::shared_ptr<const plugin_configuration> conf,
+                                   const himan::forecast_time& theTime, const himan::level& theLevel,
+                                   const himan::param& theParam, const himan::forecast_type& theType,
+                                   bool returnPacked = true);
+
+}  // namespace cuda
 
 #endif /* __NVCC__ */
 
