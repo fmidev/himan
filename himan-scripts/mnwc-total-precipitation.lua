906602d09ca7bc5719b03ae2c620c22999a35d9f
Mikko Partio <mikko.partio@fmi.fi>
2018-06-15 09:56:23 +0300

    New lua script to sum up mnwc precipitation components

---
+++ himan-scripts/mnwc-total-precipitation.lua
@@ -0,0 +1,26 @@
+-- Sum precipitation components to get total precipitation
+
+logger:Info("Calculating total precipitation")
+
+local par1 = param("RACC-KGM2") -- rain
+local par2 = param("GR-KGM2") -- graupel
+local par3 = param("SNACC-KGM2") -- snowfall
+local par4 = param("RR-KGM2") -- total precipitation
+
+local rr = luatool:FetchWithType(current_time, current_level, par1, current_forecast_type)
+local gr = luatool:FetchWithType(current_time, current_level, par2, current_forecast_type)
+local sn = luatool:FetchWithType(current_time, current_level, par3, current_forecast_type)
+
+if not rr or not gr or not sn then
+  return
+end
+
+local tp = {}
+
+for i=1, #rr do
+  tp[i] = rr[i] + gr[i] + sn[i]
+end
+
+result:SetValues(tp)
+result:SetParam(par4)
+luatool:WriteToFile(result)
