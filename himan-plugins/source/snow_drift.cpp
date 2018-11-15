0cfbcc4cb891f56d3d5659c04b713f590cf79732
Mikko Partio <mikko.partio@fmi.fi>
2018-11-15 21:19:11 +0200

    STU-9054: snow_drift plugin
    Calculate snow drift value based on algorithm by
    SNAPS/Vegsyn.
    
    Algorithm is implemented as a plugin (instead of
    a script) because it needs to be calculated sequentally
    in time.
    
    Output parameter is snow drift index, having values 0,1,2 or 3
    (no drift, low, moderate, high drift). Plugin also writes two
    intermediate parameters that are only used in the calculation
    and are not supposed to be used directly.
    
    When calculating snow drift forecast, we should 'calibrate' the
    calculation using observed data. Therefore this plugin needs to
    calculated to both observation producer (eg. LAPS) and to actual
    forecast producer.

---
+++ himan-plugins/source/snow_drift.cpp
@@ -0,0 +1,255 @@
+#include "snow_drift.h"
+#include "forecast_time.h"
+#include "level.h"
+#include "logger.h"
+#include "metutil.h"
+#include "util.h"
+
+#include "fetcher.h"
+#include "hitool.h"
+#include "radon.h"
+#include "writer.h"
+
+using namespace himan;
+using namespace himan::plugin;
+
+const param SDIParam("SNOWDRIFT-N");
+const param SAParam("SNACC-H");
+const param DAParam("SNDACC-N");
+const double SFLimit = 0.09;
+
+double DriftMagnitude(double ff, double mi)
+{
+	return (ff * ff * ff / 1728.0) * mi;
+}
+
+double DriftIndex(double sv)
+{
+	if (sv < 0.09)
+	{
+		return 0;  // no drift
+	}
+	else if (sv < 0.21)
+	{
+		return 1;  // low
+	}
+	else if (sv < 0.5)
+	{
+		return 2;  // moderate
+	}
+	else
+	{
+		return 3;  // high
+	}
+}
+
+double MobilityIndex(double da, double sa)
+{
+	double mi = MissingDouble();
+
+	if (da < 2.0 && sa < 24.0)
+	{
+		mi = 1.0;
+	}
+	else if (da >= 2.0 && da <= 6.0)
+	{
+		mi = 0.6;
+	}
+	else if (da > 6.0)
+	{
+		mi = 0.3;
+	}
+
+	if (sa >= 24.0 && mi == 1.0)
+	{
+		mi = 0.6;
+	}
+
+	return mi;
+}
+
+void CalculateSnowDriftIndex(info_t& myTargetInfo, const std::vector<double>& T, const std::vector<double>& FF,
+                             const std::vector<double>& SF, const std::vector<double>& pSA,
+                             const std::vector<double>& pDA)
+{
+	myTargetInfo->Find<param>(SDIParam);
+	auto& SI = VEC(myTargetInfo);
+	myTargetInfo->Find<param>(SAParam);
+	auto& SA = VEC(myTargetInfo);
+	myTargetInfo->Find<param>(DAParam);
+	auto& DA = VEC(myTargetInfo);
+
+	for (auto&& tup : zip_range(SI, SA, DA, T, FF, SF, pSA, pDA))
+	{
+		auto& si = tup.get<0>();        // snow drift index
+		auto& sa = tup.get<1>();        // accumulated snow cover age
+		auto& da = tup.get<2>();        // accumulated snowdrift value
+		const auto t = tup.get<3>();    // temperature
+		const auto ff = tup.get<4>();   // wind speed
+		const auto sf = tup.get<5>();   // snow fall rate (mm/h)
+		const auto psa = tup.get<6>();  // previous accumulated snow cover age
+		const auto pda = tup.get<7>();  // previous accumulated snowdrift value
+
+		if (IsMissing(t) || IsMissing(ff) || IsMissing(sf))
+		{
+			continue;
+		}
+
+		if (t > 273.15)
+		{
+			// no possibility for snow drift until next snow fall
+			si = 0;
+			sa = 0;
+			da = 0;
+			continue;
+		}
+
+		/*
+		        this is not present in the fortran code either
+		        if (ff < 6.0)
+		        {
+		            si = 0;
+		        }
+		*/
+
+		if (sf > SFLimit)
+		{
+			// during snow fall
+			si = DriftIndex(DriftMagnitude(ff, 1.0));
+			sa = 0;
+			da = 0;
+		}
+		else
+		{
+			// after snow fall
+			sa = psa + 1;  // add one hour to snowcover age
+
+			if (ff < 6)
+			{
+				si = 0;
+				da = pda;
+			}
+			else
+			{
+				const double sv = DriftMagnitude(ff, MobilityIndex(pda, sa));
+				da = pda + sv;
+				si = DriftIndex(sv);
+			}
+		}
+	}
+}
+
+snow_drift::snow_drift()
+{
+	itsLogger = logger("snow_drift");
+}
+
+void snow_drift::Process(std::shared_ptr<const plugin_configuration> conf)
+{
+	Init(conf);
+
+	SetParams({SDIParam, SAParam, DAParam});
+
+	itsThreadDistribution = ThreadDistribution::kThreadForForecastTypeAndLevel;
+
+	Start();
+}
+
+void snow_drift::Calculate(std::shared_ptr<info<double>> myTargetInfo, unsigned short theThreadIndex)
+{
+	auto myThreadedLogger = logger("snow_driftThread #" + std::to_string(theThreadIndex));
+
+	const auto forecastTime = myTargetInfo->Time();
+	const auto forecastLevel = myTargetInfo->Level();
+	const auto forecastType = myTargetInfo->ForecastType();
+	const auto prod = myTargetInfo->Producer();
+
+	if (forecastTime.Step() == 0 && prod.Id() != 109)
+	{
+		return;
+	}
+
+	myThreadedLogger.Info("Calculating time " + static_cast<std::string>(forecastTime.ValidDateTime()) + " level " +
+	                      static_cast<std::string>(forecastLevel));
+
+	const std::string deviceType = "CPU";
+
+	auto TInfo = Fetch(forecastTime, level(kHeight, 2), param("T-K"), forecastType, false);
+	auto FFInfo = Fetch(forecastTime, level(kHeight, 10), param("FF-MS"), forecastType, false);
+	auto SFInfo = Fetch(forecastTime, level(kHeight, 0), param("SNR-KGM2"), forecastType, false);
+
+	if (!TInfo || !FFInfo || !SFInfo)
+	{
+		return;
+	}
+
+	auto prevTime = forecastTime;
+
+	std::shared_ptr<info<double>> pSAInfo, pDAInfo;
+
+	prevTime.ValidDateTime().Adjust(kHourResolution, -1);
+
+	// In the start of the forecast fetch the latest sa and da
+	// values from LAPS producer.
+
+	if (prod.Id() == 109 || forecastTime.Step() == 1)
+	{
+		prevTime.OriginDateTime(prevTime.ValidDateTime());
+
+		for (int i = 0; i < 3; i++)
+		{
+			pSAInfo = Fetch(prevTime, level(kHeight, 0), SAParam, forecastType, itsConfiguration->SourceGeomNames(),
+			                producer(109), false);
+			pDAInfo = Fetch(prevTime, level(kHeight, 0), DAParam, forecastType, itsConfiguration->SourceGeomNames(),
+			                producer(109), false);
+
+			if (pSAInfo && pDAInfo)
+			{
+				break;
+			}
+		}
+	}
+	else
+	{
+		pSAInfo = Fetch(prevTime, level(kHeight, 0), SAParam, forecastType, false);
+		pDAInfo = Fetch(prevTime, level(kHeight, 0), DAParam, forecastType, false);
+	}
+
+	bool spinUp = false;
+
+	std::vector<double> pSA, pDA;
+
+	if (!pSAInfo || !pDAInfo)
+	{
+		if (prod.Id() == 109)
+		{
+			// If we don't have a history of sa & da, start accumulating
+			// it but do not write snow index.
+
+			spinUp = true;
+			pSA.resize(myTargetInfo->Grid()->Size(), 0.0);
+			pDA.resize(myTargetInfo->Grid()->Size(), 0.0);
+			myThreadedLogger.Warning("Spinup phase, producing only DA and SA");
+		}
+		else
+		{
+			myThreadedLogger.Error("DA and SA not found from obs producer");
+			return;
+		}
+	}
+	else
+	{
+		pSA = VEC(pSAInfo);
+		pDA = VEC(pDAInfo);
+	}
+
+	CalculateSnowDriftIndex(myTargetInfo, VEC(TInfo), VEC(FFInfo), VEC(SFInfo), pSA, pDA);
+
+	if (spinUp)
+	{
+		myTargetInfo->Find<param>(SDIParam);
+		myTargetInfo->Data().Fill(MissingDouble());
+	}
+
+	myThreadedLogger.Info("[" + deviceType + "] Missing: " + std::to_string(util::MissingPercent(*myTargetInfo)) + "%");
+}
