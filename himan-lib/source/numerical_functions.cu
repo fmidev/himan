4df72c1fc55dc667b8f7d0b8b056de220dd084c4
Mikko Partio <mikko.partio@fmi.fi>
2019-05-15 18:09:03 +0300

    Add missing function definition for ProbLimitEq2DGPU()

--- himan-lib/source/numerical_functions.cu
+++ himan-lib/source/numerical_functions.cu
@@ -69,4 +69,19 @@ matrix<T> numerical_functions::ProbLimitGt2DGPU(const matrix<T>& A, const matrix
 template matrix<double> numerical_functions::ProbLimitGt2DGPU(const matrix<double>&, const matrix<double>&, double);
 template matrix<float> numerical_functions::ProbLimitGt2DGPU(const matrix<float>&, const matrix<float>&, float);
 
+template <typename T>
+matrix<T> numerical_functions::ProbLimitEq2DGPU(const matrix<T>& A, const matrix<T>& B, T limit)
+{
+	return Reduce2DGPU<T>(A, B,
+	                      [=] __device__(T & val1, T & val2, const T& a, const T& b) {
+		                      if (IsValid(a * b) && a * b == limit)
+			                      val1 += T(1);
+	                      },
+	                      [] __device__(const T& val1, const T& val2) { return (val1 >= 1) ? T(1) : T(0); }, T(0),
+	                      T(0));
+}
+
+template matrix<double> numerical_functions::ProbLimitEq2DGPU(const matrix<double>&, const matrix<double>&, double);
+template matrix<float> numerical_functions::ProbLimitEq2DGPU(const matrix<float>&, const matrix<float>&, float);
+
 #endif
