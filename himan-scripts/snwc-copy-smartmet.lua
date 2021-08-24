90a72be6dac0bab4cd63edb01c220ab0a6d063d0
Mikko Partio <mikko.partio@fmi.fi>
2021-08-24 08:59:49 +0300

    Try fetching data from previous smartmet forecast if the current one is missing

--- himan-scripts/snwc-copy-smartmet.lua
+++ himan-scripts/snwc-copy-smartmet.lua
@@ -14,6 +14,8 @@ editor_prod:SetProcess(181)
 local editor_origintime = raw_time(radon:GetLatestTime(editor_prod, "", 0))
 local editor_time = forecast_time(editor_origintime, current_time:GetValidDateTime())
 
+logger:Info(string.format("Latest Smartmet data origin time is: %s", editor_origintime:String("%Y-%m-%d %H:%M:%S")))
+
 function split (inputstr, sep)
   local t={}
   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
@@ -27,8 +29,18 @@ function FetchAndWrite(par)
   local data = luatool:FetchWithProducer(editor_time, current_level, par, current_forecast_type, editor_prod, "")
 
   if not data then
-    logger:Error("Data not found")
-    return
+    logger:Error("Data not found, is smartmet data just being loaded to database? Trying previous forecast")
+
+    editor_origintime = raw_time(radon:GetLatestTime(editor_prod, "", 1))
+    editor_time = forecast_time(editor_origintime, current_time:GetValidDateTime())
+
+    logger:Info(string.format("New Smartmet data origin time is: %s", editor_origintime:String("%Y-%m-%d %H:%M:%S")))
+
+    data = luatool:FetchWithProducer(editor_time, current_level, par, current_forecast_type, editor_prod, "")
+
+    if not data then
+      return
+    end
   end
 
   result:SetParam(par)
