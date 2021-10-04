71afb76b15319e8158c93bb80bdd12e8034cb533
Mikko Partio <mikko.partio@fmi.fi>
2021-10-04 14:59:25 +0300

    STU-16860: New version of radiation script

--- himan-scripts/snwc-radiation.lua
+++ himan-scripts/snwc-radiation.lua
@@ -6,7 +6,6 @@
 -- 
 
 local MISS = missing
-local cc_prod = producer(131, "ECG")
 local rad_prod = producer(240, "ECGMTA")
 local ftype = forecast_type(HPForecastType.kDeterministic)
 
@@ -16,62 +15,75 @@ logger:Info("Latest ECMWF analysis: " .. latest_origintime:String("%Y-%m-%d %H:%
 
 local latest_time = forecast_time(latest_origintime, current_time:GetValidDateTime())
 
+local prodid = configuration:GetTargetProducer():GetId()
 local lvl0 = level(HPLevelType.kHeight, 0)
+local glob = param("RADGLOC-WM2") -- "clear sky"
+local lw = param("RADLWC-WM2") -- "clear sky"
 local n = param("N-0TO1")
-local glob = param("RADGLO-WM2")
-local lw = param("RADLW-WM2")
+local cc_scale = 1.0
+
+if prodid == 280 then
+  n = param("N-PRCNT")
+  cc_scale = 0.01
+end
 
 local SW = luatool:FetchWithProducer(latest_time, lvl0, glob, ftype, rad_prod, "")
 local LW = luatool:FetchWithProducer(latest_time, lvl0, lw, ftype, rad_prod, "")
 local CC = luatool:FetchWithType(current_time, lvl0, n, current_forecast_type)
-local CC_ORIG = luatool:FetchWithProducer(latest_time, lvl0, n, ftype, cc_prod, "")
 
 if not CC then
   return
 end
 
-if not SW or not LW or not CC_ORIG then
+if not SW or not LW then
   -- HIMAN-338
   -- try earlier forecast
   -- also accept ECMWF 06/18 runs
-  latest_origintime:Adjust(HPTimeResolution.kHourResolution, -6)
-  latest_time = forecast_time(latest_origintime, current_time:GetValidDateTime())
+  for i=1, 2 do
+    latest_origintime:Adjust(HPTimeResolution.kHourResolution, -6)
+    latest_time = forecast_time(latest_origintime, current_time:GetValidDateTime())
 
-  logger:Info("Latest forecast data missing, trying older forecast with analysis time: " .. latest_origintime:String("%Y-%m-%d %H:%M:%S"))
+    logger:Info("Latest forecast data missing, trying older forecast with analysis time: " .. latest_origintime:String("%Y-%m-%d %H:%M:%S"))
 
-  SW = luatool:FetchWithProducer(latest_time, lvl0, glob, ftype, rad_prod, "")
-  LW = luatool:FetchWithProducer(latest_time, lvl0, lw, ftype, rad_prod, "")
-  CC_ORIG = luatool:FetchWithProducer(latest_time, lvl0, n, ftype, cc_prod, "")
+    SW = luatool:FetchWithProducer(latest_time, lvl0, glob, ftype, rad_prod, "")
+    LW = luatool:FetchWithProducer(latest_time, lvl0, lw, ftype, rad_prod, "")
 
-  if not SW or not LW or not CC_ORIG then
-    return
+    if SW and LW then
+      break
+    end
   end
 end
 
-local _SW = {}
-local _LW = {}
+if not SW or not LW then
+  return
+end
+
+local out_SW = {}
+local out_LW = {}
 
 for i=1, #SW do
-  local cc = CC[i]
-  local cc_orig = CC_ORIG[i] * 0.01
-  _SW[i] = MISS
-  _LW[i] = MISS
+  local cc = CC[i] * cc_scale
+  local sw = SW[i]
+  local lw = LW[i]
 
-  if cc == cc and SW[i] == SW[i] and LW[i] == LW[i] and CC_ORIG[i] == CC_ORIG[i] then
-    _SW[i] = (1 - 0.67 * math.pow(cc, 3.32)) / (1 - 0.67 * math.pow(cc_orig, 3.32)) * SW[i]
-    _LW[i] = (1 + 0.22 * math.pow(cc, 2.75)) / (1 + 0.22 * math.pow(cc_orig, 2.75)) * LW[i]
+  out_SW[i] = MISS
+  out_LW[i] = MISS
 
-    if (_SW[i] > 0 and ElevationAngle_(result:GetLatLon(i), current_time:GetValidDateTime()) <= 0) then
-      _SW[i] = 0
+  if cc == cc and sw == sw and lw == lw then
+    out_SW[i] = (1 - 0.67 * math.pow(cc, 3.32)) * sw
+    out_LW[i] = (1 + 0.22 * math.pow(cc, 2.75)) * lw
+
+    if (out_SW[i] > 0 and ElevationAngle_(result:GetLatLon(i), current_time:GetValidDateTime()) <= 0) then
+      out_SW[i] = 0
     end
 
-    _SW[i] = math.max(0, _SW[i])
+    out_SW[i] = math.max(0, out_SW[i])
   end
 end
 
 result:SetParam(param("RADGLO-WM2"))
-result:SetValues(_SW)
+result:SetValues(out_SW)
 luatool:WriteToFile(result)
 result:SetParam(param("RADLW-WM2"))
-result:SetValues(_LW)
+result:SetValues(out_LW)
 luatool:WriteToFile(result)
