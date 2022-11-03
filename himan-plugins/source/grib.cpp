b02d12a3834936421ba35b901d2d3a5d98125e7a
Mikko Partio <mikko.partio@fmi.fi>
2022-11-03 10:54:31 +0200

    Force WGS84 to MNWC_preop

--- himan-plugins/source/grib.cpp
+++ himan-plugins/source/grib.cpp
@@ -53,12 +53,14 @@ earth_shape<double> DetermineEarthShapeForProducer(const producer& prod, const e
 	{
 		case 4:    // MEPS
 		case 7:    // MNWC
+		case 8:    // MNWC_PREOP
 		case 10:   // MEPS_PREOP
 		case 11:   // CMEPS_PREOP
 		case 260:  // MEPSMTA
 		case 261:  // MEPS_PREOPMTA
 		case 265:  // CMEPS_PREOPMTA
 		case 270:  // MNWCMTA
+		case 271:  // MNWC_PREOPMTA
 			return ELLIPS_WGS84;
 		default:
 			return defaultShape;
