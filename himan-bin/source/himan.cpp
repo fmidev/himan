91965d6e817dcf76391624e6932337a0c84c8656
Mikko Partio <mikko.partio@fmi.fi>
2023-09-20 08:11:18 +0300

    Do not get cuda device count if user has disabled cuda

--- himan-bin/source/himan.cpp
+++ himan-bin/source/himan.cpp
@@ -526,10 +526,10 @@ void SetOutputFileType(shared_ptr<configuration>& conf, const string& outfileTyp
 	{
 		conf->OutputFileType(kGRIB2);
 	}
-//	else if (outfileType == "netcdf")
-//	{
-//		conf->OutputFileType(kNetCDF);
-//	}
+	//	else if (outfileType == "netcdf")
+	//	{
+	//		conf->OutputFileType(kNetCDF);
+	//	}
 	else if (outfileType == "querydata")
 	{
 		conf->OutputFileType(kQueryData);
@@ -799,45 +799,47 @@ void ParseCommandLine(shared_ptr<configuration>& conf, int argc, char** argv)
 		conf->UseCudaForPacking(false);
 		conf->UseCudaForUnpacking(false);
 	}
-
-	// get cuda device count for this server
-
-	int devCount;
-
-	cudaError_t err = cudaGetDeviceCount(&devCount);
-
-	switch (err)
+	else
 	{
-		case cudaSuccess:
-			break;
+		// get cuda device count for this server
 
-		case cudaErrorNoDevice:
-			// No device
+		int devCount;
 
-			devCount = 0;
+		cudaError_t err = cudaGetDeviceCount(&devCount);
 
-			conf->UseCuda(false);
-			conf->UseCudaForPacking(false);
-			conf->UseCudaForUnpacking(false);
+		switch (err)
+		{
+			case cudaSuccess:
+				break;
 
-			break;
+			case cudaErrorNoDevice:
+				// No device
 
-		default:
-			// No driver present or other problems
-			// with installation
+				devCount = 0;
 
-			devCount = 0;
+				conf->UseCuda(false);
+				conf->UseCudaForPacking(false);
+				conf->UseCudaForUnpacking(false);
+				cerr << "No cuda devices found\n";
 
-			conf->UseCuda(false);
-			conf->UseCudaForPacking(false);
-			conf->UseCudaForUnpacking(false);
+				break;
 
-			cerr << fmt::format("Error from cuda library: {} ({})\n", cudaGetErrorName(err), err);
-			break;
+			default:
+				// No driver present or other problems
+				// with installation
+
+				devCount = 0;
+
+				conf->UseCuda(false);
+				conf->UseCudaForPacking(false);
+				conf->UseCudaForUnpacking(false);
+
+				cerr << fmt::format("Error from cuda library: {} ({})\n", cudaGetErrorName(err), err);
+				break;
+		}
+
+		conf->CudaDeviceCount(devCount);
 	}
-
-	conf->CudaDeviceCount(devCount);
-
 	if (opt.count("cuda-device-id"))
 	{
 		SetCudaDeviceId(conf, cudaDeviceId);
