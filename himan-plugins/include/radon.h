e802d274ea3ea3ad0aee7f33ef9f7e76518c7bab
Mikko Partio <mikko.partio@fmi.fi>
2024-04-19 09:12:46 +0300

    Remove obsolete comment

--- himan-plugins/include/radon.h
+++ himan-plugins/include/radon.h
@@ -7,17 +7,6 @@
  *
  * @brief Access to radon database.
  *
- * A note on compiling and linking:
- *
- * This class is only an overcoat to fmidb. fmidb-library is just a bunch of objects
- * and it contains no linking, so we have to link here. Now, the problem is that
- * oracle instant client provides only shared library version of libclntsh. This
- * means that we have to link every library/executable that used this class against
- * libclntsh. And libclntsh want libaio etc.
- *
- *  One option to solve this would be to link this class statically with libclntsh,
- *  but to do so we'd need the full version of oracle client. Also, the .a version
- *  of oracle client library is HUGE, nearly 100M.
  */
 
 #ifndef RADON_H
