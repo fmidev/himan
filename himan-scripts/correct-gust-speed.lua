aedc806d1ebf2fe0485e5fb1942df5888b2fae40
Mikko Partio <mikko.partio@fmi.fi>
2024-10-01 15:31:31 +0300

    PDTK-165: Adjust wind gust correction factors

--- himan-scripts/correct-gust-speed.lua
+++ himan-scripts/correct-gust-speed.lua
@@ -34,6 +34,19 @@ if not FF or not FFG then
   return
 end
 
+if configuration:Exists("min_gust_factor") then
+  min_gust_factor = tonumber(configuration:GetValue("min_gust_factor"))
+else
+  min_gust_factor = 1.1
+end
+
+if configuration:Exists("max_gust_factor") then
+  max_gust_factor = tonumber(configuration:GetValue("max_gust_factor"))
+else
+  -- basically no limit
+  max_gust_factor = 20
+end
+
 local _FF = {}
 local _FFG = {}
 
@@ -42,10 +55,10 @@ for i=1, #FF do
   local ffg = FFG[i]
 
   -- Wind gust must be
-  -- * at least as high as wind speed
-  -- * at most 2.5 times higher than wind speed
-  ffg = math.max(ffg, ff)
-  ffg = math.min(ffg, ff * 2.5)
+  -- * at least as high as min_gust_factor * wind speed
+  -- * at most as high as max_gust_factor * wind speed
+  ffg = math.max(ffg, ff * min_gust_factor)
+  ffg = math.min(ffg, ff * max_gust_factor)
 
   _FFG[i] = ffg
 end
