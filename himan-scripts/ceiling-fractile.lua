8f37e6e555b02f6b95e848cededf9dc7c6b092ac
Elma Nevala <nevala@gpu-dev.fmi.fi>
2025-01-07 14:32:46 +0200

    ceiling fractile with ec producer

--- himan-scripts/ceiling-fractile.lua
+++ himan-scripts/ceiling-fractile.lua
@@ -1,35 +1,51 @@
-logger:Info("Calculating ceiling fractiles")
-
-function is_nan(val)
-  return val ~= val
-end 
-
-function nans_to_9999(vals)
-  for i = 1, #vals do
-    if is_nan(vals[i]) then
-      vals[i] = 9999
-    end
-  end
-  return vals
-end
 
 function fills_to_nan(vals)
-  local with_nan = {}
-  for i = 1, #vals do
-    if vals[i] == 9999 then
-      with_nan[i] = 0/0
-    else
-      with_nan[i] = vals[i]
+  local temp = {}
+
+  for k,v in pairs(vals) do
+    if v==19999 then
+      table.insert( temp, missing )
+    else 
+      table.insert( temp, v )
     end
   end
-  return with_nan
+
+  return temp
 end
 
-function get_percentile(vals, percent)
-  local index = math.ceil(percent * #vals)
-  if index < 1 then index = 1 end
-  if index > #vals then index = #vals end
-  return vals[index]
+function fill_sort(vals)
+  local temp = {}
+
+  for k,v in pairs(vals) do
+    if type(v) == 'number' and v==v then
+      table.insert( temp, v )
+    else 
+      table.insert( temp, 19999 )
+    end
+  end
+
+  table.sort( temp )
+  return temp
+end
+
+
+function percentile(vals, p)
+  local index = p * #vals
+  local int_index = math.floor(index)
+  local percentile = nil
+
+  local F0 = vals[1]
+
+
+  if p == 0 then
+    percentile = vals[1]
+  elseif p == 1 then
+    percentile =  vals[#vals]
+  else
+    percentile = (vals[int_index + 1] - vals[int_index]) * (index - int_index) + vals[int_index]
+  end
+
+  return percentile
 end
 
 function write_fractile(fractile, par)
@@ -38,43 +54,62 @@ function write_fractile(fractile, par)
   luatool:WriteToFile(result)
 end
 
+
+local ens = nil
+
+
+
+
 local producer = configuration:GetSourceProducer(0)
-local ensSize = tonumber(radon:GetProducerMetaData(producer, "ensemble size"))
+local ensSize = nil
 
-print('enssize: '.. ensSize)
+if producer:GetId() == 243 then 
+  ens = ensemble(param("CEIL-2-M"), 51, 250)
+else 
+  ensSize = tonumber(radon:GetProducerMetaData(producer, "ensemble size"))
 
-if not ensSize then
-  logger.Error("Ensemble size not found from database for producer " .. producer:GetId())
-  return
+  if not ensSize then
+    logger.Error("Ensemble size not found from database for producer " .. producer:GetId())
+    return
+  end
+
+  ens = lagged_ensemble(param("CEIL-2-M"), "MEPS_LAGGED_ENSEMBLE", ensSize)
 end
 
-local ens = lagged_ensemble(param("CEIL-2-M"), "MEPS_LAGGED_ENSEMBLE", ensSize)
-
 ens:Fetch(configuration, current_time, current_level)
-
 local lagEnsSize = ens:Size()
 
 local F0, F10, F25, F50, F75, F90, F100 = {}, {}, {}, {}, {}, {}, {}
+
+ens:FirstLocation()
+local values = ens:Values()
+values = fill_sort(values)
+values = fills_to_nan(values)
+
+F0[1] = percentile(values, 0)
+F10[1] = percentile(values, 0.1)
+F25[1] = percentile(values, 0.25)
+F50[1] = percentile(values, 0.5)
+F75[1] = percentile(values, 0.75)
+F90[1] = percentile(values, 0.9)
+F100[1] = percentile(values, 1)
+
+local i = 2
 while ens:NextLocation() do
-    local vals = ens:Values()
+  local values = ens:Values()
+  values = fill_sort(values)
+  values = fills_to_nan(values)
 
-    vals = nans_to_9999(vals)
-
-    table.sort(vals)
-
-    vals = fills_to_nan(vals)
-
-    F0[#F0 + 1] = get_percentile(vals, 0)
-    F10[#F10 + 1] = get_percentile(vals, 0.1)
-    F25[#F25 + 1] = get_percentile(vals, 0.25)
-    F50[#F50 + 1] = get_percentile(vals, 0.5)
-    F75[#F75 + 1] = get_percentile(vals, 0.75)
-    F90[#F90 + 1] = get_percentile(vals, 0.9)
-    F100[#F100 + 1] = get_percentile(vals, 1)
-
+  F0[i] = percentile(values, 0)
+  F10[i] = percentile(values, 0.1)
+  F25[i] = percentile(values, 0.25)
+  F50[i] = percentile(values, 0.5)
+  F75[i] = percentile(values, 0.75)
+  F90[i] = percentile(values, 0.9)
+  F100[i] = percentile(values, 1)
+  i = i + 1
 end
 
--- set results to a parameter
 write_fractile(F0, param("F0-CEIL-2-M"))
 write_fractile(F10, param("F10-CEIL-2-M"))
 write_fractile(F25, param("F25-CEIL-2-M"))
