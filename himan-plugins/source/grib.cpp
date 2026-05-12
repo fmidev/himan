fcc6204437ebf401b96973a66642ad40e883c6d7
Mikko Partio <mikko.partio@fmi.fi>
2026-05-12 15:43:29 +0300

    Add old ECMWF numbers to Hard coded list

--- himan-plugins/source/grib.cpp
+++ himan-plugins/source/grib.cpp
@@ -2870,7 +2870,7 @@ himan::producer ReadProducer(const search_options& options, const NFmiGribMessag
 				return prod;
 			}
 
-			if (process <= 154 && process >= 142)
+			if (process <= 160 && process >= 142)
 			{
 				if (message.ForecastType() <= 2)
 				{
