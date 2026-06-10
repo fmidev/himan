9d94593b1db8c280b73c5b93984c13fd89e7a3b9
Elma Nevala <elma.nevala@fmi.fi>
2026-06-10 10:58:01 +0300

    Replace PRECFORM-N2 with POTPRECF-N to avoid dependancy issues

--- himan-scripts/precipitation-limit-values.lua
+++ himan-scripts/precipitation-limit-values.lua
@@ -6,14 +6,14 @@
 --
 
 local RR = luatool:Fetch(current_time, current_level, param("RRR-KGM2"), current_forecast_type)
-local PREC_FORM = luatool:Fetch(current_time, current_level, param("PRECFORM2-N"), current_forecast_type)
+local POT_PRECF = luatool:Fetch(current_time, current_level, param("POTPRECF-N"), current_forecast_type)
 
 if not RR then
-  plogger:Error("VIRE RR-KGM2 data not found, aborting")
+  plogger:Error("VIRE RRR-KGM2 data not found, aborting")
   return
 end
-if not PREC_FORM then
-  plogger:Error("VIRE PRECFORM2-N data not found, aborting")
+if not POT_PRECF then
+  plogger:Error("VIRE POTPRECF-N data not found, aborting")
   return
 end
 
@@ -32,29 +32,23 @@ end
 
 -- PDTK-229
 for i = 1, #RR do
-  if PREC_FORM[i] == 0 and RR[i] > 0 and RR[i] < 0.015 then
+  if POT_PRECF[i] == 0 and RR[i] > 0 and RR[i] < 0.015 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end
-  if PREC_FORM[i] == 1 and RR[i] > 0 and RR[i] < 0.1 then
+  if POT_PRECF[i] == 1 and RR[i] > 0 and RR[i] < 0.1 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end
-  if PREC_FORM[i] == 2 and RR[i] > 0 and RR[i] < 0.075 then
+  if POT_PRECF[i] == 2 and RR[i] > 0 and RR[i] < 0.075 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end
-  if PREC_FORM[i] == 3 and RR[i] > 0 and RR[i] < 0.05 then
+  if POT_PRECF[i] == 3 and RR[i] > 0 and RR[i] < 0.05 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end
-  if PREC_FORM[i] == 4 and RR[i] > 0 and RR[i] < 0.01 then
+  if POT_PRECF[i] == 4 and RR[i] > 0 and RR[i] < 0.01 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end 
-  if PREC_FORM[i] == 5 and RR[i] > 0 and RR[i] < 0.02 then
+  if POT_PRECF[i] == 5 and RR[i] > 0 and RR[i] < 0.02 then
     RR[i] = 0
-    PREC_FORM[i] = missing
   end
 end
 
@@ -63,8 +57,4 @@ rrparam = param("RRR-KGM2")
 rrparam:SetAggregation(aggregation(HPAggregationType.kAccumulation, time_duration("01:00")))
 result:SetParam(rrparam)
 result:SetValues(RR)
-luatool:WriteToFile(result)
-
-precparam = param("PRECFORM2-N")
-result:SetValues(PREC_FORM)
 luatool:WriteToFile(result)
